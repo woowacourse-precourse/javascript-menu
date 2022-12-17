82673ba5a7665d37de683448f5f6632587151d29
daal2quf <daal2quf@gmail.com>
2022-12-17 14:08:44 +0900

    feat(recommandMenuSystem): 유저에게 입력을 요청하고 입력값에대한 예외처리 기능 추가
    add:
    requestCoachName() 코치 이름을 요청
    validateInputOfCoachName(): handler함수를 이용해 잘못된 값이 예외처리될경우 코치 이름 재입력 요청

--- src/controller/RecommandMenuSystem.js
+++ src/controller/RecommandMenuSystem.js
@@ -1,4 +1,7 @@
+const { InputView } = require('../view/InputView');
 const { OutputView } = require('../view/OutputView');
+const { isCorrectInputHandler } = require('./isCorrectInputHandler');
+const { Validation } = require('./Validation');
 
 class RecommandMenuSystem {
   constructor(model) {
@@ -7,6 +10,18 @@ class RecommandMenuSystem {
 
   start() {
     OutputView.printRecommandMenuStart();
+    this.requestCoachName();
+  }
+
+  requestCoachName() {
+    InputView.readCoachName(this.validateInputOfCoachName.bind(this));
+  }
+
+  validateInputOfCoachName(userInput) {
+    if (!isCorrectInputHandler(Validation.ofCoachName, userInput)) {
+      this.requestCoachName();
+      return;
+    }
   }
 }
 
