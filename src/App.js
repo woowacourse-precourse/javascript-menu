2f1e87a63a8e914dabf3db14b76b2a55e57ea8c5
shackstack <kms1061@naver.com>
2022-12-17 15:39:27 +0900

    feat(Menu): 요일별 랜덤 카테고리 생성기능구현

--- src/App.js
+++ src/App.js
@@ -4,20 +4,10 @@ const OutputView = require("./view/OutputView");
 
 const Coach = require("./model/Coach");
 const Menu = require("./model/Menu");
-const { printErrorMessage } = require("./view/OutputView");
-
-const SAMPLE = {
-  일식: "규동, 우동, 미소시루, 스시, 가츠동, 오니기리, 하이라이스, 라멘, 오코노미야끼",
-  한식: "김밥, 김치찌개, 쌈밥, 된장찌개, 비빔밥, 칼국수, 불고기, 떡볶이, 제육볶음",
-  중식: "깐풍기, 볶음면, 동파육, 짜장면, 짬뽕, 마파두부, 탕수육, 토마토 달걀볶음, 고추잡채",
-  아시안:
-    "팟타이, 카오 팟, 나시고렝, 파인애플 볶음밥, 쌀국수, 똠얌꿍, 반미, 월남쌈, 분짜",
-  양식: "라자냐, 그라탱, 뇨끼, 끼슈, 프렌치 토스트, 바게트, 스파게티, 피자, 파니니",
-};
 
 class App {
   #coaches;
-  #coachIndex = 0;
+  #sequence = 0;
   #coachesManager;
   #menuManager = new Menu();
 
@@ -49,13 +39,13 @@ class App {
   }
 
   readUneatableMenu() {
-    if (this.#coachIndex < this.#coaches.length) {
+    if (this.#sequence < this.#coaches.length) {
       InputView.readUneatableMenu(
-        this.#coaches[this.#coachIndex],
+        this.#coaches[this.#sequence],
         this.saveUneatableMenu.bind(this)
       );
     }
-    if (this.#coachIndex === this.#coaches.length) {
+    if (this.#sequence === this.#coaches.length) {
       this.makeRecommandResult();
     }
   }
@@ -65,7 +55,7 @@ class App {
       this.#menuManager.validate(menus);
       OutputView.printEmptyLine();
 
-      this.#coachIndex += 1;
+      this.#sequence += 1;
       this.readUneatableMenu();
     } catch (error) {
       OutputView.printErrorMessage(error);
--- src/model/Menu.js
+++ src/model/Menu.js
@@ -1,4 +1,10 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+const Random = MissionUtils.Random;
+
+const { MENU_LIST } = require("../constant/MenuList");
+
 class Menu {
+  #categories = [];
   #uneatableMenus = [];
 
   validate(menus) {
@@ -14,7 +20,45 @@ class Menu {
     }
   }
 
-  makeRecommandResult() {}
+  //   일식: "규동, 우동, 미소시루, 스시, 가츠동, 오니기리, 하이라이스, 라멘, 오코노미야끼",
+  //   한식: "김밥, 김치찌개, 쌈밥, 된장찌개, 비빔밥, 칼국수, 불고기, 떡볶이, 제육볶음",
+  //   중식: "깐풍기, 볶음면, 동파육, 짜장면, 짬뽕, 마파두부, 탕수육, 토마토 달걀볶음, 고추잡채",
+  //   아시안:
+  //     "팟타이, 카오 팟, 나시고렝, 파인애플 볶음밥, 쌀국수, 똠얌꿍, 반미, 월남쌈, 분짜",
+  //   양식: "라자냐, 그라탱, 뇨끼, 끼슈, 프렌치 토스트, 바게트, 스파게티, 피자, 파니니",
+
+  makeRecommandResult() {
+    this.recommandCategories();
+  }
+
+  recommandCategories() {
+    const pickedCategory = MENU_LIST.category[Random.pickNumberInRange(0, 4)];
+
+    if (this.checkAvailable(pickedCategory) === false) {
+      return this.recommandCategories();
+    }
+    this.#categories.push(pickedCategory);
+
+    if (this.#categories.length !== 5) {
+      this.recommandCategories();
+    }
+  }
+
+  checkAvailable(pickedCategory) {
+    let count = 0;
+    this.#categories.forEach((item) => {
+      if (item === pickedCategory) {
+        count += 1;
+      }
+    });
+    if (count >= 2) {
+      return false;
+    }
+    return true;
+  }
 }
+
+const menu = new Menu();
+menu.makeRecommandResult();
 
 module.exports = Menu;
