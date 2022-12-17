b8b02acf79bc431add5372e96c5e56c9913c6408
woo-jk <201702042@o.cnu.ac.kr>
2022-12-17 17:07:04 +0900

    test: Coach 못먹는 메뉴 추가 테스트 구현

--- __tests__/CoachTest.js
+++ __tests__/CoachTest.js
@@ -10,4 +10,16 @@ describe("Coach 클래스 테스트", () => {
     const menus = coach.getMenus();
     expect(menus).toEqual(["삼겹살", "치킨"]);
   });
+
+  test("못먹는 메뉴 추가 테스트", () => {
+    const coach = new Coach("정균");
+
+    coach.addBannedMenu(["삼겹살", "치킨"]);
+
+    const notBannedMenuResult = coach.addMenu("떡볶이");
+    const bannedMenuResult = coach.addMenu("치킨");
+
+    expect(notBannedMenuResult).toBeTruthy();
+    expect(bannedMenuResult).toBeFalsy();
+  });
 });
