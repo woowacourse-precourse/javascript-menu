f6e419b7108536daccce02e96eef0f998bad3eaf
ReturnReason <reasonz@daum.net>
2022-12-17 14:40:38 +0900

    feat(InputView): 못 먹는 메뉴를 입력받는 메서드 추가

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -5,6 +5,9 @@ const InputView = {
   readCoachName(callback) {
     return readLine(INPUT_MESSAGES.name, callback);
   },
+  readCoachUnableEatMenu(coachName, callback) {
+    return readLine(INPUT_MESSAGES.printUnableEatMenu(coachName), callback);
+  },
 };
 
 module.exports = InputView;
