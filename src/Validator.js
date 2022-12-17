ed3bb4825b91aacc7db53f58ff80490b08f712e8
useon <yoosk4986@naver.com>
2022-12-17 13:56:35 +0900

    feat: 코치 이름 입력이 올바르지 않는 예외 상황을 처리한다

--- src/Validator.js
+++ src/Validator.js
@@ -4,6 +4,12 @@ const Validator = {
     if (!(namesArr.length > 0 || namesArr.length < 4)) {
       throw Error('[ERROR] 코치는 최소 2명, 최대 5명을 입력해야 합니다.');
     }
+
+    for (let inputOrder = 0; inputOrder < namesArr.length; inputOrder++) {
+      if (!(namesArr[inputOrder].length > 0 && namesArr[inputOrder].length < 4)) {
+        throw Error('[ERROR] 코치의 이름은 최소 2글자, 최대 4글자를 입력해야 합니다.');
+      }
+    }
   },
 };
 
