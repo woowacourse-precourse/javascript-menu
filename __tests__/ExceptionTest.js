cd589a831744906018671ff77213491feb16d972
shlee0820 <72565083+shlee0820@users.noreply.github.com>
2022-12-17 14:42:00 +0900

    test(ExceptionTest): 기능 1. 예외처리 테스트 케이스 추가

---
+++ __tests__/ExceptionTest.js
@@ -0,0 +1,11 @@
+const ExceptionController = require('../src/Controllers/ExceptionController');
+
+describe('사용자 입력 에러 테스트', () => {
+
+    test.each([['가가'], ['가가', '나나', '다다', '라라', '마마', '바바'], ['가', '나'], ['가', '나나', '다다']])('사용자가 잘못된 코치 입력값을 입력한 경우 테스트', (coachList) => {
+        const result = ExceptionController.isValidCoach(coachList);
+        expect(result).toBeFalsy();
+    });
+
+    
+});
