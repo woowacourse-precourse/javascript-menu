7090f07c52c2ebd840d6f8ba990a18845fe11211
inyeong-kang <tomatorkd@naver.com>
2022-12-17 13:15:15 +0900

    feat: Ask coach names

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+const InputView = require("./View/InputView");
+const OutputView = require("./View/OutputView");
+
 const SAMPLE = {
 	일식: '규동, 우동, 미소시루, 스시, 가츠동, 오니기리, 하이라이스, 라멘, 오코노미야끼',
 	한식: '김밥, 김치찌개, 쌈밥, 된장찌개, 비빔밥, 칼국수, 불고기, 떡볶이, 제육볶음',
@@ -9,7 +12,15 @@ const SAMPLE = {
 
 class App {
   play() {
-
+	OutputView.printStartMsg();
+	this.askName();
+  }
+  
+  askName() {
+	InputView.readName((names)=>{
+		const nameList = names.split(',');
+		console.log(nameList);		
+	});
   }
 }
 
--- src/View/InputView.js
+++ src/View/InputView.js
@@ -3,7 +3,9 @@ const { INPUT_MESSAGE } = require("../constants");
 
 
 const InputView = {
-
+  readName(callback) {
+    Console.readLine('코치의 이름을 입력해 주세요. (, 로 구분)\n', callback);
+  }
 };
 
 module.exports = InputView;
