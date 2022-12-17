5cfddd7fa4ec4875b766998787d03f3d558c3338
kangyeongmin <kym6050@hanyang.ac.kr>
2022-12-17 14:26:22 +0900

    fix: 입력 버그 수정

--- src/View/InputView.js
+++ src/View/InputView.js
@@ -3,19 +3,19 @@ const MissionUtils = require("@woowacourse/mission-utils");
 const InputView = {
   readNames(callback) {
     MissionUtils.Console.readLine(
-      "코치의 이름을 입력해 주세요. (, 로 구분)",
+      "코치의 이름을 입력해 주세요. (, 로 구분)\n",
       (input) => {
-        callback(input);
+        const namesArray = input.split(",");
+        callback(namesArray);
       }
     );
   },
 
   readPickyMenu(name, callback) {
     MissionUtils.Console.readLine(
-      name + "(이)가 못 먹는 메뉴를 입력해 주세요.",
+      name + "(이)가 못 먹는 메뉴를 입력해 주세요.\n",
       (input) => {
-        const menuArray = input.split(",");
-        callback(menuArray);
+        callback(input);
       }
     );
   },
