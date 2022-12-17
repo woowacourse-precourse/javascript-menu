c1ff8dfcaba64df4029ff5d06de52d519f31fe17
nangkyeonglim <nangkyeonglim@gmail.com>
2022-12-17 13:36:29 +0900

    feat(MenuSelectorController): 코치를 등록하는 로직 구현

--- src/controller/MenuSelectorController.js
+++ src/controller/MenuSelectorController.js
@@ -1,3 +1,5 @@
+const Coach = require('../models/Coach');
+const MenuSelector = require('../models/MenuSelector');
 const InputView = require('../views/InputView');
 const OutputView = require('../views/OutputView');
 
@@ -11,7 +13,14 @@ class MenuSelectorController {
     InputView.readCoachNames(this.#registerCoachPhase.bind(this));
   }
 
-  #registerCoachPhase() {}
+  //[Coach(토미), Coach(제임스), ..]
+  #registerCoachPhase(names) {
+    new MenuSelector(
+      names.split(',').map((name) => {
+        new Coach(name);
+      })
+    );
+  }
 }
 
 module.exports = MenuSelectorController;
