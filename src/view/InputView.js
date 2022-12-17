f111e87107ed17e5bcf6067a1f5b5b828900680a
shackstack <kms1061@naver.com>
2022-12-17 17:29:34 +0900

    refactor(InputView,OutputView): MissionUtils.Console 코드 축약

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -1,17 +1,15 @@
 const MissionUtils = require("@woowacourse/mission-utils");
+const Console = MissionUtils.Console;
 
 const InputView = {
   readCoachesName(fn) {
-    MissionUtils.Console.readLine(
-      "코치의 이름을 입력해 주세요. (, 로 구분)\n",
-      (names) => {
-        fn(names);
-      }
-    );
+    Console.readLine("코치의 이름을 입력해 주세요. (, 로 구분)\n", (names) => {
+      fn(names);
+    });
   },
 
   readUneatableMenu(coach, fn) {
-    MissionUtils.Console.readLine(
+    Console.readLine(
       `${coach}(이)가 못 먹는 메뉴를 입력해 주세요.\n`,
       (menus) => {
         fn(menus);
--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -1,35 +1,34 @@
 const MissionUtils = require("@woowacourse/mission-utils");
+const Console = MissionUtils.Console;
 
 const OutputView = {
   printEmptyLine() {
-    MissionUtils.Console.print("");
+    Console.print("");
   },
 
   printStartMessage() {
-    MissionUtils.Console.print("점심 메뉴 추천을 시작합니다.\n");
+    Console.print("점심 메뉴 추천을 시작합니다.\n");
   },
 
   printErrorMessage(error) {
-    MissionUtils.Console.print(error);
+    Console.print(error);
   },
 
   printResult(recommandedCategories, coachData, coaches) {
-    MissionUtils.Console.print("메뉴 추천 결과입니다.");
-    MissionUtils.Console.print(
-      "[ 구분 | 월요일 | 화요일 | 수요일 | 목요일 | 금요일 ]"
-    );
-    MissionUtils.Console.print(
+    Console.print("메뉴 추천 결과입니다.");
+    Console.print("[ 구분 | 월요일 | 화요일 | 수요일 | 목요일 | 금요일 ]");
+    Console.print(
       `[ 카테고리 | ${recommandedCategories[0]} | ${recommandedCategories[1]} | ${recommandedCategories[2]} | ${recommandedCategories[3]} | ${recommandedCategories[4]} ]`
     );
     coaches.forEach((coach) => {
-      MissionUtils.Console.print(
+      Console.print(
         `[ ${coach} | ${coachData[coach][0]} | ${coachData[coach][1]} | ${coachData[coach][2]} | ${coachData[coach][3]} | ${coachData[coach][4]} ]`
       );
     });
   },
 
   printCompleteMessage() {
-    MissionUtils.Console.print("추천을 완료했습니다.");
+    Console.print("추천을 완료했습니다.");
   },
 };
 
