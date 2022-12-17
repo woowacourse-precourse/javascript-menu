1c1176069c1a2915bfc5e92600605e882c333f85
solo5star <solo_5@naver.com>
2022-12-17 17:11:14 +0900

    feat(App): 도메인과 서비스를 사용하여 종합적으로 작동하도록 함

--- src/App.js
+++ src/App.js
@@ -1,5 +1,64 @@
+/* eslint-disable require-yield */
+/* eslint-disable no-restricted-syntax */
+const InputView = require('./views/InputView');
+const OutputView = require('./views/OutputView');
+const runGenerator = require('./utils/runGenerator');
+const Coach = require('./domains/Coach');
+const LaunchService = require('./services/LaunchService');
+const { CATEGORIES, DAYS } = require('./fixtures');
+
 class App {
-  play() {}
+  #inputView = new InputView();
+
+  #outputView = new OutputView();
+
+  #launchService = new LaunchService(CATEGORIES);
+
+  /** @type {Coach[]} */
+  #coaches;
+
+  play() {
+    runGenerator(this.#routines.bind(this));
+  }
+
+  * #routines() {
+    yield* this.#routinePrintHello();
+    yield* this.#routineReadCoaches();
+    for (const coach of this.#coaches) {
+      yield* this.#routineReadDislikeMenus(coach);
+    }
+    yield* this.#routinePrintSuggestionTable();
+    yield* this.#routinePrintGoodbye();
+
+    this.#inputView.close();
+  }
+
+  * #routinePrintHello() {
+    this.#outputView.printHello();
+  }
+
+  * #routineReadCoaches() {
+    this.#coaches = yield (resolve) => this.#inputView.readCoaches(resolve);
+  }
+
+  /**
+   * @param {Coach} coach
+   */
+  * #routineReadDislikeMenus(coach) {
+    yield (resolve) => {
+      this.#inputView.readDislikeMenus(coach, this.#launchService.getMenus(), resolve);
+    };
+  }
+
+  * #routinePrintSuggestionTable() {
+    const categories = this.#launchService.suggestCategories(DAYS);
+    const menuTable = this.#launchService.suggestMenuTable(this.#coaches, categories);
+    this.#outputView.printSuggestionTable(DAYS, categories, this.#coaches, menuTable);
+  }
+
+  * #routinePrintGoodbye() {
+    this.#outputView.printGoodbye();
+  }
 }
 
 module.exports = App;
