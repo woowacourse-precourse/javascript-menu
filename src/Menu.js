490d622734148092e30a38aa1c69d1dc56035892
hafnium1923 <hafnium1923@gmail.com>
2022-12-17 14:49:07 +0900

    feat pickMenu()구현

--- src/Menu.js
+++ src/Menu.js
@@ -1,13 +1,11 @@
-const SAMPLE = {
-	일식: '규동, 우동, 미소시루, 스시, 가츠동, 오니기리, 하이라이스, 라멘, 오코노미야끼',
-	한식: '김밥, 김치찌개, 쌈밥, 된장찌개, 비빔밥, 칼국수, 불고기, 떡볶이, 제육볶음',
-	중식: '깐풍기, 볶음면, 동파육, 짜장면, 짬뽕, 마파두부, 탕수육, 토마토 달걀볶음, 고추잡채',
-	아시안:
-		'팟타이, 카오 팟, 나시고렝, 파인애플 볶음밥, 쌀국수, 똠얌꿍, 반미, 월남쌈, 분짜',
-	양식: '라자냐, 그라탱, 뇨끼, 끼슈, 프렌치 토스트, 바게트, 스파게티, 피자, 파니니',
-};
 const MU = require("@woowacourse/mission-utils");
-
+const SAMPLE = [
+    ['규동', '우동', '미소시루', '스시', '가츠동', '오니기리', '하이라이스', '라멘', '오코노미야끼'],
+    ['김밥', '김치찌개', '쌈밥', '된장찌개', '비빔밥', '칼국수', '불고기', '떡볶이', '제육볶음'],
+    ['깐풍기', '볶음면', '동파육', '짜장면', '짬뽕', '마파두부', '탕수육', '토마토 달걀볶음', '고추잡채'],
+    ['팟타이', '카오 팟', '나시고렝', '파인애플 볶음밥', '쌀국수', '똠얌꿍', '반미', '월남쌈', '분짜'],
+    ['라자냐', '그라탱', '뇨끼', '끼슈', '프렌치 토스트', '바게트', '스파게티', '피자', '파니니']
+]
 const Menu ={
     /**
      * 카테고리는 Random.pickNumberInRange()를 사용
@@ -30,8 +28,18 @@ const Menu ={
      * const menu = Randoms.shuffle(menus)[0];//Menu는 무작위 배열 > 
      * 못먹거나 이미 선택한 경우 다시 섞어서 첫번째 값 사용
      */
-    pickMenu() {
-
+    pickMenu(COACH, category, hateMenu) {
+        let pickMenu = [];
+        for(let i = 0; i < COACH.length; i++) {
+            let j = 0;
+            while(j < 5) {
+                let pick = MU.Random.shuffle(SAMPLE[category[j]-1])[0];
+                if(pickMenu[i].includes(pick) || hateMenu[i].includes(pick)) continue;
+                pickMenu[i][j] = pick;
+                j++;
+            }
+        }
+        console.log(pickMenu);
     }
 };
 
