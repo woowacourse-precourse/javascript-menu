75bbf3912af1b8bc6ae1aef0c5a88bf0d9c522ed
Q Kim <qoli0703@gmail.com>
2022-12-17 14:06:55 +0900

    refactor(Recommend): select random menu index, not name
    ApplicationTest의 mockShuffles를 사용한 테스트를 보면
    섞는 건 1-9까지 숫자의 배열임
    즉 메뉴명이 아니라 인덱스를 섞기 때문에
    해당 조건에 맞춰 코드를 수정함

--- src/Recommend.js
+++ src/Recommend.js
@@ -1,6 +1,8 @@
 const MissionUtils = require('@woowacourse/mission-utils');
 const { MenuConfig } = require('./Config');
 
+const sequenced = (_, idx) => idx + 1;
+
 const Recommend = {
   randomCategory() {
     const categoryNumber = MissionUtils.Random.pickNumberInRange(1, MenuConfig.CATEGORIES + 1);
@@ -9,7 +11,8 @@ const Recommend = {
 
   randomMenu(category, menu) {
     const menuList = menu[category].trim().split(', ');
-    const menuNumber = MissionUtils.Random.shuffle(menuList)[0];
+    const menuIndexArray = Array.from({ length: menuList.length }, sequenced);
+    const menuNumber = MissionUtils.Random.shuffle(menuIndexArray)[0];
     return menuList[menuNumber - 1];
   },
 };
