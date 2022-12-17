e0544013857a4c12a3181f506a4a211bb759d4c1
Q Kim <qoli0703@gmail.com>
2022-12-17 17:21:24 +0900

    test: refactor import directories
    참고: 87859606d1e3200d64b7f372ac2d424375209ada
    디렉토리 변경으로 인한 코드 수정

--- __tests__/CategoryTest.js
+++ __tests__/CategoryTest.js
@@ -1,6 +1,6 @@
 const MissionUtils = require('@woowacourse/mission-utils');
-const Recommend = require('../src/Recommend');
-const Category = require('../src/Category');
+const Recommend = require('../src/selector/Recommend');
+const Category = require('../src/selector/Category');
 
 const SAMPLE = {
   일식: '규동, 우동, 미소시루, 스시, 가츠동, 오니기리, 하이라이스, 라멘, 오코노미야끼',
--- __tests__/CoachTest.js
+++ __tests__/CoachTest.js
@@ -1,5 +1,5 @@
 const MissionUtils = require('@woowacourse/mission-utils');
-const Coach = require('../src/Coach');
+const Coach = require('../src/coach/Coach');
 
 const SAMPLE = {
   일식: '규동, 우동, 미소시루, 스시, 가츠동, 오니기리, 하이라이스, 라멘, 오코노미야끼',
