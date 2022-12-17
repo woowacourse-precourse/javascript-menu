758742406e0cd6502b33420fb6134c0142cadd73
daal2quf <daal2quf@gmail.com>
2022-12-17 16:49:47 +0900

    feat(validation): 예외처리 조건문에 대한 추상화 레벨 맞추기
    add:
    isCorrectHowManyCoach(): 2~5명의 코치를 입력했는지 확인
    isCorrectInputOfFood(): 메뉴에 음식이 있는지 판별

--- src/controller/Validation.js
+++ src/controller/Validation.js
@@ -5,20 +5,27 @@ const Validation = {
     if (!isCorrectLength(userInput)) {
       throw new Error('[ERROR] 코치의 이름은 2글자 이상 4글자 이하로만 입력해주세요');
     }
-    if (userInput.split(',').length < 2 || userInput.split(',').length > 5) {
+    if (isCorrectHowManyCoach(userInput)) {
       throw new Error('[ERROR] 코치는 최소 2명 이상 입력해야 합니다.');
     }
   },
 
   ofMenu(userInput) {
-    const menu = [...MenuMap.values()].join(', ').split(', ');
-    if (!userInput.split(',').every((food) => menu.includes(food)))
-      throw new Error('[ERROR] 메뉴에 없는 메뉴입니다.');
+    if (!isCorrectInputOfFood(userInput)) throw new Error('[ERROR] 메뉴에 없는 메뉴입니다.');
   },
 };
 
 const isCorrectLength = (userInput) => {
   return userInput.split(',').every((name) => name.length >= 2 && name.length <= 4);
+};
+
+const isCorrectHowManyCoach = (userInput) => {
+  return userInput.split(',').length < 2 || userInput.split(',').length > 5;
+};
+
+const isCorrectInputOfFood = (userInput) => {
+  const menu = [...MenuMap.values()].join(', ').split(', ');
+  return userInput.split(',').every((food) => menu.includes(food));
 };
 
 module.exports = { Validation };
