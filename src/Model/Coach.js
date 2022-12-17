b9268d615456b553ada82371e65c90e0e2b8e727
kangyeongmin <kym6050@hanyang.ac.kr>
2022-12-17 14:48:39 +0900

    feat(Coach): 코치가 못 먹는 메뉴가 0~2개인지 검사한다

--- src/Model/Coach.js
+++ src/Model/Coach.js
@@ -6,7 +6,6 @@ class Coach {
   #menuOfWeek;
 
   constructor(name, pickyMenu) {
-    this.validateName(name);
     this.validatePickyMenu(pickyMenu);
     this.#name = name;
     this.#pickyMenu = pickyMenu;
@@ -15,12 +14,10 @@ class Coach {
 
   getName() {}
 
-  validateName(name) {
-    if (!CrewInfo.course.includes(options[0]))
-      throw new Error("[ERROR] 코치의 이름은 2~4글자여야 합니다.");
+  validatePickyMenu(pickyMenu) {
+    if (pickyMenu.length > 3)
+      throw new Error("[ERROR] 못 먹는 메뉴는 최대 2개까지만 입력 가능합니다.");
   }
-
-  validatePickyMenu(pickyMenu) {}
 }
 
 module.exports = Coach;
