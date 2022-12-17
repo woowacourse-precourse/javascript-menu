97fd835152ee7ec31728ccd691c4653ca093e0d5
daal2quf <daal2quf@gmail.com>
2022-12-17 17:57:58 +0900

    test(recommandMenu): argument로 전달된 문자열이 원하는 모양의 배열로 변하는지 테스트
    add:
    test1: 원하는 배열로 바뀌는것 확인

--- __tests__/RecommandMenuTest.js
+++ __tests__/RecommandMenuTest.js
@@ -1,4 +1,5 @@
 const { Console } = require('@woowacourse/mission-utils');
+const { MenuMap } = require('../src/domain/MenuMap');
 const RecommandMenu = require('../src/domain/RecommandMenu');
 
 afterEach(() => {
@@ -16,5 +17,15 @@ describe('RecommandMenu 클래스의 exceptHateFoodOfMenu 메서드 테스트', 
     const hateFood = ['야채'];
     const result = recommandMenu.exceptHateFoodOfMenu(menu, hateFood);
     expect(result).toEqual(['치킨', '피자']);
+  });
+});
+
+describe('RecommandMenu 클래스의 convertArray 메서드 테스트', () => {
+  test('메뉴맵 형식의 문자열을 메뉴추천 프로그램에 맞는 형태의 배열로변환', () => {
+    jest.setTimeout(30000);
+    const menu = '참치, 김치';
+    const expected = ['참치', '김치'];
+    const result = recommandMenu.convertArray(menu);
+    expect(result).toEqual(expected);
   });
 });
