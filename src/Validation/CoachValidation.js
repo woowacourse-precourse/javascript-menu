6672f87ac4aa78a65f155c1c76bcf7572a62c8f3
gyeongza <gyeongza@naver.com>
2022-12-17 15:09:18 +0900

    feat : [예외처리] 코치명의 배열 길이가 2 ~ 5가 아닐 경우

--- src/Validation/CoachValidation.js
+++ src/Validation/CoachValidation.js
@@ -4,7 +4,8 @@ const CoachValidation = {
   validateCoach(name) {
     const string = this.checkString(name);
     const length = this.checkStringLength(name);
-    const correctName = string || length;
+    const coachLength = this.checkCoachLength(name);
+    const correctName = string || length || coachLength;
     return correctName;
   },
 
@@ -28,6 +29,23 @@ const CoachValidation = {
       for (i = 0; i < nameArray.length; i++) {
         if (nameArray[i].length < 2 || nameArray[i].length > 4)
           throw new Error("[ERROR] 코치 이름은 최소 2글자 ~ 최대 4글자입니다.");
+      }
+    } catch (e) {
+      Console.print(e.message);
+      return true;
+    }
+    return false;
+  },
+
+  checkCoachLength(name) {
+    const nameArray = name.split(",").map((name) => {
+      return name;
+    });
+
+    try {
+      for (i = 0; i < nameArray.length; i++) {
+        if (nameArray.length < 2 || nameArray.length > 5)
+          throw new Error("[ERROR] 코치의 수는 최소 2명 ~ 최대 5명입니다.");
       }
     } catch (e) {
       Console.print(e.message);
