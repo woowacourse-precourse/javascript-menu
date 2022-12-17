35e599c451298a782068936b5a54003550bb444f
ksone02 <eksovus@gmail.com>
2022-12-17 14:13:31 +0900

    feat: 코치 이름 입력 예외처리

--- src/utils/Validator.js
+++ src/utils/Validator.js
@@ -3,7 +3,29 @@ const IO = require('./IO');
 const Utils = require('./Utils');
 
 class Validator {
-  static coachValidate(coaches) {}
+  static coachNameValidate(coach, from, to) {
+    if (coach.length < from || coach.length > to) return true;
+    return false;
+  }
+
+  static coachesNameValidate(coaches) {
+    const coachesArr = Utils.divideString(coaches);
+    coachesArr.forEach((coach) => {
+      if (this.coachNameValidate(coach, 2, 4)) throw ERROR.COACH_NAME;
+    });
+  }
+
+  static coachesNumberValidate(coaches) {
+    const coachesArr = Utils.divideString(coaches);
+    if (coachesArr.length < 2 || coachesArr.length > 5) {
+      throw ERROR.COACH_NUMBER;
+    }
+  }
+
+  static coachValidate(coaches) {
+    this.coachesNumberValidate(coaches);
+    this.coachesNameValidate(coaches);
+  }
 
   static handleException(validateFunction, innerFunction, errorFunction) {
     try {
