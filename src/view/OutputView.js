e43f8c6c95288a87043b1eb2251560519d5cdfc6
sh981013s <sh981013s@gmail.com>
2022-12-17 16:39:09 +0900

    feat(OutputView): 마지막 결과 관련 Output 로직 구현

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -1,5 +1,5 @@
 const { Console } = require("@woowacourse/mission-utils");
-const { COACH_NAME } = require("../constants/Messages");
+const { COACH_NAME, RECOMMENDATION } = require("../constants/Messages");
 
 const OutputVIew = {
   printFirstMessage() {
@@ -8,6 +8,24 @@ const OutputVIew = {
 
   printError(errorLog) {
     Console.print(errorLog.message);
+  },
+
+  printCategory(categories) {
+    Console.print(`[ 카테고리 | ${categories.join(" | ")} ]`);
+  },
+
+  printFoodRecommendations(singleCoach) {
+    Console.print(`[ ${singleCoach.name} | ${singleCoach.recommendedFood.join(" | ")} ]`);
+  },
+
+  printResult(categories, overallInformation) {
+    Console.print(RECOMMENDATION.OPENING);
+    Console.print(RECOMMENDATION.DAYS);
+    this.printCategory(categories);
+    for (const singleCoach of overallInformation) {
+      this.printFoodRecommendations(singleCoach);
+    }
+    Console.print(RECOMMENDATION.CLOSING);
   },
 };
 
