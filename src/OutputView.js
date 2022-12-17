339baa1f809972fe016f96b326acc889f43b466e
하송희 <99heesong@naver.com>
2022-12-17 17:59:40 +0900

    기능추가: 지금까지 한 것

--- src/OutputView.js
+++ src/OutputView.js
@@ -8,6 +8,19 @@ const OutputView = {
   printError() {
     Console.print("[ERROR] 입력 형식을 지켜야합니다.");
   },
+
+  printRecommedResult(){
+    Console.print("메뉴 추천 결과입니다.\n")
+    Console.print([ 구분 | 월요일 | 화요일 | 수요일 | 목요일 | 금요일 ])
+    Console.print([ 카테고리 | ${menu} ])
+    Console.print([ ${name} | ${menu} ])
+    Console.print([ ${name} | ${menu} ])
+    Console.print([ ${name} | ${menu} ])
+  },
+
+  printEndGameMent(){
+    Console.print("추천을 완료했습니다.");
+  }
 };
 
 module.exports = OutputView;
--- src/Validator.js
+++ src/Validator.js
@@ -9,8 +9,9 @@ const Validator = {
     try {
       if (!hangeul) throw Error; //한글과 ,로만 이루어지지 않은 경우
       for (let idx = 0; idx < coachNameArr.length; idx++) {
+        //코치 이름 길이 오류
         if (coachNameArr[idx].length < 2 || coachNameArr[idx].length > 4) throw Error;
-      } //코치 이름 길이 오류
+      }
       if (coachNameArr.length < 2 || coachNameArr.length > 5) throw Error; //코치 인원 오류
       if (nameDuplication.size !== coachNameArr.length) throw Error; //코치 이름 중복 오류
       if (!hangeulStart) throw Error; //한글이 아닌 문자로 시작하는 경우
