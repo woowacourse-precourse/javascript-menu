b79f9e0efa91e19e984dc8f88f7a52b0a1d69b6d
HeoJiye <heoze1031@gmail.com>
2022-12-17 13:26:12 +0900

    feat: 메뉴 추천을 받을 코치의 이름 입력 예외 처리 구현

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -3,8 +3,29 @@ const MissionUtils = require("@woowacourse/mission-utils");
 const InputView = {
     readCoachsName(callback) {
         MissionUtils.Console.readLine('코치의 이름을 입력해주세요. (,로 구분)\n', (answer) => {
-            callback(answer);
+            const coachNameList = answer.split(',');
+            try {
+                this.invaildate_coachNameList(coachNameList);
+            } catch(e) {
+                MissionUtils.Console.print(e);
+                this.readCoachsName(callback);
+                return;
+            }
+            callback(coachNameList);
         });
+    },
+    invaildate_coachNameList(list) {
+        if(list.length < 2 || list.length > 5) {
+            throw "[ERROR] 코치는 최소 2명, 최대 5명까지 식사를 함께합니다.";
+        }
+        list.arrayforEach(name => {
+            this.invaildate_coachName(name)
+        });
+    },
+    invaildate_coachName(name) {
+        if(name.length < 2 || name.length > 4) {
+            throw "[ERROR] 코치의 이름은 최소 2글자 최대 4글자입니다.";
+        }
     }
 }
 
