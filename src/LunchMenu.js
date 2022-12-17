554d05ce7f8b0f0392d13914ed6cf3e0f60c8e8a
wonkyum-kim <wonkyum256@gmail.com>
2022-12-17 13:44:07 +0900

    feat(LunchMenu): input coach names
    - Add coachNameInputHandler

--- src/LunchMenu.js
+++ src/LunchMenu.js
@@ -1,9 +1,21 @@
 const MissionUtils = require('@woowacourse/mission-utils');
+const LunchMenuError = require('./Error/LunchMenuError');
 
 class LunchMenu {
   start() {
     const RECOMMEND_START = '점심 메뉴 추천을 시작합니다.\n';
     MissionUtils.Console.print(RECOMMEND_START);
+    const INPUT_COATCH_NAMES = '코치의 이름을 입력해 주세요. (, 로 구분)\n';
+    MissionUtils.Console.readLine(INPUT_COATCH_NAMES, this.coachNameInputHandler);
+  }
+
+  coachNameInputHandler(names) {
+    try {
+      const coachNames = names.split(',');
+      LunchMenuError.isValidName(coachNames);
+    } catch {
+
+    }
   }
 }
 
