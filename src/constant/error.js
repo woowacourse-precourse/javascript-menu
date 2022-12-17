80fbb2e7f6a82b837352ea365046ee7aa97ba14a
unsnruu <unsnruu@gmail.com>
2022-12-17 14:14:37 +0900

    feat : 사용자에게 출력할 에러메세지를 정리하기

---
+++ src/constant/error.js
@@ -0,0 +1,8 @@
+const error = {
+  TRIPLE_CATEGORY_ERROR: `[ERROR] 같은 카테고리가 3번 넘게 나왔습니다.`,
+  NAME_RANGE_ERROR: `[ERROR] 코치 이름의 글자 수를 확인해 주세요.`,
+  MENU_RANGE_ERROR: `[ERROR] 먹지 못하는 음식은 2개까지 말할 수 있습니다.`,
+  COACH_RANGE_ERROR: `[ERROR] 코치의 수가 너무 적가나 많습니다.`,
+};
+
+module.exports = error;
