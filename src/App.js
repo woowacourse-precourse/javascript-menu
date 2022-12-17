edcf5feec846a081f9eb4aa2ca8582be9edf7a25
jiheon788 <jiheon788@ajou.ac.kr>
2022-12-17 15:07:00 +0900

    feat: 카테고리 랜덤추출

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 const Coach = require("./Coach");
 const InputView = require("./view/InputView");
 const OutputView = require("./view/OutputView");
-const { Console } = require('@woowacourse/mission-utils');
+const { Console, Random } = require('@woowacourse/mission-utils');
 
+const CATEGORIES_MAP = {
+	"1": "일식",
+	"2": "한식",
+	"3": "중식",
+	"4": "아시안",
+	"5": "양식",
+}
 
 class App {
+	#categoriesOfWeek = [];
   #coaches = [];
 	index = 0;
 
@@ -35,26 +43,22 @@ class App {
 	survey() {
 		if(this.index !== this.#coaches.length) {
 			Console.readLine(`${this.#coaches[this.index].getName()}(이)가 못 먹는 메뉴를 입력해 주세요.\n`, (input) => {
-				console.log(this.index)
 				this.#coaches[this.index].setDisabledFood(input);
-	
 				this.index += 1;
-			
 				this.survey();
 			});
 		} else if (this.index === this.#coaches.length) {
-			console.log("end")
+			this.recommendCategory()
 		}
 	}
 
-	// requestDisabledFoods(coach) {
-	// 	InputView.readDisabledFood(coach.getName(), (response) => {
-	// 		coach.setDisabledFood(response);
-	// 	})
-	// }
+	recommendCategory() {
+		for (let i = 0; i < 5; i++) {
+			this.#categoriesOfWeek.push(CATEGORIES_MAP[Random.pickNumberInRange(1, 5)])
+		}
+		OutputView.recommend(this.#categoriesOfWeek)
+	}
 
-
-	
 
 }
 
--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -5,11 +5,12 @@ const OutputView = {
     Console.print('점심 메뉴 추천을 시작합니다.\n');
   },
 
-  recommend() {
+  recommend(categories) {
     Console.print(
     `
 메뉴 추천 결과입니다.
 [ 구분 | 월요일 | 화요일 | 수요일 | 목요일 | 금요일 ]
+[ 카테고리 | ${categories.join(' | ')} ]
     `);
 
   },
