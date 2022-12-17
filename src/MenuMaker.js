d0ad9a1bcc062b8fd4d495285eb1f99648c53410
Adult96 <djxor007@gmail.com>
2022-12-17 15:55:33 +0900

    feat(MenuMaker): 랜덤 숫자 별로 카테고리 선택 메서드 구현

--- src/MenuMaker.js
+++ src/MenuMaker.js
@@ -1,10 +1,33 @@
 const MissionUtils = require('@woowacourse/mission-utils');
 
 class MenuMaker {
-  constructor(SAMPLE, userName) {
+  constructor() {
     this.SAMPLE = {};
     this.userName = [];
   }
+
+  creatMenu() {
+    const selectMenu = this.selectMenu(this.SAMPLE, this.randomCategories());
+  }
+
+  selectMenu(SAMPLE, select) {
+    if (select === 1) {
+      return SAMPLE.일식;
+    }
+    if (select === 2) {
+      return SAMPLE.한식;
+    }
+    if (select === 3) {
+      return SAMPLE.중식;
+    }
+    if (select === 4) {
+      return SAMPLE.아시안;
+    }
+    if (select === 5) {
+      return SAMPLE.양식;
+    }
+  }
+
   setSAMPLE(SAMPLE) {
     return (this.SAMPLE = SAMPLE);
   }
