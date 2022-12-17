6dc6f0b8260ae4182aff0050116a436a874f1d5f
NaveOWO <pianowell96@gmail.com>
2022-12-17 17:44:42 +0900

    [ feat ] 입력값 유효성추가 검증

--- src/InputView.js
+++ src/InputView.js
@@ -7,6 +7,8 @@ const InputView = {
         Console.readLine("코치의 이름을 입력해 주세요. (, 로 구분)\n", (input) => {
             try {
                 Validations.validateLength(input.split(","))
+                Validations.validateCount(input.split(","))
+
                 continueGame(input)
             } catch (e) {
                 Console.print(e)
--- src/Validations.js
+++ src/Validations.js
@@ -3,6 +3,10 @@ const Validations = {
         nameArr.forEach(element => {
             if (element.length<2 || element.length>4)throw new Error("[ERROR]")
         });
+    },
+
+    validateCount(nameArr) {
+        if (nameArr.length < 2 || nameArr.length > 4) throw new Error("[ERROR]")
     }
 }
 
