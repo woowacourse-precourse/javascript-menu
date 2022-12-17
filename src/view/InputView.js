fd134e4fe762cd629ebe0857ef47a153faaefce4
sh981013s <sh981013s@gmail.com>
2022-12-17 14:43:53 +0900

    refactor(InputView, Messages): 못먹는 음식 input 처리하는 view 메서드 구현

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -1,10 +1,16 @@
 const { Console } = require("@woowacourse/mission-utils");
-const { COACH_NAME } = require("../constants/Messages");
+const { COACH_NAME, HATE_FOOD } = require("../constants/Messages");
 
 const InputView = {
   readCoachNameInput(callbackFunction) {
     Console.readLine(COACH_NAME.INPUT, (coachNameInput) => {
       callbackFunction(coachNameInput);
+    });
+  },
+
+  readHateFoodInput(singleCoachName, callbackFunction) {
+    Console.readLine(`\n${singleCoachName}${HATE_FOOD.INPUT}`, (coachNameInput) => {
+      callbackFunction(singleCoachName, coachNameInput);
     });
   },
 };
