b865c99f851482b330cfde5f8dcb6ff0020f9ea7
gyeongza <gyeongza@naver.com>
2022-12-17 16:59:59 +0900

    feat : 입력 받은 코치의 순서별로 못먹는 메뉴를 입력받기

--- src/Controller/Menu.js
+++ src/Controller/Menu.js
@@ -1,5 +1,6 @@
 const CoachValidation = require("../Validation/coachValidation");
 const InputView = require("../View/InputView");
+const { Console } = require("@woowacourse/mission-utils");
 
 class Menu {
   #coachNames;
@@ -13,13 +14,22 @@ class Menu {
   }
 
   handleCoachNames = (name) => {
-    const error = CoachValidation.validateCoach(name)
+    const error = CoachValidation.validateCoach(name);
     if (error) return this.getCoachNames();
     this.#coachNames = name.split(",").map((name) => {
       return name;
     });
-    console.log(this.#coachNames)
+    console.log(this.#coachNames);
+    this.getDislikeMenu(this.#coachNames);
   };
+
+  getDislikeMenu(coachNames) {
+    InputView.getDislikeMenu(coachNames, this.handleDislikeMenu);
+  }
+
+  handleDislikeMenu = (dislikeMenu) => {
+    console.log(dislikeMenu)
+  }
 }
 
 module.exports = Menu;
