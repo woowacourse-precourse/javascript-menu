947f5b403710f6edff47b04fc4810bf4b33fea3f
letsjo <hyunoh.jo@gmail.com>
2022-12-17 16:08:38 +0900

    feat(*): 카테고리별 랜덤 추출 메소드 생성

--- src/repositorys/CategoryRepository.js
+++ src/repositorys/CategoryRepository.js
@@ -1,9 +1,21 @@
 const Category = require('../model/Category');
+const { Random } = require('@woowacourse/mission-utils');
 class CategoryRepository {
-  #categories = new Map();
+  #categoryList = new Map();
+  #categories = [];
 
   addCategory(name, menus) {
-    this.#categories.set(name, new Category(menus));
+    this.#categories.push(name);
+    this.#categoryList.set(name, new Category(menus));
+  }
+
+  getRandomCategory() {
+    const category = this.#categories.get(Random.pickNumberInRange(1, 5));
+    return category;
+  }
+
+  getRandomMenuByCategory(category) {
+    return this.#categoryList.get(category).getRandomMenu();
   }
 }
 
