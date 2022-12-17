820f8c7795de5216068255377e3e900180f9caff
kyw0716 <kyw0716@naver.com>
2022-12-17 17:00:08 +0900

    fix: 예시 출력 형식과 일치하지 않는 부분 수정 => 줄 바꿈 추가

--- src/static/Static.js
+++ src/static/Static.js
@@ -84,7 +84,7 @@ const GuideString = Object.freeze({
   READ_COACH_NAME: "\n코치의 이름을 입력해 주세요. (, 로 구분)\n",
   READ_UNLIKE_MENU: `(이)가 못 먹는 메뉴를 입력해 주세요.\n`,
   START_MENU_RECOMMEND: "점심 메뉴 추천을 시작합니다.\n",
-  RESULT: "메뉴 추천 결과입니다.",
+  RESULT: "\n메뉴 추천 결과입니다.",
   DAYS: "[ 구분 | 월요일 | 화요일 | 수요일 | 목요일 | 금요일 ]",
   END: "\n추천을 완료했습니다.",
 });
