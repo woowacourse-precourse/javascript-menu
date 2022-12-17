0c1b47e1cc708661562244b791c82b8191f021d9
동동이 <drdragon777@naver.com>
2022-12-17 16:05:43 +0900

    refactor - change makeCategory logic(add category once)

--- src/utils/CategoryMaker.js
+++ src/utils/CategoryMaker.js
@@ -1,17 +1,15 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 
 const CategoryMaker = {
-  makeCategory: () => {
-    const categories = [];
-    while (categories.length < 5) {
+  makeCategory: (categories) => {
+    while (1) {
       const randomCategory = MissionUtils.Random.pickNumberInRange(1, 5);
       const isIncluded = categories.includes(randomCategory);
       const isDuplicatedCategory =
         categories.filter((x) => x === randomCategory).length === 2;
       if (isIncluded && isDuplicatedCategory) continue;
-      categories.push(randomCategory);
+      return [...categories, randomCategory];
     }
-    return categories;
   },
 };
 module.exports = CategoryMaker;
