1bfb757934d98c4d3f5a9b4fd37a9871ad534100
rayark1 <max980203@snu.ac.kr>
2022-12-17 15:01:56 +0900

    feat(src/InputView.js): 코치의 이름을 입력 받는 readCoachName() 구현

--- src/InputView.js
+++ src/InputView.js
@@ -2,6 +2,11 @@ const { Console } = require("@woowacourse/mission-utils");
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
-const InputView = {};
+const InputView = {
+    MESSAGE_INPUT_COACH_NAME:'코치의 이름을 입력해 주세요. (, 로 구분)',
+    readCoachName(callback) {
+      Console.readLine(`${InputView.MESSAGE_INPUT_COACH_NAME}`, callback);
+    },
+};
 
 module.exports = InputView;
