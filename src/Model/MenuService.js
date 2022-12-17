c7558e6ce7d269f061856b76409d05d2fc930e92
kangyeongmin <kym6050@hanyang.ac.kr>
2022-12-17 14:52:41 +0900

    feat(MenuService): 입력받은 코치들의 이름이 유효한지(2~5명, 2~4글자) 검사한다

--- src/Model/MenuService.js
+++ src/Model/MenuService.js
@@ -32,6 +32,21 @@ class MenuService {
   setStatus(newStatus) {
     this.#status = newStatus;
   }
+
+  setNames(newNames) {
+    this.validateNames(newNames);
+    this.#names = newNames;
+  }
+
+  validateNames(names) {
+    if (names.length < 2 || names.length > 5)
+      throw new Error("[ERROR] 코치는 2~5명을 입력해야 합니다.");
+
+    names.forEach((name) => {
+      if (name.length < 2 || name.length > 4)
+        throw new Error("[ERROR] 코치의 이름은 2~4글자여야 합니다.");
+    });
+  }
 }
 
 module.exports = MenuService;
