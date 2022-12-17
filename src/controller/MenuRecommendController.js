5f0903d9a625793a07838197bffa3e7002dbacaa
HeoJiye <heoze1031@gmail.com>
2022-12-17 15:39:25 +0900

    feat: controller 생성
    메뉴 추천 프로그램을 진행할 MenuRecommendController 생성

---
+++ src/controller/MenuRecommendController.js
@@ -0,0 +1,14 @@
+const MenuRecommend = require("../model/MenuRecommend");
+const Category = require("../model/Category");
+const Coach = require("../model/Coach");
+
+class MenuRecommendController {
+    #categorys;
+    #coachs;
+
+    constructor(menu) {
+
+    }
+}
+
+module.exports = Category;
