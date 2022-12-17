6d6e29754211665db00a35d408439c623fa594ff
jjo-mi <whdal2636@naver.com>
2022-12-17 14:03:06 +0900

    feat : 2. 코치 이름 받기

--- src/App.js
+++ src/App.js
@@ -10,9 +10,18 @@ const SAMPLE = {
 };
 
 class App {
+  coachs;
   play() {
 	Console.print("점심 메뉴 추천을 시작합니다.");
+	this.inputCreatCoach();
 	
+  }
+
+  inputCreatCoach() {
+	Console.readLine("코치의 이름을 입력해 주세요. (, 로 구분).\n", (names) => {
+		console.log(`이름: ${names}`);
+	  });
+
   }
 }
 
