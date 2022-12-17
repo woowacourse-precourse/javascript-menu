ed36e9c1581972418ab149edb784317a1841654c
sujungYu <su_moment@naver.com>
2022-12-17 17:42:41 +0900

    feat(Constant) : 출력 상수 추가

--- src/util/Constant.js
+++ src/util/Constant.js
@@ -5,19 +5,11 @@ const INPUT_MESSAGE = Object.freeze({
 
 const OUTPUT_MESSAGE = Object.freeze({
   START_TITLE: '점심 메뉴 추천을 시작합니다.\n',
-  RESULT_TITLE: '메뉴 추천 결과입니다.',
+  RESULT_TITLE: '\n메뉴 추천 결과입니다.',
+  RESULT_DAY: '[ 구분 | 월요일 | 화요일 | 수요일 | 목요일 | 금요일]',
   END_TITLE: '추천을 완료했습니다.',
 });
 
-const ERROR_MESSAGE = Object.freeze({
-  IS_EMPTY: '[ERROR] 값을 입력해야 합니다.',
-
-  BRIDGE_SIZE_NOT_NUMBER: '[ERROR] 숫자만 입력해야 합니다.',
-  BRIDGE_SIZE_NOT_RIGHT_RANGE: '[ERROR] 3이상 20이하 숫자를 입력해야 합니다.',
-
-  MOVE_NOT_RIGHT_LETTER: '[ERROR] U or D 를 입력해야 합니다.',
-
-  COMMAND_NOT_RIGHT_LETTER: '[ERROR] Q or R 를 입력해야 합니다.',
-});
+const ERROR_MESSAGE = Object.freeze({});
 
 module.exports = { INPUT_MESSAGE, OUTPUT_MESSAGE, ERROR_MESSAGE };
