708357461f69ddd146319fa5cb2cd83cbe4a9796
동동이 <drdragon777@naver.com>
2022-12-17 17:22:44 +0900

    chore - fix view constants msgs

--- src/view/view.constants.js
+++ src/view/view.constants.js
@@ -1,8 +1,8 @@
 const MESSAGE = Object.freeze({
   SERVICE_START_MESSAGE: "점심 메뉴 추천을 시작합니다.\n",
-  SERVICE_END_MESSAGE: "\n추천을 완료했습니다.",
-  MENU_RECOMMENDATION_RESULT: "\n메뉴 추천 결과입니다.",
-  REQUEST_COACH_NAME: "코치 이름을 입력해 주세요. (, 로 구분)\n",
+  SERVICE_END_MESSAGE: "추천을 완료했습니다.",
+  MENU_RECOMMENDATION_RESULT: "\n메뉴 추천 결과입니다.\n",
+  REQUEST_COACH_NAME: "코치의 이름을 입력해 주세요. (, 로 구분)\n",
   REQUEST_NOT_WANT_MENU: "(이)가 못 먹는 메뉴를 입력해 주세요.\n",
   MENU_WEEK: "[ 구분 | 월요일 | 화요일 | 수요일 | 목요일 | 금요일 ]",
 });
