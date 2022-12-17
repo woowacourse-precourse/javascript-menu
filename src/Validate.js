64873d022d4a3ba2b905860cfe945786ffbfbfcf
powercording <adoim@naver.com>
2022-12-17 17:53:52 +0900

    Refator: 코치 이름 유효성검사조건추가

--- src/Validate.js
+++ src/Validate.js
@@ -11,6 +11,8 @@ class Validate {
         throw new Error(`[Error]${Constant.WRONG_NAME}`);
       if (element.includes(' '))
         throw new Error(`[Error]${Constant.WRONG_NAME}`);
+      if (element.length < 2 || element.length > 4)
+        throw new Error(`[Error]${Constant.WRONG_NAME}`);
     });
   }
 }
