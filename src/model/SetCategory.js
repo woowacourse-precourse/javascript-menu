48f3d52d14375b46c44e6f377aa504eecbb4f24f
SEMNIL5202 <semnil5202@gmail.com>
2022-12-17 15:35:11 +0900

    feat: 요일마다 추천할 카테고리를 무작위로 정하는 기능

---
+++ src/model/SetCategory.js
@@ -0,0 +1,50 @@
+const { Random } = require('@woowacourse/mission-utils');
+
+class setCategory {
+  #category = {
+    1: '일식',
+    2: '한식',
+    3: '중식',
+    4: '아시안',
+    5: '양식',
+  };
+  #counts = [0, 0, 0, 0, 0];
+  #list = [];
+
+  makeCategoryList() {
+    while (this.#list.length !== 5) {
+      this.categoryOfWeak();
+    }
+    return this.NumberToName();
+  }
+
+  categoryOfWeak() {
+    let categoryNumber = this.randomGenerator();
+    if (this.#list.includes(categoryNumber)) this.#counts[categoryNumber - 1] += 1;
+    if (this.isAddToResults(categoryNumber)) this.#list.push(categoryNumber);
+    else this.categoryOfWeak();
+  }
+
+  isAddToResults(categoryNumber) {
+    let booleanResult = false;
+    this.#counts.forEach((number, index) => {
+      if (number === 2 && categoryNumber - 1 === index) booleanResult = false;
+      else booleanResult = true;
+    });
+    return booleanResult;
+  }
+
+  NumberToName() {
+    const results = [];
+    for (let i = 0; i < this.#list.length; i++) {
+      results.push(this.#category[this.#list[i]]);
+    }
+    return results;
+  }
+
+  randomGenerator() {
+    return Random.pickNumberInRange(1, 5);
+  }
+}
+
+module.exports = setCategory;
