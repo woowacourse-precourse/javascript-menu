063df473ead9108ec15d7955acd38edc86601c4a
pakxe <pigkill40@naver.com>
2022-12-17 16:32:26 +0900

    feat(MenuRecommender): 싫어하는 음식과 중복 음식을 제외하고 메뉴를 추천해주는 메서드 작성

--- src/domain/MenuRecommender.js
+++ src/domain/MenuRecommender.js
@@ -1,35 +1,86 @@
 const { Random } = require('@woowacourse/mission-utils');
-const { MENU, WEEK, CATEGORY } = require('../constants/Constants');
+const { MENU, WEEK, CATEGORY, MARK } = require('../constants/Constants');
 
 const MenuRecommender = class {
   recommend(names, hateMenus) {
     const weekCategory = this.#initWeekCategory();
-    const recommendList = {};
-
-    names.reduce((recommendList, name, i) => {
-      recommendList[`${name}`] = this.#personalRecommend(name, hateMenus[i]);
+    return names.reduce((recommendList, name, i) => {
+      recommendList[`${name}`] = this.#personalRecommend(
+        weekCategory,
+        hateMenus[i]
+      );
+      return recommendList;
     }, {});
   }
 
-  #personalRecommend(weekCategory, name, hateMenus) {}
+  #personalRecommend(weekCategory, hateMenus) {
+    const menuArr = Object.entries(MENU).map(([_, category]) =>
+      category.split(`${MARK.rest} `)
+    );
+
+    return weekCategory.reduce((menus, category) => {
+      const categoryMenus = menuArr[category - 1];
+      const numArr = this.#generateArr(categoryMenus.length);
+
+      let menuNumber = Random.shuffle(numArr)[0];
+      let menu = categoryMenus[menuNumber];
+
+      while (1) {
+        if (
+          !this.#isAlreadyEaten(menus, menu) &&
+          !this.#isHateThisFood(hateMenus, menu)
+        ) {
+          menus.push(menu);
+          break;
+        }
+        menuNumber = Random.shuffle(numArr)[0];
+        menu = categoryMenus[menuNumber];
+      }
+
+      return menus;
+    }, []);
+  }
+
+  #isHateThisFood(hateMenus, menu) {
+    return hateMenus.includes(menu);
+  }
+
+  #isAlreadyEaten(menus, menu) {
+    return menus.includes(menu);
+  }
 
   #initWeekCategory() {
     const category = [];
 
     while (category.length < WEEK.size) {
       const selected = Random.pickNumberInRange(1, CATEGORY.size);
-      if (!this.#isAlreadyHave(category, selected)) category.push(selected);
+      if (!this.#isMoreThan2(category, selected)) category.push(selected);
     }
     return category;
   }
 
-  #isAlreadyHave(category, selected) {
+  #isMoreThan2(category, selected) {
     const duplicatedCount = category.filter(
       (already) => already === selected
     ).length;
     return duplicatedCount >= CATEGORY.max;
   }
+
+  #generateArr(size) {
+    return Array(size)
+      .fill(0)
+      .map((_, i) => i);
+  }
 };
 
 const a = new MenuRecommender();
+console.log(
+  a.recommend(
+    ['세현', '상준'],
+    [
+      ['김치찌개', '제육볶음'],
+      ['스파게티', '라자냐'],
+    ]
+  )
+);
 module.exports = MenuRecommender;
