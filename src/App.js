9d66fe0bc837351fcb4007d8840f7278059f0d3f
gabrielyoon7 <gabrielyoon7@gmail.com>
2022-12-17 17:07:00 +0900

    feat: 한 주에 같은 카테고리가 최대 2회만 나오도록 카테고리 추천 및 설정

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
+const MissionUtils = require('@woowacourse/mission-utils');
 const { Coach } = require('./models/Coach');
 const { findIndexByCoachName } = require('./utils/common');
+const { SAMPLE } = require('./utils/constants');
 const { readCoaches, readBannedFoods } = require('./views/InputView');
 const { OutputView } = require('./views/OutputView');
 
 class App {
   #coaches = [];
 
+  #categories = [];
+
   play() {
+    this.setCategories();
     OutputView.welcome();
     readCoaches(this.setCoaches.bind(this));
+  }
+
+  setCategories() {
+    const numbers = [];
+    while (numbers.length !== 5) {
+      const pick = MissionUtils.Random.pickNumberInRange(1, 5);
+      if (numbers.filter((number) => number === pick).length < 2) {
+        numbers.push(pick);
+      }
+    }
+    const category = Object.keys(SAMPLE);
+    this.#categories = numbers.map((number) => category[number - 1]);
   }
 
   setCoaches(answer) {
@@ -28,7 +45,11 @@ class App {
     arr.forEach((el) => {
       this.#coaches[findIndexByCoachName(this.#coaches, el.name)].setBannedFoods(el.foods);
     });
-    this.#coaches.map((c) => console.log(c.getBannedFoods()));
+    this.recommendFoods();
+  }
+
+  recommendFoods() {
+    console.log('추천 시작한다');
   }
 }
 
