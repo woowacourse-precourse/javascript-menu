8fce369c855d8b808ad57183ded4ad7b557fc6ef
SEMNIL5202 <semnil5202@gmail.com>
2022-12-17 15:06:34 +0900

    fix: 각 코치마다 메뉴 입력을 못하는 오류 수정

--- src/controller/Controller.js
+++ src/controller/Controller.js
@@ -2,6 +2,7 @@ const InputView = require('../view/InputView');
 const OuputView = require('../view/OutputView');
 
 class Controller {
+  #index = 0;
   #coachs;
   #notEatList = [];
 
@@ -19,27 +20,32 @@ class Controller {
   #checkCoach(input) {
     try {
       this.#coachs = input.split(',');
-      this.#checkCoachNotEat();
+      this.#checkCoachList();
+      console.log(this.#coachs);
     } catch (error) {}
   }
 
-  #checkCoachNotEat() {
-    this.#coachs.forEach((coachName) => {
-      this.#inputNotEat(coachName);
-    });
+  #checkCoachList() {
+    this.#inputNotEat(this.#coachs[this.#index]);
   }
 
   #inputNotEat(coachName) {
     InputView.notEat(coachName, (input) => {
       this.#checkNotEat(coachName, input);
+      this.#index += 1;
+      if (this.#index === this.#coachs.length) this.#newMethod();
+      else this.#checkCoachList();
     });
   }
 
   #checkNotEat(coachName, input) {
     try {
       this.#notEatList.push([coachName, input]);
+      console.log(this.#notEatList);
     } catch (error) {}
   }
+
+  #newMethod() {}
 }
 
 module.exports = Controller;
