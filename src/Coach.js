30bb5717cf28e2ba4db5bd133b23441bf6dff17e
tkdrb12 <tkdrb15@gmail.com>
2022-12-17 13:24:18 +0900

    feat : 코치 클래스를 정의하고 코치의 이름과 못먹는 메뉴를 가져오는 기능 추가

---
+++ src/Coach.js
@@ -0,0 +1,20 @@
+class Coach{
+    #name;
+    #menus;
+
+    constructor(name){
+        this.#name = name;
+    }
+
+    setMenu(menus){
+        this.menuValidate(menus);
+    }
+
+    getMenu(){
+        return this.#menus;
+    }
+
+    getName(){
+        return this.#name;
+    }
+}
