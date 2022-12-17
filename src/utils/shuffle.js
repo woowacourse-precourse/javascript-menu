ca3aa964c6304c27917a5e6653a7a7c410f5fd9a
evencoding <evencoding@gmail.com>
2022-12-17 17:57:29 +0900

    fix: shuffle은 1부터 받도록 수정

--- src/utils/shuffle.js
+++ src/utils/shuffle.js
@@ -1,9 +1,10 @@
 const { Random } = require('@woowacourse/mission-utils');
 
 const shuffle = (args) => {
-  const array = Array.from({ length: args.length }).map((_, index) => index);
+  const array = Array.from({ length: args.length }).map((_, index) => index + 1);
   const randoms = Random.shuffle(array);
-  const shuffledOrigin = args.map((_, index, origin) => origin[randoms[index]]);
+
+  const shuffledOrigin = args.map((_, index, origin) => origin[randoms[index + 1]]);
 
   return shuffledOrigin;
 };
