5b99a47f622e9f426fa0d51558eba6d549aacf1c
hafnium1923 <hafnium1923@gmail.com>
2022-12-17 16:06:38 +0900

    feat coahNameError 처리추가

--- src/ErrorHandler.js
+++ src/ErrorHandler.js
@@ -5,10 +5,11 @@ const ErrorHandler = {
      */
     coachNameError(coach) {
         if(coach.length < 2 || coach > 5) throw '\n[ERROR] 코치는 최소 2명 최대 5명까지 입력 가능합니다.';
-        for(let i = 0; i < coach.length; i++){
-            if(coach[i].length < 2 || 4 < coach[i].length)
-                throw '\n[ERROR] 코치 이름은 최소 2글자 최대 4글자여야 합니다.';
+        for(let i = 0; i < coach.length; i++) {
+            if(coach[i].length < 2 || 4 < coach[i].length) throw '\n[ERROR] 코치 이름은 최소 2글자 최대 4글자여야 합니다.';
         }
+        const nameSet = new Set(coach);
+        if(nameSet.size != coach.length) throw '\n[ERROR] 코치이름은 중복될 수 없습니다.'
     },
     /**
      * @param {String[]} menu : 싫은 음식 배열
