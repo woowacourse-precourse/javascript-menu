9ec8e9086b7ea80dccfd7875b27e3a48d00e771f
nangkyeonglim <nangkyeonglim@gmail.com>
2022-12-17 13:17:31 +0900

    feat(InputView): 싫어 하는 메뉴 입력받는 기능 구현

--- src/views/InputView.js
+++ src/views/InputView.js
@@ -4,6 +4,10 @@ const InputView = {
   readCoachNames(callback) {
     Console.readLine('코치 이름을 입력해 주세요. (, 로 구분)\n', callback);
   },
+
+  readDislikeMenu(name, callback) {
+    Console.readLine(`${name}(이)가 못 먹는 메뉴를 입력해 주세요.`, callback);
+  },
 };
 
 module.exports = InputView;
