3a8dc82f4d3428cdb5877f0e8555e239d8a3da6e
useon <yoosk4986@naver.com>
2022-12-17 16:26:46 +0900

    refactor: 랜덤 카테고리 생성 로직을 추가한다

--- src/MenuRecommend.js
+++ src/MenuRecommend.js
@@ -1,6 +1,7 @@
 const { Random } = require('@woowacourse/mission-utils');
 
 class MenuRecommend {
+  #category = [];
   MENU = {
     일식: '규동, 우동, 미소시루, 스시, 가츠동, 오니기리, 하이라이스, 라멘, 오코노미야끼',
     한식: '김밥, 김치찌개, 쌈밥, 된장찌개, 비빔밥, 칼국수, 불고기, 떡볶이, 제육볶음',
@@ -20,7 +21,14 @@ class MenuRecommend {
       }
       if (count < 2) categoryArr.push(randomNumber);
     }
+    categoryArr.map((number, index) => {
+      categoryArr[index] = categories[number - 1];
+    });
+    this.#category = categoryArr;
+    return this.#category;
   }
+
+  createMenu() {}
 }
 
 module.exports = MenuRecommend;
