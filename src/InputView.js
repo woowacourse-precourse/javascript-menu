67ec1950393aecbd8a402878349661cb10b4845e
ashleysyheo <ashleysyheo@gmail.com>
2022-12-17 13:46:30 +0900

    feat: read coach's inedible menus

--- src/InputView.js
+++ src/InputView.js
@@ -11,7 +11,19 @@ const InputView = {
         return this.readCoaches();
       }
 
-      // 각 코치들이 먹지 못하는 메뉴 입력받기 
+      // 각 코치들이 먹지 못하는 메뉴 입력받기
+    });
+  },
+
+  readInedibleMenus() {
+    MissionUtils.Console.readLine(CONSOLE_MESSAGE.inedibleMenuInput, (input) => {
+      try {
+        // check exception;
+      } catch (error) {
+        return this.readInedibleMenus();
+      }
+
+      // 각 요일별 메뉴 생성하기 
     });
   },
 };
