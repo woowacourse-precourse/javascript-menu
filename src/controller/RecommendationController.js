2a984903b6449bad443d641979b0d3c2e39c562e
sh981013s <sh981013s@gmail.com>
2022-12-17 15:29:00 +0900

    refactor(ReccomendationController): 코치 생성 메서드 분리

--- src/controller/RecommendationController.js
+++ src/controller/RecommendationController.js
@@ -10,16 +10,21 @@ class RecommendationController {
     this.#recommendationModel = new RecommendationModel();
   }
 
+  makeAnotherCoach(validCoachNames, singleCoachName) {
+    this.#mainController.readHateFoodInput(
+      validCoachNames,
+      validCoachNames[validCoachNames.indexOf(singleCoachName) + 1],
+    );
+  }
+
   processHateFoodInput(validCoachNames, singleCoachName, hateFoodInput) {
     try {
       HateFoodInputValidation.validate(hateFoodInput);
       this.#recommendationModel.generateCoach(singleCoachName, hateFoodInput.split(","));
       if (validCoachNames.indexOf(singleCoachName) !== validCoachNames.length - 1) {
-        this.#mainController.readHateFoodInput(
-          validCoachNames,
-          validCoachNames[validCoachNames.indexOf(singleCoachName) + 1],
-        );
+        this.makeAnotherCoach(validCoachNames, singleCoachName);
       }
+      this.#mainController.generateRecommendatations();
     } catch (errorLog) {
       this.#mainController.printError(errorLog);
       this.#mainController.readHateFoodInput(validCoachNames, singleCoachName);
