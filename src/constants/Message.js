13242583a0a9f0e3abe314cdb8d4cf813c0e7775
letsjo <hyunoh.jo@gmail.com>
2022-12-17 13:35:13 +0900

    feat(*): 메세지 상수 파일 생성

---
+++ src/constants/Message.js
@@ -0,0 +1,14 @@
+const deepFreeze = require('../utils/deepFreeze');
+const { PROCESS_CONSTANTS } = require('./Setting');
+
+const MESSAGE = {
+  process: {
+    startNotice: '점심 메뉴 추천를 시작합니다.',
+    quitNotice: '추천을 완료했습니다.',
+  },
+
+  error: {
+  },
+};
+
+module.exports = MESSAGE;
