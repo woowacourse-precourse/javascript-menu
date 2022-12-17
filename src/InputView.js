441a495c08075e4786a532667fe6730aa363f7ec
rayark1 <max980203@snu.ac.kr>
2022-12-17 14:31:02 +0900

    chore(*): I/O 관련 파일에 제공되는 mission-utils Console import

--- src/InputView.js
+++ src/InputView.js
@@ -1,3 +1,4 @@
+const { Console } = require("@woowacourse/mission-utils");
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
--- src/OutputView.js
+++ src/OutputView.js
@@ -1,3 +1,4 @@
+const { Console } = require("@woowacourse/mission-utils");
 /**
  * 사용자에게 메뉴 추천 프로그램의 진행 상황과 결과를 출력하는 역할을 한다.
  */
