438ce96f9537fe706f7376df107d9a07cce3ad10
daal2quf <daal2quf@gmail.com>
2022-12-17 16:46:46 +0900

    feat(validation): 예외처리 조건문에 대한 추상화 레벨 맞추기
    add:
    isCorrectLength(): 2~4사이의 글자수일떄 ture

--- src/controller/Validation.js
+++ src/controller/Validation.js
@@ -2,7 +2,7 @@ const { MenuMap } = require('../domain/MenuMap');
 
 const Validation = {
   ofCoachName(userInput) {
-    if (!userInput.split(',').every((name) => name.length >= 2 && name.length <= 4)) {
+    if (!isCorrectLength(userInput)) {
       throw new Error('[ERROR] 코치의 이름은 2글자 이상 4글자 이하로만 입력해주세요');
     }
     if (userInput.split(',').length < 2 || userInput.split(',').length > 5) {
@@ -15,6 +15,10 @@ const Validation = {
     if (!userInput.split(',').every((food) => menu.includes(food)))
       throw new Error('[ERROR] 메뉴에 없는 메뉴입니다.');
   },
+};
+
+const isCorrectLength = (userInput) => {
+  return userInput.split(',').every((name) => name.length >= 2 && name.length <= 4);
 };
 
 module.exports = { Validation };
