03713d3730f42631bf914c042a2de3eca1a3bad4
HeoJiye <heoze1031@gmail.com>
2022-12-17 13:50:35 +0900

    feat: 카테코기 출력 구현

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -8,12 +8,17 @@ const OutputView = {
     printEnding() {
         PrintRecommandResult();
     },
-    printRecommandResult() {
+    printRecommandResult(categorys) {
         MissionUtils.Console.print('메뉴 추천 결과입니다.');
         printClass();
+        MissionUtils.Console.print(this.categorysToString(categorys));
     },
     printClass() {
         MissionUtils.Console.print('[ 구분 | 월요일 | 화요일 | 수요일 | 목요일 | 금요일 ]');
-    }
+    },
+    categorysToString(categorys) {
+        const content = ['카테코리', ...categorys];
+        return '[ ' + content.join(' | ') + ' ]';
+    },
 };
 module.exports = OutputView;
