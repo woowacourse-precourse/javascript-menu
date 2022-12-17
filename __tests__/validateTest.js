a9260e7520e1fa9d87ed34a3ed0bb5eaa26e2e3d
turtle601 <turtle601@naver.com>
2022-12-17 15:16:20 +0900

    fix(validate) 테스트 텍스트 수정

--- __tests__/validateTest.js
+++ __tests__/validateTest.js
@@ -1,11 +1,11 @@
 const InputException = require('../src/utils/InputException');
 
-describe('(잔돈) InputException 입력값 테스트', () => {
+describe('(코치) InputException 입력값 테스트', () => {
   test.each([['토미,제임스,포코,']])('(실패) 숫자 입력값 확인', (input) => {
     expect(() => InputException.checkCoach(input)).toThrow('[ERROR]');
   });
 
-  test('(성공) 재시작/종료 입력값 확인', () => {
+  test('(성공) 입력값', () => {
     const input = '토미,제임스,포코';
     expect(() => InputException.checkCoach(input)).not.toThrow();
   });
