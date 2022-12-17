9345b1c887688c1fb8d482dd1b8e6eeaf4212a00
ReturnReason <reasonz@daum.net>
2022-12-17 17:00:36 +0900

    feat(MenuController): 추천 메뉴를 만드는 메서드 추가

--- src/domain/MenuController.js
+++ src/domain/MenuController.js
@@ -8,6 +8,8 @@ const OutputView = require('../view/OutputView');
 class MenuController {
   #coachMembers;
   #unableToEatMenu = [];
+  #weekCategory = [];
+  #weekMenu = [];
 
   constructor() {
     this.validator = new Validator();
@@ -22,13 +24,7 @@ class MenuController {
     const callback = (coachName) => {
       try {
         this.#coachMembers = coachName.split(',');
-        console.log(this.#coachMembers);
-
-        this.validator.checkCoachMemberCount(this.#coachMembers.length);
-        this.validator.checkCoachNameLength(this.#coachMembers);
-        // 예외처리 : 코치 이름 2글자 ~ 4글자가 아닌 경우
-        // 예외처리 : 코치 2명 이상, 5명 이하가 아닌 경우
-        // 예외처리 : 콤마로 구분하지 않는 경우
+        this.validateCoachName();
         this.getUnableToEatMenu();
       } catch (error) {
         OutputView.printError(error);
@@ -39,13 +35,22 @@ class MenuController {
     InputView.readCoachName(callback);
   }
 
+  validateCoachName() {
+    this.validator.checkCoachMemberCount(this.#coachMembers.length);
+    this.validator.checkCoachNameLength(this.#coachMembers);
+    this.validator.checkSeperator(this.#coachMembers);
+  }
+
   getUnableToEatMenu() {
     if (this.#coachMembers.length === 2) {
       InputView.readCoachUnableEatMenu(this.#coachMembers[0], (menus) => {
         this.#unableToEatMenu.push({ [this.#coachMembers[0]]: menus.split(',') });
         InputView.readCoachUnableEatMenu(this.#coachMembers[1], (menus) => {
           this.#unableToEatMenu.push({ [this.#coachMembers[1]]: menus.split(',') });
-          console.log(this.#unableToEatMenu);
+          for (let i = 0; i < 5; i++) {
+            this.createRecommendedMenu();
+          }
+          this.printResult();
         });
       });
     }
@@ -95,6 +100,30 @@ class MenuController {
         });
       });
     }
+  }
+
+  createRecommendedMenu() {
+    const categoryNumber = pickNumberInRange(1, 5);
+    // 1이면 일식, 2면 한식, 3이면 중식, 4면 아시안, 5면 양식
+    const menu = menuList[categoryNumber - 1];
+
+    let category = '';
+    let menuItemList = '';
+    Object.entries(menu).forEach(([todayCategory, menuItem]) => {
+      category = todayCategory;
+      menuItemList = menuItem;
+    });
+
+    this.#weekCategory.push(category);
+
+    // 셔플에서 뽑은 인덱스에 해당하는 메뉴가 추천 메뉴가된다.
+    const randomIndex = shuffle([0, 1, 2, 3, 4, 5, 6, 7, 8]);
+    this.#weekMenu.push(menuItemList[randomIndex]);
+  }
+
+  // 결과를 출력시키는 메서드
+  printResult() {
+    OutputView.printResult(this.#coachMembers[0], this.#weekCategory, this.#weekMenu);
   }
 }
 
