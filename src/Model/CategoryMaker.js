e1715451a7e02b88df10a70a6d3cd484d650d427
Jisoo Yoo <youzysu@gmail.com>
2022-12-17 14:10:06 +0900

    feat: 한 코치의 요일별 추천 카테고리를 만든다.

---
+++ src/Model/CategoryMaker.js
@@ -0,0 +1,23 @@
+const { Random } = require('@woowacourse/mission-utils');
+
+const CategoryMaker = {
+  RANDOM_LOWER_INCLUSIVE: 1,
+  RANDOM_UPPER_INCLUSIVE: 5,
+
+  makeCategory() {
+    const category = [];
+    while (category.length < 5) {
+      const randomNumber = this.generateRandomNumber();
+      const filtered = category.filter((item) => item === randomNumber);
+      if (filtered.length > 1) continue;
+      category.push(randomNumber);
+    }
+    return category;
+  },
+
+  generateRandomNumber() {
+    return Random.pickNumberInRange(CategoryMaker.RANDOM_LOWER_INCLUSIVE, CategoryMaker.RANDOM_UPPER_INCLUSIVE);
+  },
+};
+
+module.exports = CategoryMaker;
