5164ca8d412defd8acd15c668c05723d023e5276
chsua <csj1919@naver.com>
2022-12-17 16:41:22 +0900

    Feat: callback 함수 및 카테고리 고르는 함수 구현

--- src/App.js
+++ src/App.js
@@ -5,25 +5,48 @@ const Make = require("./Random")
 
 class App {
 	#weekCategory
+	#coachs
 
 	constructor() {
 		this.#weekCategory = [];
+		this.#coachs = [];
 	}
 
 	play() {
+		Output.start();
+		Input.requestName(this.requestNameCallback.bind(this));
+	}
+
+	requestNameCallback(names) {  	//["이름","이름"...]
+		names.forEach(name => { this.#coachs.push(new Coach(name)) })
+		const name = this.#coachs[0].name()
+		Input.requestNotEat(name, 0, this.requestNotEatCallbackVal.bind(this))
+	}
+	requestNotEatCallbackVal(i, foods) {
+		this.#coachs[i].isNotEat(foods);
+		const name = this.#coachs[i + 1].name()
+		if (i + 2 < this.#coachs.length) {
+			Input.requestNotEat(name, i + 1, this.requestNotEatCallbackVal.bind(this))
+		} else {
+			Input.requestNotEat(name, i + 1, this.requestNotEatCallback.bind(this))
+		}
+	}
+
+	requestNotEatCallback(i, foods) {
+		this.#coachs[i].isNotEat(foods);
+		this.checkOverlapCategory();
 
 	}
 
-	requestNameCallback(names) {
-
-	}
-
-	requestNotEatCallback(names) {
-
-	}
-
-	checkOverlapCategory(category) {
-
+	checkOverlapCategory() {
+		while (this.#weekCategory.length !== 5) {
+			const foodStyle = Make.makeCategory();
+			let count = 0;
+			this.#weekCategory.forEach(x => {
+				if (foodStyle === x) count++
+			})
+			if (count < 3) this.#weekCategory.push(foodStyle);
+		}
 	}
 
 	CheckCanEatFood(food) {
