7199d51fb8d4aa6b6deac4faafab7b1cfa557849
rayark1 <max980203@snu.ac.kr>
2022-12-17 14:45:41 +0900

    feat(src/OutputView.js): 프로그램 시작, 종료 메세지를 출력하는 printRecommendStartMessage(), printRecommendEndMessage() 구현

--- src/OutputView.js
+++ src/OutputView.js
@@ -2,6 +2,15 @@ const { Console } = require("@woowacourse/mission-utils");
 /**
  * 사용자에게 메뉴 추천 프로그램의 진행 상황과 결과를 출력하는 역할을 한다.
  */
-const OutputView = {};
+const OutputView = {
+  MESSAGE_START_LUNCH_MENU_RECOMMEND: "점심 메뉴 추천을 시작합니다.",
+  MESSAGE_END_LUNCH_MENU_RECOMMEND: "추천을 완료했습니다.",
+  printRecommendStartMessage() {
+    Console.print(`${OutputView.MESSAGE_START_LUNCH_MENU_RECOMMEND}`);
+  },
+  printRecommendEndMessage() {
+    Console.print(`${OutputView.MESSAGE_END_LUNCH_MENU_RECOMMEND}`);
+  },
+};
 
 module.exports = OutputView;
