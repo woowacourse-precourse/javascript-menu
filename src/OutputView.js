f0a9cae46487d61d022e031649d6a4bcb4a47d6a
haesa <xdhaesa@gmail.com>
2022-12-17 17:38:13 +0900

    fix: OutputView에서 메뉴 출력 함수의 인자 변경

--- src/OutputView.js
+++ src/OutputView.js
@@ -8,8 +8,8 @@ class OutputView {
     Console.print(SERVICE_MESSAGE.RESULT);
     Console.print(SERVICE_MESSAGE.DAYS);
     Console.print(SERVICE_MESSAGE.CATEGORY(categories));
-    names.forEaach((name, index) =>
-      Console.print(SERVICE_MESSAGE.MENU(name, recommandMenus[index]))
+    recommandMenus.forEach((menus, index) =>
+      Console.print(SERVICE_MESSAGE.MENU(names[index], menus))
     );
     Console.print(SERVICE_MESSAGE.FINISH);
   }
