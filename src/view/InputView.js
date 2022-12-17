81d74919f18dbf40ca840136c39c0e330a3b147d
letsjo <hyunoh.jo@gmail.com>
2022-12-17 14:27:07 +0900

    feat(InputView): coach 이름 입력값 받기 구현

---
+++ src/view/InputView.js
@@ -0,0 +1,37 @@
+const { Console } = require('@woowacourse/mission-utils');
+const OutputView = require('./OutputView');
+const MESSAGE = require('../constants/Message');
+
+const InputView = {
+  readCoach(callback) {
+    InputView.question(MESSAGE.process.inputCoachName, input => {
+      const names = InputView.convertInputToItems(input);
+
+      names.forEach(name => InputView.validateName(name));
+      callback(names);
+    });
+  },
+
+  question(string, callback) {
+    Console.readLine(string, input => {
+      try {
+        callback(input);
+      } catch (error) {
+        OutputView.printError(error);
+        InputView.question(string, callback);
+      }
+    });
+  },
+
+  validateName(name) {
+    if (name.length < 2 || name.length > 4) {
+      throw new Error('이름은 최소 2글자, 최대 4글자여야 합니다.');
+    }
+  },
+
+  convertInputToItems(input) {
+    return input.split(',');
+  },
+};
+
+module.exports = InputView;
