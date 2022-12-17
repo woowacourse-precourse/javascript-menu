33d490efc12b2548559d1b26a6c318fae7140662
wzrabbit <curious.wzrabbit@gmail.com>
2022-12-17 15:43:24 +0900

    feat(menuchooser): 랜덤 메뉴를 하나 반환하는 기능 구현

--- src/model/MenuChooser.js
+++ src/model/MenuChooser.js
@@ -10,6 +10,22 @@ class MenuChooser {
       this.#shuffler.push(index);
     }
   }
+
+  #chooseOnce(category, banMenus) {
+    let pickedMenu;
+
+    while (true) {
+      pickedMenu = this.#getRandomMenuOnce(category);
+      if (!this.#chosenMenus.includes(pickedMenu) && !banMenus.includes(pickedMenu)) {
+        break;
+      }
+    }
+    this.#chosenMenus.push(pickedMenu);
+  }
+
+  #getRandomMenuOnce(category) {
+    return MENUS[category][Random.shuffle(this.#shuffler)[0]];
+  }
 }
 
 module.exports = MenuChooser;
