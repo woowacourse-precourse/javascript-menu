dd34216b6646512e03be62f489edb878ae3b5982
devMingu <rooster100@naver.com>
2022-12-17 13:37:35 +0900

    feat: 메세지 추가

---
+++ src/Message.js
@@ -0,0 +1,15 @@
+const START_MESSAGE = {
+    START: '점심 메뉴 추천을 시작합니다.',
+}
+
+const INPUT_MESSAGE = {
+    INPUT_MEMBER: '코치의 이름을 입력해 주세요. (, 로 구분)',
+    INPUT_NOT_EAT: '(이)가 못 먹는 메뉴를 입력해 주세요.',
+}
+
+const OUTPUT_MESSAGE = {
+    MENU_RESULT: '메뉴 추천 결과입니다.',
+    MENU_FINISH: '추천을 완료했습니다.',
+}
+
+module.exports = {START_MESSAGE, INPUT_MESSAGE, OUTPUT_MESSAGE};
