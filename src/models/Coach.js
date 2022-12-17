97b2990171a2223338342a4f2b7a025901ea5658
nangkyeonglim <nangkyeonglim@gmail.com>
2022-12-17 14:39:35 +0900

    feat(Coach): 코치들의 싫어하는 메뉴 등록 메소드 작성

--- src/models/Coach.js
+++ src/models/Coach.js
@@ -6,6 +6,14 @@ class Coach {
   constructor(name) {
     this.#name = name;
   }
+
+  setDislikeMenu(menu) {
+    this.#dislikeMenu = menu.split(',');
+  }
+
+  getName() {
+    return this.#name;
+  }
 }
 
 module.exports = Coach;
