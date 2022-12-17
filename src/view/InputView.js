af12d42cdc24f2a4b5c1564d29482933783a6ea5
daal2quf <daal2quf@gmail.com>
2022-12-17 13:25:47 +0900

    feat(inputView): 코치 이름 입력 받는 기능 추가
    add:
    readCoachName(): 코치의 이름을 입력받음

---
+++ src/view/InputView.js
@@ -0,0 +1,9 @@
+const { Console } = require('@woowacourse/mission-utils');
+
+const InputView = {
+  readCoachName(callback) {
+    Console.readLine('코치의 이름을 입력해 주세요. (, 로 구분)', callback);
+  },
+};
+
+module.exports = { InputView };
