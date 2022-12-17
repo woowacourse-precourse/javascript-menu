08732ff48efed191bc2af57a0f70c1157c8effba
wzrabbit <curious.wzrabbit@gmail.com>
2022-12-17 16:03:19 +0900

    feat(outputview): 최종 추천 결과를 출력하는 기능 구현

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -6,6 +6,19 @@ const OutputView = {
   printMessage(message) {
     Console.print(message);
   },
+
+  printResult(result) {
+    const categories = result.categories.map((category) => CATEGORY_NAME[category]);
+    Console.print(
+      Trimmer.templateTrim(`
+      [ 구분 | 월요일 | 화요일 | 수요일 | 목요일 | 금요일 ]
+      [ 카테고리 | ${categories.join(' | ')}]`)
+    );
+
+    result.pickResult.forEach((currentData) => {
+      Console.print(`[ ${currentData.join(' | ')} ]`);
+    });
+  },
 };
 
 module.exports = OutputView;
