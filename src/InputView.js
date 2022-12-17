799b4563811a7dff99efb27e17e03d9f137a8758
NaveOWO <pianowell96@gmail.com>
2022-12-17 13:20:03 +0900

    [ feat ] 코치가 못먹는 메뉴를 입력받는 기능

--- src/InputView.js
+++ src/InputView.js
@@ -3,10 +3,16 @@ const {Console} = require("@woowacourse/mission-utils")
 const InputView = {
 
     inputCoachName(continueGame) {
-        Console.readLine("코치의 이름을 입력해 주세요. (, 로 구분)", (input) => {
+        Console.readLine("코치의 이름을 입력해 주세요. (, 로 구분)\n", (input) => {
             continueGame(input)
         })
-    }
+    },
+
+    inputNotEat(continueGame) {
+        Console.readLine("", (input) => {
+            continueGame(input)
+        })
+    },
 
 
 }
