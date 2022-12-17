5c7d07e244fbd14e1c322b6d44371d44f587443a
curomame <curomame@naver.com>
2022-12-17 13:58:44 +0900

    ➕ feat - <OutputView> [printStartMessage] add
    점심 메뉴 추천 서비스 시작을 알리는 메세지 출력 기능 구현

---
+++ src/view/OutputView.js
@@ -0,0 +1,12 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
+const OutputView = {
+
+  printStartMessage() {
+    MissionUtils.Console.print("점심 메뉴 추천을 시작합니다.\n")
+  },
+
+};
+
+
+module.exports = OutputView;
