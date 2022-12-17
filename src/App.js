9af288e43b786c187175204478b9de019bf33463
2yunseong <dbsdltjd3701@naver.com>
2022-12-17 14:34:40 +0900

    feat(app): 못 먹는 음식 입력받는 기능

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ const InputUI = require('./view/InputUI');
 const Vaildator = require('./util/vaildator');
 const Coach = require('./Coach');
 const InputProcessor = require('./util/InputProcessor');
+const Food = require('./Food');
+const MENUS = require('./constant/menus');
+const MenuUtil = require('./util/menuUtil');
 
 class App {
   constructor() {
     this.output = new OutputUI();
     this.input = new InputUI();
     this.coachs = [];
+    this.dislikeIdx = 0;
   }
   play() {
     this.output.print('점심 메뉴 추천을 시작합니다.');
@@ -32,6 +36,30 @@ class App {
     coachNames.forEach((coachname) => {
       this.coachs.push(new Coach(coachname));
     });
+    this.inputCoachsDislikeFood();
+  }
+  inputCoachsDislikeFood() {
+    if (this.dislikeIdx >= this.coachs.length - 1) {
+      // 메뉴 추천 결과 실행 로직
+    }
+    this.input.readLine(
+      this.inputDislikeFoodByCoach.bind(this),
+      `${this.coachs[this.dislikeIdx].name}(이)가 못 먹는 메뉴를 입력해 주세요.`
+    );
+  }
+  inputDislikeFoodByCoach(input) {
+    const disLikeFoods = InputProcessor.parseCommaStringsToArray(input);
+
+    // 개수가 올바른지 판단하기
+
+    // 올바르다면
+    disLikeFoods.forEach((food) => {
+      const category = MenuUtil.findCategoryByFoodName(food);
+      this.coachs[this.dislikeIdx].push(new Food(food, category));
+    });
+
+    this.dislikeIdx++;
+    this.inputCoachsDislikeFood();
   }
 }
 
