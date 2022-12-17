5d81d3283bee3708e8ecaef1ade70d6c06e21091
hafnium1923 <hafnium1923@gmail.com>
2022-12-17 16:42:50 +0900

    test 카테고리 중복 테스트

--- __tests__/MenuTest.js
+++ __tests__/MenuTest.js
@@ -48,7 +48,7 @@ describe('에러처리 테스트', () => {
 		jest.clearAllMocks();
 	});
 
-	describe('코치 이름 에러처리', () => {
+	describe('에러처리 테스트', () => {
 		test('2명 미만의 코치 입력', () => {
 			const logSpy = getLogSpy();
 			mockQuestions(['구구']);
@@ -88,6 +88,50 @@ describe('에러처리 테스트', () => {
 
 			const log = getOutput(logSpy);
             expectLogContains(log, ["[ERROR]"]);
+		});
+	});
+
+    describe('전체 기능 테스트', () => {
+		test('카테고리 메뉴 중복 있는 추천', () => {
+			const logSpy = getLogSpy();
+
+			mockRandoms([2, 5, 2, 3, 4]);
+			mockQuestions(['구구,제임스', '김밥', '떡볶이']);
+
+			const sequenced = (_, idx) => idx + 1;
+			mockShuffles([
+				// 구구
+				[2, Array.from({ length: 9 }, sequenced)],
+				[7, Array.from({ length: 9 }, sequenced)],
+				[3, Array.from({ length: 9 }, sequenced)],
+				[4, Array.from({ length: 9 }, sequenced)],
+				[2, Array.from({ length: 9 }, sequenced)],
+
+				//제임스
+				[9, Array.from({ length: 9 }, sequenced)],
+				[1, Array.from({ length: 9 }, sequenced)],
+				[5, Array.from({ length: 9 }, sequenced)],
+				[5, Array.from({ length: 9 }, sequenced)],
+				[4, Array.from({ length: 9 }, sequenced)],
+			]);
+
+			const app = new App();
+			app.play();
+			const log = getOutput(logSpy);
+
+			expect(log.replace(/\n/g, '')).toEqual(
+				expect.stringContaining(
+					[
+						'점심 메뉴 추천을 시작합니다.',
+						'메뉴 추천 결과입니다.',
+						'[ 구분 | 월요일 | 화요일 | 수요일 | 목요일 | 금요일 ]',
+						'[ 카테고리 | 한식 | 양식 | 한식 | 중식 | 아시안 ]',
+						'[ 구구 | 김치찌개 | 스파게티 | 쌈밥 | 짜장면 | 카오 팟 ]',
+						'[ 제임스 | 제육볶음 | 라자냐 | 비빔밥 | 짬뽕 | 파인애플 볶음밥 ]',
+						'추천을 완료했습니다.',
+					].join(''),
+				),
+			);
 		});
 	});
 });
