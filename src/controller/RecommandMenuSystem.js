c6b9d31d32fe2cfa1aa757322a08cd3000166f8b
daal2quf <daal2quf@gmail.com>
2022-12-17 17:41:37 +0900

    feat(recommandMenuSystem): 각각의 코치에 대한 싫어하는 음식요청 비동기 처리 실패로 인한 메서드 추가
    add:
    setCoachName(): 코치한명한명 물어봐야할 readline을 해결하지못해 생성된 코치를 배열로 등록하고 각각의 인덱스로 불러와 각각의 코치에게 싫어하는 음식 요청하는 로직으로 변경
    saveFirstCoachCharacter(): 첫번째 코치의 이름을 키값으로 싫어하는 음식 입력값을 밸류값으로 등록
    requestHateFoodOfSecondCoach(): 지양해야하지만 getter를 이용해 두번쨰 코치에게 싫어하는 음식 요청

--- src/controller/RecommandMenuSystem.js
+++ src/controller/RecommandMenuSystem.js
@@ -4,8 +4,9 @@ const { isCorrectInputHandler } = require('./isCorrectInputHandler');
 const { Validation } = require('./Validation');
 
 class RecommandMenuSystem {
-  constructor(model) {
+  constructor(model, coachRepository) {
     this.recommandMenu = model;
+    this.coach = coachRepository;
   }
 
   start() {
@@ -17,12 +18,56 @@ class RecommandMenuSystem {
     InputView.readCoachName(this.validateInputOfCoachName.bind(this));
   }
 
-  validateInputOfCoachName(userInput) {
-    if (!isCorrectInputHandler(Validation.ofCoachName, userInput)) {
+  validateInputOfCoachName(coachName) {
+    if (!isCorrectInputHandler(Validation.ofCoachName, coachName)) {
       this.requestCoachName();
       return;
     }
+    this.setCoachName(coachName);
   }
+
+  setCoachName(coach) {
+    this.coach.setName(coach);
+    this.requestHateFoodOfFirstCoach();
+  }
+
+  requestHateFoodOfFirstCoach() {
+    const firstCoach = this.coach.getName()[0];
+    InputView.readNotEatMenu(firstCoach, this.saveFirstCoachCharacter.bind(this));
+  }
+
+  saveFirstCoachCharacter(userInputMenu) {
+    const firstCoach = this.coach.getName()[0];
+    this.coach.setHateFood(firstCoach, userInputMenu);
+    this.requestHateFoodOfSecondCoach();
+  }
+  requestHateFoodOfSecondCoach() {
+    const secondCoach = this.coach.getName()[1];
+    InputView.readNotEatMenu(secondCoach, this.saveSecondCoachCharacter.bind(this));
+  }
+
+  saveSecondCoachCharacter(userInputMenu) {
+    const secondCoach = this.coach.getName()[1];
+    this.coach.setHateFood(secondCoach, userInputMenu);
+    this.requestHateFoodOfThirdCoach();
+  }
+  requestHateFoodOfThirdCoach() {
+    const thirdCoach = this.coach.getName()[2];
+    InputView.readNotEatMenu(thirdCoach, this.saveThirdCoachCharacter.bind(this));
+  }
+
+  saveThirdCoachCharacter(userInputMenu) {
+    const thirdCoach = this.coach.getName()[2];
+    this.coach.setHateFood(thirdCoach, userInputMenu);
+    this.analyzeStart;
+  }
+
+  analyzeStart() {
+    OutputView.printRecommandMenuStart();
+    this.recommandMenu.analinzing();
+  }
+
+  analinzing() {}
 }
 
 module.exports = RecommandMenuSystem;
