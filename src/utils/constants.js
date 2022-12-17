61584823ad6ce8f62c5e3f07f928d82b9fe9a25a
ashleysyheo <ashleysyheo@gmail.com>
2022-12-17 16:40:10 +0900

    fix: change regex for testing special characters

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -12,7 +12,7 @@ const PARAMETERS = {
     양식: ['라자냐', '그라탱', '뇨끼', '끼슈', '프렌치 토스트', '바게트', '스파게티', '피자', '파니니'],
   },
   checkBlackSpace: /^\S+$/g,
-  checkSpecialCharacters: /[ `!@#$%^&*()_+\-=\[\]{};':"\\|.<>\/?~]/,
+  checkSpecialCharacters: /[`!@#$%^&*()_+\-=\[\]{};':"\\|.<>\/?~]/,
 };
 
 const CONSOLE_MESSAGE = {
