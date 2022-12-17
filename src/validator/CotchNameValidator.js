bdd14b78743c991a86aa6204b0763325b3469310
sujungYu <su_moment@naver.com>
2022-12-17 17:50:56 +0900

    refactor(CotchNameValidator) : 유효성 수정

--- src/validator/CotchNameValidator.js
+++ src/validator/CotchNameValidator.js
@@ -1,15 +1,15 @@
 class CotchNameValidator {
-  constructor(names) {
-    this.validate(names);
+  constructor(cotchs) {
+    this.validate(cotchs);
   }
-  validate(names) {
-    const cotchs = names.split(',').map((cotch) => cotch.trim());
+  validate(cotchs) {
+    // const cotchs = names.split(',').map((cotch) => cotch.trim());
     if (this.isCotchsRange(cotchs)) throw new Error('[ERROR] 최소 2명, 최대 5명이 함께 식사할 수 있습니다.');
     cotchs.forEach((cotch) => {
-      console.log(cotch);
       if (!this.isString(cotch)) throw new Error('[ERROR] 한글로 입력해야 합니다.');
       if (this.isNameRange(cotch)) throw new Error('[ERROR] 최소 2글자, 최대 4글자를 입력해야 합니다.');
     });
+    return cotchs;
   }
   isCotchsRange(cotchs) {
     return cotchs.length < 2 || cotchs.length > 5;
