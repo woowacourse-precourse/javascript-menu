49a90df78570ac74e7768458dadea5e295090c34
solo5star <solo_5@naver.com>
2022-12-17 17:33:11 +0900

    feat(io): InputView의 검증 로직을 App으로 옮김

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable class-methods-use-this */
 /* eslint-disable require-yield */
 /* eslint-disable no-restricted-syntax */
 const InputView = require('./views/InputView');
@@ -6,6 +7,7 @@ const runGenerator = require('./utils/runGenerator');
 const Coach = require('./domains/Coach');
 const LaunchService = require('./services/LaunchService');
 const { CATEGORIES, DAYS } = require('./fixtures');
+const AppError = require('./errors/AppError');
 
 class App {
   #inputView = new InputView();
@@ -23,9 +25,26 @@ class App {
 
   * #run() {
     for (const routine of this.#routines()) {
-      yield* routine;
+      yield* this.#runRoutine(routine);
       this.#outputView.printDivider();
     }
+  }
+
+  * #runRoutine(routine) {
+    while (true) {
+      try {
+        yield* routine;
+        break;
+      } catch (error) {
+        this.#handleError(error);
+      }
+    }
+  }
+
+  #handleError(error) {
+    if (!(error instanceof AppError)) throw error;
+
+    this.#outputView.printError(error);
   }
 
   * #routines() {
@@ -45,16 +64,24 @@ class App {
   }
 
   * #routineReadCoaches() {
-    this.#coaches = yield (resolve) => this.#inputView.readCoaches(resolve);
+    const coachNames = yield (resolve) => this.#inputView.readCoachNames(resolve);
+    this.#coaches = coachNames.map((coachName) => new Coach(coachName.trim()));
   }
 
   /**
    * @param {Coach} coach
    */
   * #routineReadDislikeMenus(coach) {
-    yield (resolve) => {
-      this.#inputView.readDislikeMenus(coach, this.#launchService.getMenus(), resolve);
-    };
+    const allMenus = this.#launchService.getMenus();
+    const menuNames = yield (resolve) => this.#inputView.readDislikeMenuNames(coach, resolve);
+    const menus = menuNames.map((menuName) => allMenus.find((menu) => menu.getName() === menuName.trim()) ?? null);
+    if (menus.some((menu) => menu === null)) {
+      throw new AppError('존재하지 않는 메뉴를 입력하였습니다.');
+    }
+    if (menus.length > 2) {
+      throw new AppError('못 먹는 메뉴는 최소 0개, 최대 2개까지 입력할 수 있습니다.');
+    }
+    coach.setDislikeMenus(menus);
   }
 
   * #routinePrintSuggestionTable() {
--- src/views/InputView.js
+++ src/views/InputView.js
@@ -1,8 +1,6 @@
 /* eslint-disable class-methods-use-this */
 const { Console } = require('@woowacourse/mission-utils');
-const Menu = require('../domains/Menu');
 const Coach = require('../domains/Coach');
-const AppError = require('../errors/AppError');
 
 class InputView {
   /**
@@ -20,29 +18,19 @@ class InputView {
     Console.print(message);
   }
 
-  readCoaches(callback) {
+  readCoachNames(callback) {
     this.read('코치의 이름을 입력해 주세요. (, 로 구분)', (text) => {
-      callback(text.split(',').map((coachName) => new Coach(coachName.trim())));
+      callback(text.split(',').map((coachName) => coachName.trim()));
     });
   }
 
   /**
    * @param {Coach} coach
-   * @param {Menu[]} sourceMenus
    * @param {function(string): void} callback
    */
-  readDislikeMenus(coach, sourceMenus, callback) {
+  readDislikeMenuNames(coach, callback) {
     this.read(`${coach.getName()}(이)가 못 먹는 메뉴를 입력해 주세요.`, (text) => {
-      const menus = text
-        .split(',')
-        .map((menuName) => sourceMenus.find((menu) => menu.getName() === menuName.trim()) ?? null);
-      if (menus.some((menu) => menu === null)) {
-        throw new AppError('존재하지 않는 메뉴를 입력하였습니다.');
-      }
-      if (menus.length > 2) {
-        throw new AppError('못 먹는 메뉴는 최소 0개, 최대 2개까지 입력할 수 있습니다.');
-      }
-      callback(menus);
+      callback(text.split(',').map((dislikeMenuName) => dislikeMenuName.trim()));
     });
   }
 
