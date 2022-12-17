7f7a2c4aa0a34142a7eaabcdb7a6e84a10adc240
daal2quf <daal2quf@gmail.com>
2022-12-17 15:52:38 +0900

    feat(validation): 메뉴에 없는 음식을 입력했을시 예외처리기능 추가
    add:
    ofMenu: 메뉴맵의 밸류값을 돌면서 입력한 음식이 있는지 확인

--- src/controller/Validation.js
+++ src/controller/Validation.js
@@ -1,11 +1,19 @@
+const { MenuMap } = require('../domain/MenuMap');
+
 const Validation = {
   ofCoachName(userInput) {
     if (!userInput.split(',').every((name) => name.length >= 2 && name.length <= 4)) {
       throw new Error('[ERROR] 코치의 이름은 2글자 이상 4글자 이하로만 입력해주세요');
     }
-    if (!userInput.split(',').length < 2 || userInput.split(',').length > 5) {
+    if (userInput.split(',').length < 2 || userInput.split(',').length > 5) {
       throw new Error('[ERROR] 코치는 최소 2명 이상 입력해야 합니다.');
     }
+  },
+
+  ofMenu(userInput) {
+    const menu = [...MenuMap.values()].join(', ').split(', ');
+    if (!userInput.split(',').every((food) => menu.includes(food)))
+      throw new Error('[ERROR] 메뉴에 없는 메뉴입니다.');
   },
 };
 
