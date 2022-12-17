9a2d1564dd6ac719a34abe0ae457cc4441e87218
ksone02 <eksovus@gmail.com>
2022-12-17 17:21:36 +0900

    fix: 상수 오타 수정

--- src/data/Constants.js
+++ src/data/Constants.js
@@ -1,6 +1,6 @@
 const MESSAGE = Object.freeze({
-  START: '점심 메뉴 추천을 시작합니다.\n',
-  INPUT_COACH: '코치의 이름을 입력해 주세요. (, 로 구분)\n',
+  START: '점심 메뉴 추천을 시작합니다.',
+  INPUT_COACH: '코치의 이름을 입력해 주세요. (, 로 구분)',
   INPUT_CANT_EAT: '(이)가 못 먹는 메뉴를 입력해 주세요.\n',
   RESULT: '메뉴 추천 결과입니다.',
   DIVISION: '[ 구분 | 월요일 | 화요일 | 수요일 | 목요일 | 금요일 ]',
@@ -8,7 +8,7 @@ const MESSAGE = Object.freeze({
   MENU_END: ' ]',
   MENU_DIVIDE: ' | ',
   CATEGORY: '카테고리',
-  END: '추첨을 완료했습니다.',
+  END: '추천을 완료했습니다.',
 });
 
 const ERROR = Object.freeze({
