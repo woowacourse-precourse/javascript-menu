9fe4ebdc32c6bf5c5cf0465b694770553f37f4cd
한수지 <fatholic5@gmail.com>
2022-12-17 15:49:46 +0900

    :sparkles: feat(Controller): 데이터를 처리할 모델과 호출 오류처리 로직 구현 사용자가 못먹는 음식에 대한 입력이 조건 밖일때 예외처리를 구현하고, 재입력 받을 수 있도록 로직을 구현함 오류시에는 오류메세지 출력기능을 가진 메서드를 호출할 수 있도록 만듬

--- src/Controller.js
+++ src/Controller.js
@@ -7,6 +7,7 @@ class Controller {
 
   constructor() {
     this.validate =  new Validate();
+    this.lunchModel = new LunchModel();
   }
 
   play() {
@@ -18,15 +19,40 @@ class Controller {
     InputView.readName(this.tryBuildUser.bind(this));
   }
 
+  requestFood() {
+    InputView.readMenu(this.tryBuildFood.bind(this));
+  }
+
   tryBuildUser(name) {
     try {
-
+      this.validate.validateUserName(name);
+      this.lunchModel(name);
+      this.requestFood();
     } catch(error) {
-
+      this.retryRequestUser();
     }
   }
 
-  
+  tryBuildFood(food) {
+    try {
+      this.validate.validateFood(food);
+      this.lunchModel(food);
+      this.requestFood();
+    } catch(error) {
+      this.retryRequestFood();
+    }
+  }
+
+  retryRequestUser() {
+    OutputView.printSizeError();
+    this.requestUser();
+  }
+
+  retryRequestFood() {
+    OutputView.printMenuError();
+    this.requestFood();
+  }
+
 }
 
 module.exports = Controller;
