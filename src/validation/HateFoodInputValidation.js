f1ec422669029657539f91fd7aa6d5d88e512a60
sh981013s <sh981013s@gmail.com>
2022-12-17 14:56:13 +0900

    feat(HateFoodInputValidation): 못먹는 음식 input 유효성 검사 로직 구현

--- src/validation/HateFoodInputValidation.js
+++ src/validation/HateFoodInputValidation.js
@@ -1,5 +1,12 @@
+const { HATE_FOOD } = require("../constants/Messages");
+
 class HateFoodInputValidation {
-  static validate(hateFoodInput) {}
+  static validate(hateFoodInput) {
+    const splitHateFood = hateFoodInput.split(",");
+    if (splitHateFood.length > 2) {
+      throw new Error(HATE_FOOD.ERROR);
+    }
+  }
 }
 
 module.exports = HateFoodInputValidation;
