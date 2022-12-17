16746241b1ef3c2577eeaee821be650c7e37e4bc
2pandi <coder.2pandi@gmail.com>
2022-12-17 13:52:20 +0900

    fix(constants): fix stating order

--- src/constants.js
+++ src/constants.js
@@ -1,16 +1,17 @@
-const MESSAGE = Object.freeze({
-  START: "점심 메뉴 추천을 시작합니다.",
-  ASK_NAME: `코치의 이름을 입력해 주세요. (${SYMBOL.COMMA} 로 구분)`,
-  ASK_MENU: "(이)가 못 먹는 메뉴를 입력해 주세요.",
-  RESULT_START: "메뉴 추천 결과입니다.",
-  RESULT_END: "추천을 완료했습니다.",
-});
-
 const SYMBOL = Object.freeze({
   COMMA: ",",
   RESULT_OPEN: "[ ",
   RESULT_CLOSE: " ]",
   SEPARATOR: " | ",
+  NEW_LINE: "\n",
+});
+
+const MESSAGE = Object.freeze({
+  START: `점심 메뉴 추천을 시작합니다.${SYMBOL.NEW_LINE}`,
+  ASK_NAME: `코치의 이름을 입력해 주세요. (${SYMBOL.COMMA} 로 구분)${SYMBOL.NEW_LINE}`,
+  ASK_MENU: `(이)가 못 먹는 메뉴를 입력해 주세요.${SYMBOL.NEW_LINE}`,
+  RESULT_START: "메뉴 추천 결과입니다.",
+  RESULT_END: "추천을 완료했습니다.",
 });
 
 const DAY = Object.freeze({
@@ -31,4 +32,4 @@ const CATEGORY = Object.freeze({
   WES: "양식",
 });
 
-module.exports = { MESSAGE };
+module.exports = { MESSAGE, SYMBOL, DAY, CATEGORY };
