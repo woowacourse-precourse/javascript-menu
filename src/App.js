52fdd0ff6fad99d98b3c10ba4c5dabc08b5ddc45
bassyu <yrkdgus@gmail.com>
2022-12-17 14:54:04 +0900

    refactor(CategorisMaker): utils 폴더로 이동

--- src/App.js
+++ src/App.js
@@ -1,14 +1,42 @@
-const SAMPLE = {
-	일식: '규동, 우동, 미소시루, 스시, 가츠동, 오니기리, 하이라이스, 라멘, 오코노미야끼',
-	한식: '김밥, 김치찌개, 쌈밥, 된장찌개, 비빔밥, 칼국수, 불고기, 떡볶이, 제육볶음',
-	중식: '깐풍기, 볶음면, 동파육, 짜장면, 짬뽕, 마파두부, 탕수육, 토마토 달걀볶음, 고추잡채',
-	아시안:
-		'팟타이, 카오 팟, 나시고렝, 파인애플 볶음밥, 쌀국수, 똠얌꿍, 반미, 월남쌈, 분짜',
-	양식: '라자냐, 그라탱, 뇨끼, 끼슈, 프렌치 토스트, 바게트, 스파게티, 피자, 파니니',
-};
+const CategorisMaker = require('./utils/CategorisMaker');
+const InputView = require('./views/InputView');
+const OutputView = require('./views/OutputView');
+const MissionUtils = require('@woowacourse/mission-utils');
+const Menu = require('./models/Menu');
 
 class App {
-  play() {}
+  #categoris;
+  #names;
+  #menus;
+
+  play() {
+    OutputView.printStart();
+    this.#categoris = CategorisMaker.make();
+    this.#names = [];
+    this.#menus = [];
+    InputView.readNames(this.#handleNames.bind(this));
+  }
+
+  #handleNames(input) {
+    this.#names = input.split(',');
+    InputView.readHates(this.#handleHates.bind(this), this.#names[this.#menus.length]);
+  }
+
+  #handleHates(input) {
+    const hates = input.split(',');
+    this.#menus.push(new Menu(this.#categoris, hates));
+    if (this.#menus.length < this.#names.length) {
+      InputView.readHates(this.#handleHates.bind(this), this.#names[this.#menus.length]);
+    } else {
+      this.#showMenus();
+    }
+  }
+
+  #showMenus() {
+    const foods = this.#menus.map((menu) => menu.choiceFood());
+    OutputView.printMenus(this.#categoris, this.#names, foods);
+    InputView.close();
+  }
 }
 
 module.exports = App;
