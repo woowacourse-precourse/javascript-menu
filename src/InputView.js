4ea8fc99c1fd408202a254081b689ab11a495dc0
useon <yoosk4986@naver.com>
2022-12-17 14:20:20 +0900

    feat: 입력이 잘못되었을 경우 에러 메세지 출력 후 입력을 다시 받는다

--- src/InputView.js
+++ src/InputView.js
@@ -5,13 +5,18 @@ const Validator = require('./Validator');
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 const InputView = {
+  reEnter(error, readMethod, callback) {
+    Console.print(error.message);
+    readMethod(callback);
+  },
+
   readName(callback) {
     Console.readLine('\n코치의 이름을 입력해 주세요. (, 로 구분)\n', (name) => {
       try {
         Validator.name(name);
         callback(name);
       } catch (error) {
-        // InputView.reEnter(error, InputView.readName, callback);
+        InputView.reEnter(error, InputView.readName, callback);
       }
     });
   },
