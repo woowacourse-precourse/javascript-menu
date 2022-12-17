c77613ded1d8139a6d9ae6c7bd0cc2de40deda3f
curomame <curomame@naver.com>
2022-12-17 13:56:02 +0900

    ➕ feat - <InputView> [readInedibleMenus] add
    코치의 이름에 따라 못 먹는 메뉴를 입력받는 기능 구현

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -8,6 +8,14 @@ const InputView = {
   readCoachNames(callback) {
     MissionUtils.Console.readLine("코치의 이름을 입력해 주세요. (, 로 구분)\n", callback);
   },
+
+   /**
+   * 사용자에게 코치의 이름에 따라 못 먹는 메뉴를 입력받는다.
+   */
+     readInedibleMenus(callback,coachName) {
+      MissionUtils.Console.readLine(`${coachName}(이)가 못 먹는 메뉴를 입력해 주세요.\n`, callback);
+    },
 };
+
 
 module.exports = InputView;
