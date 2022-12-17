7d1c07abe2c2b094b2094e1fd3322c3e33355a8e
한수지 <fatholic5@gmail.com>
2022-12-17 17:02:52 +0900

    :recycle: refactor(InputView): 사용자마다 달라지는 메세지를 외부에서 받아오도록 수정 사용자에게 못먹는 음식을 입력받을때, 사용자마다 이름이 달라지므로 외부에서 데이터값을 받아올 수 있도록 인자를 연결한다.

--- src/InputView.js
+++ src/InputView.js
@@ -16,8 +16,8 @@ const InputView = {
   /**
    * 사용자에게 못먹는 음식을 입력받는다.
    */
-  readMenu(callback) {
-    Console.readLine(`\n${INPUT.HATE_MENU}\n`, callback);
+  readMenu(user, callback) {
+    Console.readLine(`\n${INPUT.HATE_MENU(user)}\n`, callback);
   },
 };
 
