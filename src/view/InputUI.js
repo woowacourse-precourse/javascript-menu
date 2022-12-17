89d175d8e8296d9cce28884a3bdf75fd94f389c8
2yunseong <dbsdltjd3701@naver.com>
2022-12-17 14:03:31 +0900

    refactor(InputUI): 코치 이름을 입력받는 로직을 개선

--- src/view/InputUI.js
+++ src/view/InputUI.js
@@ -1,9 +1,16 @@
 const MissionUtils = require('@woowacourse/mission-utils');
+const OutputUI = require('./OutputUI');
 
 class InputUI {
-  readLine(callback) {
+  constructor() {
+    this.output = new OutputUI();
+  }
+  readCoachNames(callback) {
+    this.output.print('코치의 이름을 입력해 주세요. (, 로 구분)');
     MissionUtils.Console.readLine('', (answer) => {
       callback(answer);
     });
   }
 }
+
+module.exports = InputUI;
