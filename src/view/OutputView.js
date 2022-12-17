4c83ed91eab6458958f9e885381798cd2aeb4208
ReturnReason <reasonz@daum.net>
2022-12-17 15:32:51 +0900

    feat(OutputView): 에러 메시지를 출력하는 메서드 추가

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -4,6 +4,14 @@ const OutputView = {
   printStart(message) {
     print(message);
   },
+
+  printResult() {
+    //메뉴 추천 결과를 출력
+  },
+
+  printError(error) {
+    print(error);
+  },
 };
 
 module.exports = OutputView;
