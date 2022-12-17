bb257a0cd67317b98a5d5503dd12e444b53f571e
하송희 <99heesong@naver.com>
2022-12-17 13:32:17 +0900

    기능추가: 서비스 시작 문구 출력

---
+++ src/OutputView.js
@@ -0,0 +1,9 @@
+const { Console } = require("@woowacourse/mission-utils");
+
+const OutputView = {
+  printStartMent() {
+    Console.print("점심 메뉴 추천을 시작합니다.");
+  },
+};
+
+module.exports = OutputView;
