65661033585599c8eaa5fdf980c692be84746a10
chsua <csj1919@naver.com>
2022-12-17 13:57:55 +0900

    Feat: output 기능 구현

--- src/Constant.js
+++ src/Constant.js
@@ -20,7 +20,7 @@ const COACH = {
 
 const MESSAGE = {
     START: "점심 메뉴 추천을 시작합니다.\n",
-    END_FIRST: "메뉴 추천 결과입니다.",
+    END_FIRST: "메뉴 추천 결과입니다.\n[ 구분 | 월요일 | 화요일 | 수요일 | 목요일 | 금요일 ]",
     END_SECOND: "\n추천을 완료했습니다.",
     INPUT_COACH_NAME: "코치의 이름을 입력해 주세요. (, 로 구분)\n",
     INPUT_NOTEAT: "(이)가 못 먹는 메뉴를 입력해 주세요.\n",
--- src/Output.js
+++ src/Output.js
@@ -1,1 +1,30 @@
 const { NAME, COACH, MESSAGE } = require("./Constant");
+const { Console } = require("@woowacourse/mission-utils");
+
+const Output = {
+    start() {
+        Console.print(MESSAGE.START);
+    },
+
+    endHead(category) {
+        Console.print(MESSAGE.END_FIRST);
+        Console.print(`[ 카테고리 | ${category.join(" | ")} ]`)
+    },
+
+    endBody(coachName, menu) {
+        menu.shift(coachName);
+        const printMenu = coachMenu.join(" | ")
+        Console.print(`[ ${printMenu.join(" | ")} ]`)
+    },
+
+    endHead() {
+        Console.print(MESSAGE.END_SECOND);
+    },
+
+    Error(message) {
+        Console.print(message);
+    },
+}
+
+
+module.exports = Output;
