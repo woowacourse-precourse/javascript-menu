85a2b4f4f018db96c103f79257ec8276ac7fc70f
Yoonkyoung Kim <100656920+Yoonkyoungme@users.noreply.github.com>
2022-12-17 13:56:42 +0900

    feat: 코치 이름 입력 받기

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 const OutputView = require("./OutputView");
+const { Console } = require("@woowacourse/mission-utils");
+const { INPUT } = require("./constants/messages");
 
 const SAMPLE = {
   일식: "규동, 우동, 미소시루, 스시, 가츠동, 오니기리, 하이라이스, 라멘, 오코노미야끼",
@@ -10,8 +12,19 @@ const SAMPLE = {
 };
 
 class App {
+  constructor() {
+    this.names = [];
+  }
+
   play() {
     OutputView.start();
+    this.inputCoachName();
+  }
+
+  inputCoachName() {
+    Console.readLine(INPUT.NAME, (name) => {
+      console.log(name);
+    });
   }
 }
 
