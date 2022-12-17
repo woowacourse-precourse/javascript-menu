4aaa2736ee16a8649734bee8d91bec895e77d3a0
chsua <csj1919@naver.com>
2022-12-17 14:32:41 +0900

    Feat: 중복되거나 못먹는 음식인지  확인하는 함수로 재구현

--- src/Coach.js
+++ src/Coach.js
@@ -21,8 +21,8 @@ class Coach {
         return false
     }
 
-    menuValidate(menu) { //못먹는 음식이면 false, 먹을 수 있으면 true
-        return (!this.#notEat.includes(menu))
+    menuValidate(menu) { //못먹거나 이미 있는 음식이면 false, 먹을 수 있으면 true
+        return ((!this.#weekMenu.includes(menu)) && (!this.#notEat.includes(menu)))
     }
 
 }
