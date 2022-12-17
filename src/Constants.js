3d8c67cd3277950da402ddf81e3a0360d00de459
dim <dim@student.42seoul.kr>
2022-12-17 17:01:42 +0900

    Feat: 결과 출력

--- src/Constants.js
+++ src/Constants.js
@@ -2,13 +2,16 @@ const MESSAGE = Object.freeze({
   START: '점심 메뉴 추천을 시작합니다.\n',
   COACH_NAME: '코치의 이름을 입력해 주세요. (, 로 구분)\n',
   AVOID_MENU: '(이)가 못 먹는 메뉴를 입력해 주세요.\n',
-  RESULT: '메뉴 추천 결과입니다.\n',
-  FINISH: '추천을 완료했습니다.',
+  RESULT: '\n메뉴 추천 결과입니다.',
+  FINISH: '\n추천을 완료했습니다.',
 });
 
 const DAYS = Object.freeze(['월요일', '화요일', '수요일', '목요일', '금요일']);
 
+const CATEGORY = Object.freeze(['일식', '한식', '중식', '아시안', '양식']);
+
 module.exports = {
   MESSAGE,
   DAYS,
+  CATEGORY,
 };
--- src/Views/OutputView.js
+++ src/Views/OutputView.js
@@ -1,9 +1,31 @@
 const { Console } = require('@woowacourse/mission-utils');
+const { MESSAGE, DAYS, CATEGORY } = require('../Constants');
 
 const OutputView = {
 
   printMessage(message) {
     Console.print(message);
+  },
+
+  printArrayForm(array, list) {
+    let print = '';
+    if (list) print += `${list} | `;
+    print += array.join(' | ');
+    Console.print(`[ ${print} ]`);
+  },
+
+  printResult(coaches) {
+    this.printMessage(MESSAGE.RESULT);
+    this.printArrayForm(DAYS, '구분');
+    this.printArrayForm(CATEGORY, '카테고리');
+    coaches.forEach((coach) => {
+      this.printCoachesDailyMenu(coach);
+    });
+    this.printMessage(MESSAGE.FINISH);
+  },
+
+  printCoachesDailyMenu(coach) {
+    this.printArrayForm(coach.getDailyMenu(), coach.name);
   },
 };
 
