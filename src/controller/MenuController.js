776263ab49e03807317298783ca6178506abdcfb
Gilpop8663 <wolfye@naver.com>
2022-12-17 13:35:45 +0900

    feat(InputView): 코치의 이름을 입력받는 기능

--- src/controller/MenuController.js
+++ src/controller/MenuController.js
@@ -1,8 +1,14 @@
+const { readCoachesName } = require('../view/InputView');
 const { printStart } = require('../view/OutputView');
 
 class MenuController {
   start() {
     printStart();
+    readCoachesName(this.checkCoachesName.bind(this));
+  }
+
+  checkCoachesName(names) {
+    console.log(names);
   }
 }
 
--- src/view/InputView.js
+++ src/view/InputView.js
@@ -1,5 +1,12 @@
 const { Console } = require('@woowacourse/mission-utils');
+const { GAME_MESSAGE } = require('../Constant');
 
-const InputView = {};
+const InputView = {
+  readCoachesName(callback) {
+    Console.readLine(`${GAME_MESSAGE.inputCoachesName}\n`, (names) => {
+      return callback(names);
+    });
+  },
+};
 
 module.exports = InputView;
