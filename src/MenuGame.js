3d62ea7765630d1eb97b8129b14c5664a106a051
ashleysyheo <ashleysyheo@gmail.com>
2022-12-17 14:42:54 +0900

    feat: set menu based on category

--- src/MenuGame.js
+++ src/MenuGame.js
@@ -1,11 +1,13 @@
 const MissionUtils = require('@woowacourse/mission-utils');
 const { PARAMETERS } = require('./utils/constants');
+const Validation = require('./Validation');
 
 class MenuGame {
   constructor() {
     this.coaches = [];
     this.inedibleMenus = {};
     this.categories = [];
+    this.menus = {};
   }
 
   // 각 요일별 메뉴 생성하기
@@ -13,14 +15,33 @@ class MenuGame {
     const CATEGORY = PARAMETERS.category[this.getCategory() - 1];
     const DUPLICATES = this.categories.filter((ele) => ele === CATEGORY).length;
     if (DUPLICATES.length === 2) {
-      return this.setCategory();
+      this.setCategory();
     }
 
     this.categories.push(CATEGORY);
+    return CATEGORY;
   }
 
   getCategory() {
     return MissionUtils.Random.pickNumberInRange(1, 5);
+  }
+
+  setMenu(category, coach) {
+    if (this.menus[coach] === undefined) {
+      this.menus[coach] = [];
+    }
+
+    const MENU = this.getMenu(category);
+
+    if (!Validation.checkMenu(MENU, this.menus[coach], this.inedibleMenus[coach])) {
+      this.setMenu(category, coach);
+    }
+
+    this.menus[coach].push(MENU);
+  }
+
+  getMenu(category) {
+    return MissionUtils.Random.shuffle(category)[0];
   }
 }
 
