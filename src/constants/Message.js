9aa31a707f9b51b99f328cc8553fdeb54e2f5ee2
letsjo <hyunoh.jo@gmail.com>
2022-12-17 23:01:16 +0900

    refactor(*): 상수 문자열 변경

--- src/constants/Message.js
+++ src/constants/Message.js
@@ -3,7 +3,7 @@ const { PROCESS_CONSTANTS } = require('./Setting');
 
 const MESSAGE = {
   process: {
-    startNotice: '점심 메뉴 추천를 시작합니다.',
+    startNotice: '점심 메뉴 추천을 시작합니다.',
     inputCoachName: '\n코치의 이름을 입력해 주세요. (, 로 구분)\n',
     inputNotGoodMenu: '(이)가 못 먹는 메뉴를 입력해 주세요.\n',
     printMenuNotice: '\n메뉴 추천 결과입니다.\n',
