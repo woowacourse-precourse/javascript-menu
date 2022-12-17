269ff7a403b8b250cbb911d67dc8fffbf03e6fe2
Dahye <06robin11@gmail.com>
2022-12-17 17:32:24 +0900

    style(constant):새로운 라인 없앰

--- src/Constant.js
+++ src/Constant.js
@@ -2,11 +2,11 @@ const NEW_LINE = "\n";
 const ERROR = "[ERROR]";
 
 const MESSAGE = Object.freeze({
-  start: `점심 메뉴 추천을 시작합니다.${NEW_LINE}`,
+  start: `점심 메뉴 추천을 시작합니다.`,
   askName: `코치의 이름을 입력해 주세요. (, 로 구분)${NEW_LINE}`,
   askPickyFood: (name) =>
     `${name} (이)가 못 먹는 메뉴를 입력해 주세요.${NEW_LINE}`,
-  result: `메뉴 추천 결과입니다.${NEW_LINE}`,
+  result: `메뉴 추천 결과입니다.`,
   end: "추천을 완료했습니다.",
 });
 
