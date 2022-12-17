6b27eb3c6fe0824fac6ce6da2aa56181c94c05e2
yooni <jiyoon0616@naver.com>
2022-12-17 17:47:36 +0900

    feat (Coach) : addMenus 함수 구현

--- src/Coach.js
+++ src/Coach.js
@@ -1,8 +1,18 @@
 class Coach {
   #name;
+  #menus;
   #banMenuSet;
   constructor(CoachName) {
     this.#name = CoachName;
+    this.#menus = [];
+  }
+
+  addMenus(menus) {
+    let menu = MissionUtils.Randoms.shuffle(menus)[0];
+    while (banMenuSet.has(menu) || this.#menus.includes(menu)) {
+      menu = MissionUtils.Randoms.shuffle(menus)[0];
+    }
+    this.#menus.push(menu);
   }
 
   addBanMenus(banMenuArray) {
