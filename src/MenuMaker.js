a85c57025bbce24d6706e6254cf5e968ddf6c62b
TaeeunKim <xodms0309@ewhain.net>
2022-12-17 16:53:07 +0900

    refactor: 카테고리 인덱스의 변수명 수정

--- src/MenuMaker.js
+++ src/MenuMaker.js
@@ -10,9 +10,9 @@ class MenuMaker {
 
   createMenu() {
     const weekMenu = [];
-    for (const category of this.#categories) {
+    for (const categoryIndex of this.#categories) {
       while (true) {
-        const selectedMenu = this.selectRandomMenu(category);
+        const selectedMenu = this.selectRandomMenu(categoryIndex);
         if (weekMenu.find((menu) => menu === selectedMenu)) continue; //이미 일주일 메뉴에 있는 경우
         if (this.#dislikeMenu.find((menu) => menu === selectedMenu)) continue; //못 먹는 메뉴일 경우
         weekMenu.push(selectedMenu);
@@ -22,11 +22,11 @@ class MenuMaker {
     return weekMenu;
   }
 
-  selectRandomMenu(category) {
+  selectRandomMenu(categoryIndex) {
     const numMenu = MENUS[0].length;
     const menuIndexArr = Array.from({ length: numMenu }, (_, i) => i + 1);
     const selectedMenuIndex = Random.shuffle(menuIndexArr)[0];
-    const selectedMenu = MENUS[category - 1][selectedMenuIndex - 1];
+    const selectedMenu = MENUS[categoryIndex - 1][selectedMenuIndex - 1];
     return selectedMenu;
   }
 }
