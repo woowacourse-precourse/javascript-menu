e49a97ec25b86fdf3ff9d999b99c9986cd5a00d0
wusi-hub <jws9595@naver.com>
2022-12-17 13:53:06 +0900

    feat: 시작을 위한 해당 문구 출력

---
+++ src/OutputView.js
@@ -0,0 +1,11 @@
+const { Console } = require("@woowacourse/mission-utils");
+
+
+const OutputView = {
+  // 시작 문구의 출력
+  printStart() {
+    Console.print("점심 메뉴 추천을 시작합니다.");
+  },
+};
+
+module.exports = OutputView;
