2c2d92fa95d24de2d66a31aa2a9938ef023cde11
moseung <tmdah900@naver.com>
2022-12-17 13:17:08 +0900

    🎉 초기세팅 : 초기세팅 하는 파일들 추가

---
+++ src/Model/MenuModel.js
@@ -0,0 +1,3 @@
+class MenuModel {}
+
+module.exports = MenuModel;
---
+++ src/View/OutputView.js
@@ -0,0 +1,9 @@
+const { Console } = require("@woowacourse/mission-utils");
+
+const OutputView = {
+  printResult(message) {
+    Console.print(message);
+  },
+};
+
+module.exports = OutputView;
