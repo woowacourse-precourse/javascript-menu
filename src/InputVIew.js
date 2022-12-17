a199cd40f766cd52d24c072ab5a0717f7550470a
hafnium1923 <hafnium1923@gmail.com>
2022-12-17 15:37:30 +0900

    feat 최적의 상황 구현 완료

--- src/InputVIew.js
+++ src/InputVIew.js
@@ -1,18 +1,19 @@
 const MU = require("@woowacourse/mission-utils");
 const ErrorHandler = require('./ErrorHandler');
 const OutputView = require('./OutputView');
-
+const Menu = require('./Menu');
+const { pickMenu } = require("./Menu");
 const InputView = {
     /**
      * 코치 이름을 입력 받는다
      */
     readCoachName() {
-        MU.Console.readLine('코치의 이름을 입력해 주세요. (, 로 구분)\n', (nameString) => {
+        MU.Console.readLine('\n코치의 이름을 입력해 주세요. (, 로 구분)\n', (nameString) => {
             try {
                 const COACH = nameString.split(',');
                 ErrorHandler.coachNameError(COACH);
-                let hateMenu = this.readHateMenu(COACH,[],0);
-                
+                this.readHateMenu(COACH,[],0);
+                //Menu.pickMenu(COACH, Menu.pickCategory(), hateMenu);
             } catch(e) {
                 OutputView.printError(e);
                 this.readCoachName();
@@ -23,12 +24,13 @@ const InputView = {
      * 싫어하는 음식을 입력받는다
      */
     readHateMenu(COACH,hateMenu,i) {
-        MU.Console.readLine(`${COACH[i]} (이)가 못 먹는 메뉴를 입력해 주세요.\n`,(menu) => {
+        MU.Console.readLine(`\n${COACH[i]} (이)가 못 먹는 메뉴를 입력해 주세요.\n`,(menu) => {
             try {
                 hateMenu[i] = menu.split(',');
                 ErrorHandler.hateMenuError(hateMenu[i]);
-                if(i == COACH.length - 1)
-                    return hateMenu
+                if(i == COACH.length - 1){
+                    Menu.pickMenu(COACH, Menu.pickCategory(), hateMenu);
+                }
                 else this.readHateMenu(COACH,hateMenu,++i);
             } catch(e) {
                 OutputView.printError(e);
