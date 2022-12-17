5874ac12afbb646fe73ba8054208cf703f3edd01
nangkyeonglim <nangkyeonglim@gmail.com>
2022-12-17 13:20:14 +0900

    feat(OutputView): 메뉴 추천 결과를 출력하는 기능 구현

---
+++ src/views/OutputView.js
@@ -0,0 +1,13 @@
+const { Console } = require('@woowacourse/mission-utils');
+
+const OutputView = {
+  printResult(result) {
+    Console.print(`메뉴 추천 결과입니다.
+      [ 구분 | 월요일 | 화요일 | 수요일 | 목요일 | 금요일 ]
+      [ 카테고리 | 한식 | 한식 | 일식 | 중식 | 아시안 ]`);
+    Console.print(result);
+    Console.print('\n추천을 완료했습니다.');
+  },
+};
+
+module.exports = OutputView;
