d2da2cd54a4f2606ceb3e8ad055e4a0a4cc5d5b4
feb-dain <feb.dain@gmail.com>
2022-12-17 15:44:22 +0900

    feat: 못 먹는 메뉴 입력받고 예외 확인

--- src/UI/InputView.js
+++ src/UI/InputView.js
@@ -1,7 +1,8 @@
 const { GAME_MESSAGES } = require("../Utils/Constants");
-const { NAME } = GAME_MESSAGES;
+const { NAME, CAN_NOT_EAT } = GAME_MESSAGES;
 const { readLine, print } = require("../Utils/MissionUtils");
 const Validator = require("../Utils/Validator");
+const OutputView = require("./OutView");
 
 const InputView = {
   /**
@@ -13,9 +14,28 @@ const InputView = {
         name = name.split(",");
         Validator.notEnoughPeople(name);
         Validator.inappropriateName(name);
+        this.readHateMenu(name, 0);
       } catch (error) {
         print(error);
         this.readCoachName();
+      }
+    });
+  },
+
+  /**
+   * 못 먹는 메뉴를 입력받는다.
+   */
+  readHateMenu(name, index) {
+    readLine(`\n${name[index]}${CAN_NOT_EAT}`, (menu) => {
+      try {
+        Validator.notExistMenu(menu);
+        Validator.tooMany(menu);
+        index += 1;
+        if (index === name.length) return;
+        this.readHateMenu(name, index, menu);
+      } catch (error) {
+        print(error);
+        this.readHateMenu(name, index);
       }
     });
   },
