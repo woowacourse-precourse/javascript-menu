34bc35191744368cd0b44c3ce484b319aa7d22ed
shackstack <kms1061@naver.com>
2022-12-17 13:23:28 +0900

    feat(App): 점심 메뉴 추천 시작 메시지 출력 기능

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 const MissionUtils = require("@woowacourse/mission-utils");
+const OutputView = require("./view/OutputView");
 
 const SAMPLE = {
   일식: "규동, 우동, 미소시루, 스시, 가츠동, 오니기리, 하이라이스, 라멘, 오코노미야끼",
@@ -10,7 +11,16 @@ const SAMPLE = {
 };
 
 class App {
-  play() {}
+  play() {
+    this.printStartMessage();
+  }
+
+  printStartMessage() {
+    OutputView.printStartMessage();
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -1,5 +1,9 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 
-const OutputView = {};
+const OutputView = {
+  printStartMessage() {
+    MissionUtils.Console.print("점심 메뉴 추천을 시작합니다.\n");
+  },
+};
 
 module.exports = OutputView;
