4265c444859d5c28dd0ed94cf7c68bf43cc28e75
HeoJiye <heoze1031@gmail.com>
2022-12-17 13:46:12 +0900

    feat: 구분 출력 단순 구현

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -3,6 +3,17 @@ const MissionUtils = require("@woowacourse/mission-utils");
 const OutputView = {
     printStart() {
         MissionUtils.Console.print('점심 메뉴 추천을 시작합니다.');
+    },
+    
+    printEnding() {
+        PrintRecommandResult();
+    },
+    printRecommandResult() {
+        MissionUtils.Console.print('메뉴 추천 결과입니다.');
+        printClass();
+    },
+    printClass() {
+        MissionUtils.Console.print('[ 구분 | 월요일 | 화요일 | 수요일 | 목요일 | 금요일 ]');
     }
 };
 module.exports = OutputView;
