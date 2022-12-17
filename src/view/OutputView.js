0568a76428f6eb4f264d22c9fd1949d0758bb3f0
woo-jk <201702042@o.cnu.ac.kr>
2022-12-17 16:39:41 +0900

    refactor: OutputView 코치별 메뉴 출력 함수 분리

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -5,13 +5,17 @@ const OutputView = {
     MissionUtils.Console.print("점심 메뉴 추천을 시작합니다.");
   },
 
+  printMenu(coachsMenus) {
+    coachsMenus.forEach(({ name, menus }) => {
+      MissionUtils.Console.print(`[ ${name} | ${menus.join(" | ")} ]`);
+    });
+  },
+
   printRecommendedMenu(categorys, coachsMenus) {
     MissionUtils.Console.print("\n메뉴 추천 결과입니다.");
     MissionUtils.Console.print("[ 구분 | 월요일 | 화요일 | 수요일 | 목요일 | 금요일 ]");
     MissionUtils.Console.print(`[ 카테고리 | ${categorys.join(" | ")} ]`);
-    coachsMenus.forEach(({ name, menus }) => {
-      MissionUtils.Console.print(`[ ${name} | ${menus.join(" | ")} ]`);
-    });
+    this.printMenu(coachsMenus);
     MissionUtils.Console.print("\n추천을 완료했습니다.");
   },
 
