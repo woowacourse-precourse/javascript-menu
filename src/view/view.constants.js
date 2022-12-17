fac81770a553cc12fff5227b7d77b8a3e447fde0
동동이 <drdragon777@naver.com>
2022-12-17 13:42:30 +0900

    feat - add initial view message constants

---
+++ src/view/view.constants.js
@@ -0,0 +1,9 @@
+const MESSAGE = Object.freeze({
+  SERVICE_START_MESSAGE: "점심 메뉴 추천을 시작합니다.",
+  SERVICE_END_MESSAGE: "추천을 완료했습니다.",
+  MENU_RECOMMENDATION_RESULT: "메뉴를 추천 결과입니다.",
+  REQUEST_COACH_NAME: "코치 이름을 입력해 주세요. (, 로 구분)",
+  REQUEST_NOT_WANT_MENU: "(이)가 못 먹는 메뉴를 입력해 주세요.",
+});
+
+module.exports = MESSAGE;
