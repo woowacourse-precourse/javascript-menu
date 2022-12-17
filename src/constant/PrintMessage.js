35e2482541e91dcc8f1e97ed970b4af6396c3f24
Inae Choi <inaechoi89@gmail.com>
2022-12-17 13:36:36 +0900

    feat: 출력 메시지 상수 추가

---
+++ src/constant/PrintMessage.js
@@ -0,0 +1,10 @@
+const Message = {
+  START: '점심 메뉴 추천을 시작합니다.',
+  INPUT_NAME: '코치의 이름을 입력해 주세요. (, 로 구분)',
+  INPUT_NO_MENU: '(이)가 못 먹는 메뉴를 입력해 주세요.',
+  RESULT: '메뉴 추천 결과입니다.',
+  CATEGORY: '[ 구분 | 월요일 | 화요일 | 수요일 | 목요일 | 금요일 ]',
+  END: '추천을 완료했습니다.',
+};
+
+module.exports = Message;
