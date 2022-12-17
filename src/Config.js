57e1cea8c44d1656591d1b3133f2144d88b5f515
Q Kim <qoli0703@gmail.com>
2022-12-17 13:34:01 +0900

    feat(Config): constant configurations

---
+++ src/Config.js
@@ -0,0 +1,16 @@
+const MenuConfig = {
+  CATEGORIES: 5,
+  JAPANESE: 1,
+  KOREAN: 2,
+  CHINESE: 3,
+  ASIAN: 4,
+  WESTERN: 5,
+
+  1: '일식',
+  2: '한식',
+  3: '중식',
+  4: '아시안',
+  5: '양식',
+};
+
+module.exports = { MenuConfig };
