60a4c0e9809eb79542e375797b2682149be1c4ff
한수지 <fatholic5@gmail.com>
2022-12-17 14:44:44 +0900

    :sparkles: feat(Controller): 컨트롤러에서 사용할 기능들 모듈 연결 계속 사용되어야 할 모델 클래스들은 생성자를 통해 단축화함(오류판별 기능모델)

---
+++ src/Controller.js
@@ -0,0 +1,18 @@
+const OutputView = require("./OutputView");
+const InputView = require("./InputView");
+const Validate = require("./Validate");
+const LunchModel = require("./LunchModel");
+
+class Controller {
+
+  constructor() {
+    this.validate =  new Validate();
+  }
+
+  play() {
+    
+  }
+  
+}
+
+module.exports = Controller;
