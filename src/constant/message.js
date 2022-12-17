58015d98a9523858b73a053db4c15bb54421287a
2yunseong <dbsdltjd3701@naver.com>
2022-12-17 17:14:59 +0900

    refactor(message): 에러메시지와 시스템 메시지 리터럴 변수화

---
+++ src/constant/message.js
@@ -0,0 +1,14 @@
+const MESSAGE = {
+  ERROR: {
+    OUT_RANGE: '[ERROR]: 유효 범위에 맞지 않은 입력입니다.',
+    NOT_VALID: '[ERROR]: 올바르지 않는 입력입니다.',
+  },
+  INFO: {
+    START: '점심 메뉴 추천을 시작합니다.',
+    RESULT_TITLE: '메뉴 추천 결과입니다.',
+    RESULT_DAY: '[ 구분 | 월요일 | 화요일 | 수요일 | 목요일 | 금요일 ]',
+    FINISH: '추천을 완료했습니다.',
+  },
+};
+
+module.exports = MESSAGE;
