dfcc2eb6e49ae38d97b4cf2dffbf53c0ee926b36
AhnGeunTaek <dksrmsxor@gmail.com>
2022-12-17 15:24:40 +0900

    refactor: 메뉴 추천 받을 코치들의 이름 입력 받는 기능 모듈화

--- src/controllers/Controller.js
+++ src/controllers/Controller.js
@@ -1,8 +1,17 @@
-const OutputView = require('../views/OutputView');
+const { OutputView, InputView } = require('../views/View');
 
 class Controller {
   start() {
     OutputView.printRecommendStart();
+    this.getCoachNames();
+  }
+
+  getCoachNames() {
+    InputView.readCoachNames((coachName) => this.handleCoachNames(coachName));
+  }
+
+  handleCoachNames(coachName) {
+    console.log(`입력한 코치들은 ${coachName}입니다.`);
   }
 }
 
