341715027480c21a4461e66d8e7e9c1d2439ee9d
woo-jk <201702042@o.cnu.ac.kr>
2022-12-17 16:26:47 +0900

    feat: App 메뉴 추천 로직 구현

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
-const Coach = require("./Coach");
-const MenuManager = require("./MenuManager");
+const MissionUtils = require("@woowacourse/mission-utils");
+const Coach = require("./domain/Coach");
+const MenuManager = require("./domain/MenuManager");
+const RandomCategoryGenerator = require("./RandomCategoryGenerator");
 const Validator = require("./Validator");
 const InputView = require("./view/InputView");
 const OutputView = require("./view/OutputView");
-
-const SAMPLE = {
-  일식: "규동, 우동, 미소시루, 스시, 가츠동, 오니기리, 하이라이스, 라멘, 오코노미야끼",
-  한식: "김밥, 김치찌개, 쌈밥, 된장찌개, 비빔밥, 칼국수, 불고기, 떡볶이, 제육볶음",
-  중식: "깐풍기, 볶음면, 동파육, 짜장면, 짬뽕, 마파두부, 탕수육, 토마토 달걀볶음, 고추잡채",
-  아시안: "팟타이, 카오 팟, 나시고렝, 파인애플 볶음밥, 쌀국수, 똠얌꿍, 반미, 월남쌈, 분짜",
-  양식: "라자냐, 그라탱, 뇨끼, 끼슈, 프렌치 토스트, 바게트, 스파게티, 피자, 파니니",
-};
 
 class App {
   #menuManager;
@@ -57,10 +51,18 @@ class App {
 
     const nextCoach = this.#menuManager.getNextCoach(coach.getName());
     if (!nextCoach) {
-      console.log("wow");
+      this.makeRecommendedMenu();
       return;
     }
     this.requestBannedMenu(nextCoach);
+  }
+
+  makeRecommendedMenu() {
+    const randomCategorys = RandomCategoryGenerator.generateRandomCategorys();
+    this.#menuManager.setMenus(randomCategorys);
+    const coachsMenus = this.#menuManager.getCoachsMenus();
+    OutputView.printRecommendedMenu(randomCategorys, coachsMenus);
+    MissionUtils.Console.close();
   }
 
   handleError(callback, request) {
