47acd573bdc4a6f11ecc2d5cb7fe618d10ea27d2
ReturnReason <reasonz@daum.net>
2022-12-17 14:38:27 +0900

    feat(MenuController): 코치들이 못 먹는 메뉴를 입력받는 로직 추가

--- src/domain/MenuController.js
+++ src/domain/MenuController.js
@@ -4,6 +4,7 @@ const OutputView = require('../view/OutputView');
 
 class MenuController {
   #coachMembers;
+  #unableToEatMenu = [];
 
   constructor() {}
 
@@ -19,12 +20,71 @@ class MenuController {
         // 예외처리 : 코치 이름 2글자 ~ 4글자가 아닌 경우
         // 예외처리 : 코치 2명 이상, 5명 이하가 아닌 경우
         // 예외처리 : 콤마로 구분하지 않는 경우
+        this.getUnableToEatMenu();
       } catch (error) {
         console.log(error);
       }
     };
 
     InputView.readCoachName(callback);
+  }
+
+  getUnableToEatMenu() {
+    if (this.#coachMembers.length === 2) {
+      InputView.readCoachUnableEatMenu(this.#coachMembers[0], (menus) => {
+        this.#unableToEatMenu.push({ [this.#coachMembers[0]]: menus.split(',') });
+        InputView.readCoachUnableEatMenu(this.#coachMembers[1], (menus) => {
+          this.#unableToEatMenu.push({ [this.#coachMembers[1]]: menus.split(',') });
+          console.log(this.#unableToEatMenu);
+        });
+      });
+    }
+
+    // TODO : 리팩토링
+    if (this.#coachMembers.length === 3) {
+      InputView.readCoachUnableEatMenu(this.#coachMembers[0], (menus) => {
+        this.#unableToEatMenu.push({ [this.#coachMembers[0]]: menus });
+        InputView.readCoachUnableEatMenu(this.#coachMembers[1], (menus) => {
+          this.#unableToEatMenu.push({ [this.#coachMembers[1]]: menus });
+          InputView.readCoachUnableEatMenu(this.#coachMembers[2], (menus) => {
+            this.#unableToEatMenu.push({ [this.#coachMembers[2]]: menus });
+          });
+        });
+      });
+    }
+
+    if (this.#coachMembers.length === 4) {
+      InputView.readCoachUnableEatMenu(this.#coachMembers[0], (menus) => {
+        this.#unableToEatMenu.push({ [this.#coachMembers[0]]: menus });
+        InputView.readCoachUnableEatMenu(this.#coachMembers[1], (menus) => {
+          this.#unableToEatMenu.push({ [this.#coachMembers[1]]: menus });
+          InputView.readCoachUnableEatMenu(this.#coachMembers[2], (menus) => {
+            this.#unableToEatMenu.push({ [this.#coachMembers[2]]: menus });
+            InputView.readCoachUnableEatMenu(this.#coachMembers[3], (menus) => {
+              this.#unableToEatMenu.push({ [this.#coachMembers[3]]: menus });
+            });
+          });
+        });
+      });
+    }
+
+    if (this.#coachMembers.length === 5) {
+      InputView.readCoachUnableEatMenu(this.#coachMembers[0], (menus) => {
+        this.#unableToEatMenu.push({ [this.#coachMembers[0]]: menus });
+        InputView.readCoachUnableEatMenu(this.#coachMembers[1], (menus) => {
+          this.#unableToEatMenu.push({ [this.#coachMembers[1]]: menus });
+          InputView.readCoachUnableEatMenu(this.#coachMembers[2], (menus) => {
+            this.#unableToEatMenu.push({ [this.#coachMembers[2]]: menus });
+            InputView.readCoachUnableEatMenu(this.#coachMembers[3], (menus) => {
+              this.#unableToEatMenu.push({ [this.#coachMembers[3]]: menus });
+              InputView.readCoachUnableEatMenu(this.#coachMembers[4], (menus) => {
+                this.#unableToEatMenu.push({ [this.#coachMembers[4]]: menus });
+              });
+            });
+          });
+        });
+      });
+    }
   }
 }
 
