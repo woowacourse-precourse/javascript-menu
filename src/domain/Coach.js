fe717563deac05561d9a29a173afd0b8dae27b22
dladncks1217 <dlaxodud1217@gmail.com>
2022-12-17 14:17:53 +0900

    feat(Coach): Coach Class 구체화

--- src/domain/Coach.js
+++ src/domain/Coach.js
@@ -1,3 +1,25 @@
-class Coach {}
+class Coach {
+  #unableEatList;
+  #eatenFoods;
+
+  constructor() {
+    this.#unableEatList = [];
+    this.#eatenFoods = [];
+  }
+
+  hasUnableToEatList() {
+    if (this.#unableEatList.length) return true;
+    return false;
+  }
+
+  isAlreadyEatenTwice(menu) {
+    const food = this.#eatenFoods.slice();
+    food = food.filter((value) => value === menu);
+    if (food.length > 2) return true;
+    return false;
+  }
+
+  suggestMenu() {}
+}
 
 module.exports = Coach;
