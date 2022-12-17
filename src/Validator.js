301d1fd70f3fbf59c7475c801c252a55d14b8b3c
useon <yoosk4986@naver.com>
2022-12-17 14:15:07 +0900

    refactor: 입력값 검증 숫자 범위를 수정한다

--- src/Validator.js
+++ src/Validator.js
@@ -1,14 +1,17 @@
 const Validator = {
   name(names) {
     const namesArr = names.split(',');
-    if (!(namesArr.length > 0 || namesArr.length < 4)) {
+    if (!(namesArr.length > 1 && namesArr.length < 6)) {
       throw Error('[ERROR] 코치는 최소 2명, 최대 5명을 입력해야 합니다.');
     }
 
     for (let inputOrder = 0; inputOrder < namesArr.length; inputOrder++) {
-      if (!(namesArr[inputOrder].length > 0 && namesArr[inputOrder].length < 4)) {
+      let wrongInput = false;
+      if (!(namesArr[inputOrder].length > 1 && namesArr[inputOrder].length < 5)) {
+        wrongInput = true;
         throw Error('[ERROR] 코치의 이름은 최소 2글자, 최대 4글자를 입력해야 합니다.');
       }
+      if (wrongInput) break;
     }
   },
 };
