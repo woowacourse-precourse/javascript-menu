c0834d6bb69ce31f4055c9e7a95f47d091bf71c4
nangkyeonglim <nangkyeonglim@gmail.com>
2022-12-17 16:45:10 +0900

    feat(OutputView): 게임이 끝나고 콘솔을 닫는 메소드 구현

--- src/views/OutputView.js
+++ src/views/OutputView.js
@@ -5,12 +5,22 @@ const OutputView = {
     Console.print('점심 메뉴 추천을 시작합니다.');
   },
 
-  printResult(result) {
-    Console.print(`메뉴 추천 결과입니다.
-      [ 구분 | 월요일 | 화요일 | 수요일 | 목요일 | 금요일 ]
-      [ 카테고리 | 한식 | 한식 | 일식 | 중식 | 아시안 ]`);
-    Console.print(result);
+  printOneCoachMenu(name, menu) {
+    Console.print(`[ ${name} | ${menu.join(' | ')} ]`);
+  },
+
+  printResultTitle(weekCategories) {
+    Console.print('\n메뉴 추천 결과입니다.');
+    Console.print('[ 구분 | 월요일 | 화요일 | 수요일 | 목요일 | 금요일 ]');
+    Console.print(`[ 카테고리 | ${weekCategories.join(' | ')} ]`);
+  },
+
+  printEndMessage() {
     Console.print('\n추천을 완료했습니다.');
+  },
+
+  close() {
+    Console.close();
   },
 };
 
