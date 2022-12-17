02ffdb29afbe4f2c284fe0f8d5b509d164529728
Minhye Kang <alsgp9898@naver.com>
2022-12-17 13:38:36 +0900

    feat: 코치 이름이 2~4글자가 아니면 에러 발생

--- src/Validation.js
+++ src/Validation.js
@@ -1,7 +1,8 @@
 const Validation = {
   checkCoachNames(names) {
-    names = names.trim();
-    if (!names) throw new Error('아무것도 입력되지 않았습니다.');
+    if (!names.trim()) throw new Error('아무것도 입력되지 않았습니다.');
+    names = names.split(',')
+    if (names.some((name) => name.length < 2 || name.length > 4)) throw new Error('코치 이름은 최소 2글자, 최대 4글자여야 합니다.')
   }
 }
 
