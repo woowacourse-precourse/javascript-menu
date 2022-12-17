cb7a1b1a742adc3ac6565fddf16a6d4130c40707
curomame <curomame@naver.com>
2022-12-17 16:43:13 +0900

    ➕<Exception> [checkInputCoachNameListLength] add
    입력되는 코치의 수를 확인하는 기능 구현

--- src/utils/Exception.js
+++ src/utils/Exception.js
@@ -1,4 +1,12 @@
 const Exception = {
+
+  checkInputCoachNameListLength(nameList){
+    if (nameList.length < 2 || nameList.length > 5) {
+      return false;
+    }
+    return true;
+  },
+
   checkInputCoachNameLength(nameList) {
     nameList.forEach((name) => {
       if (name.length < 2 || name.length > 4) {
