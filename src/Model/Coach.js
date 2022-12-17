81c3999164b351cfc63bfe7cf85ead7196d6b9a3
kangyeongmin <kym6050@hanyang.ac.kr>
2022-12-17 15:59:03 +0900

    feat(Coach): 알맞은 카테고리에서 추천할 메뉴를 임의로 정한다

--- src/Model/Coach.js
+++ src/Model/Coach.js
@@ -1,3 +1,4 @@
+const MissionUtils = require("@woowacourse/mission-utils");
 const { MENU_SAMPLE } = require("../constant/value");
 
 class Coach {
@@ -12,11 +13,33 @@ class Coach {
     this.#menuOfWeek = [];
   }
 
-  getName() {}
-
   validatePickyMenu(pickyMenu) {
     if (pickyMenu.length > 2)
       throw new Error("[ERROR] 못 먹는 메뉴는 최대 2개까지만 입력 가능합니다.");
+  }
+
+  getName() {
+    return this.#name;
+  }
+
+  getPickyMenu() {
+    return this.#pickyMenu;
+  }
+
+  getMenuOfWeek() {
+    return this.#menuOfWeek;
+  }
+
+  randomMenu(categoryNumber) {
+    const category = Object.keys(MENU_SAMPLE)[categoryNumber - 1];
+    const menuArray = MENU_SAMPLE[category].split(", ");
+    const numberArray = menuArray.map((menu, index) => (menu = index));
+    const menuNumber = MissionUtils.Random.shuffle(numberArray)[0];
+    const menu = menuArray[menuNumber];
+    if (this.isPickyMenu(menu)) {
+      this.randomMenu(categoryNumber);
+    }
+    return menu;
   }
 }
 
