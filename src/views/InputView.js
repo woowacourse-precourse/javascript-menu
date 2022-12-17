a46a300532fef86249e695b385dedef761e9ccff
gabrielyoon7 <gabrielyoon7@gmail.com>
2022-12-17 13:42:16 +0900

    refactor: 변수의 상수화

--- src/views/InputView.js
+++ src/views/InputView.js
@@ -1,8 +1,9 @@
 const MissionUtils = require('@woowacourse/mission-utils');
+const { READ_MESSAGE } = require('../utils/constants');
 
 const InputView = {
   readCoaches(setCoaches) {
-    MissionUtils.Console.readLine('코치의 이름을 입력해 주세요. (, 로 구분)', (answer) => {
+    MissionUtils.Console.readLine(READ_MESSAGE.readCoaches, (answer) => {
       setCoaches(answer);
     });
   },
