3c1df8699a192ba3af9160464b959c12aa03f3ec
ming0830 <minsun0830@naver.com>
2022-12-17 17:39:07 +0900

    style: 코드 정리

--- src/Model/Menu.js
+++ src/Model/Menu.js
@@ -1,22 +1,24 @@
+const { Random } = require("@woowacourse/mission-utils");
+const { MENU_OBJ } = require("../Constant");
+
 class Menu {
-  constructor(){
-    this.userCategory = {}
-    this.categoryName = ['일식','한식','중식','아시안','양식']
-
+  constructor() {
+    this.userCategory = {};
+    this.categoryName = Object.keys(MENU_OBJ);
   }
 
-  getCategory(generate){
-    
-    if(generate() === 1) return this.categoryName[0]
-    if(generate() === 2) return this.categoryName[1]
-    if(generate() === 3) return this.categoryName[2]
-    if(generate() === 4) return this.categoryName[3]
-    if(generate() === 5) return this.categoryName[4]
+  getCategory(generate) {
+    if (generate() === 1) return this.categoryName[0];
+    if (generate() === 2) return this.categoryName[1];
+    if (generate() === 3) return this.categoryName[2];
+    if (generate() === 4) return this.categoryName[3];
+    if (generate() === 5) return this.categoryName[4];
   }
 
-  // getUseCategory(user,day,generate){
-  //   return [user, this.getCategory(day,generate)]
-  // }
+  getUserMenuCategory(category) {
+    const menuStr = MENU_OBJ[category].split(",") ;
+    return Random.shuffle(menuStr)[0];
+  }
 }
 
 module.exports = Menu;
