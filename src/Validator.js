2b9de36d3c3c434381867fd321dc12b16ba21556
woo-jk <201702042@o.cnu.ac.kr>
2022-12-17 13:46:55 +0900

    feat: Validator 코치 입력 검증 함수 구현

---
+++ src/Validator.js
@@ -0,0 +1,23 @@
+const Validator = {
+  validateCoachsLength(names) {
+    if (names.length < 2 || names.length > 5) {
+      throw new Error("[ERROR] 코치는 최소 2명 이상, 최대 5명 이하로 입력해야 합니다.");
+    }
+  },
+
+  validateCoachNamesLength(names) {
+    names.forEach((name) => {
+      if (name.length < 2 || name.length > 4) {
+        throw new Error("[ERROR] 코치의 이름은 최소 2글자 이상, 최대 4글자 이하로 입력해야 합니다.");
+      }
+    });
+  },
+
+  validateCoachNames(nameString) {
+    const names = nameString.split(",");
+    this.validateCoachsLength(names);
+    this.validateCoachNamesLength(names);
+  },
+};
+
+module.exports = Validator;
