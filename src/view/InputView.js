3eea2ed79e2b88a55524dce05844de1cd3d74a7a
wzrabbit <curious.wzrabbit@gmail.com>
2022-12-17 13:29:03 +0900

    feat(inputview): 코치가 못 먹는 메뉴 입력받는 기능 구현

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -5,6 +5,10 @@ const InputView = {
   readNames(callback) {
     Console.readLine(QUESTION.ENTER_NAMES, callback);
   },
+
+  readBanMenu(coachName, callback) {
+    Console.readLine(QUESTION.BAN_MENU(coachName), callback);
+  },
 };
 
 module.exports = InputView;
