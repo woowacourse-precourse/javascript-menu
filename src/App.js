6dec9966a18642624418ea1f68ec2d049accaaa1
2yunseong <dbsdltjd3701@naver.com>
2022-12-17 14:04:06 +0900

    feat(app): 코치들을 입력받는 기능

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
-const SAMPLE = {
-	일식: '규동, 우동, 미소시루, 스시, 가츠동, 오니기리, 하이라이스, 라멘, 오코노미야끼',
-	한식: '김밥, 김치찌개, 쌈밥, 된장찌개, 비빔밥, 칼국수, 불고기, 떡볶이, 제육볶음',
-	중식: '깐풍기, 볶음면, 동파육, 짜장면, 짬뽕, 마파두부, 탕수육, 토마토 달걀볶음, 고추잡채',
-	아시안:
-		'팟타이, 카오 팟, 나시고렝, 파인애플 볶음밥, 쌀국수, 똠얌꿍, 반미, 월남쌈, 분짜',
-	양식: '라자냐, 그라탱, 뇨끼, 끼슈, 프렌치 토스트, 바게트, 스파게티, 피자, 파니니',
-};
+const OutputUI = require('./view/OutputUI');
+const InputUI = require('./view/InputUI');
+const Vaildator = require('./util/vaildator');
+const Coach = require('./Coach');
+const InputProcessor = require('./util/InputProcessor');
 
 class App {
-  play() {}
+  constructor() {
+    this.output = new OutputUI();
+    this.input = new InputUI();
+    this.coachs = [];
+  }
+  play() {
+    this.output.print('점심 메뉴 추천을 시작합니다.');
+    this.input.readCoachNames(this.inputCoachs.bind(this));
+  }
+
+  inputCoachs(input) {
+    const coachNames = InputProcessor.parseCommaStringsToArray(input);
+
+    if (!coachNames.every(Vaildator.isVaildNameLength)) {
+      // 에러 처리
+    }
+
+    if (!Vaildator.isVaildCoachNumber(coachNames)) {
+      // 에러 처리
+    }
+
+    coachNames.forEach((coachname) => {
+      this.coachs.push(new Coach(coachname));
+    });
+  }
 }
 
 module.exports = App;
