01c84d16010f90e78b066cff4c7766cbf77abcdf
NaveOWO <pianowell96@gmail.com>
2022-12-17 13:22:45 +0900

    [ feat ] 서비스 시작문구 출력기능

--- src/OutputView.js
+++ src/OutputView.js
@@ -3,5 +3,9 @@ const { Console } = require("@woowacourse/mission-utils")
 const OutputView = {
     printNotEat(coach) {
         Console.print(`${coach}(이)가 못 먹는 메뉴를 입력해 주세요.`)
+    },
+
+    printStartMessage() {
+        Console.pirnt("점심 메뉴 추천을 시작합니다.")
     }
 }
