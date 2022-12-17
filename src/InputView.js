3101a0f59b3fa676c0b30bdf32f5039b1a2a614c
NaveOWO <pianowell96@gmail.com>
2022-12-17 17:32:02 +0900

    [ refactor ] 사용하지 않는 함수 제거

--- src/InputView.js
+++ src/InputView.js
@@ -6,13 +6,7 @@ const InputView = {
         Console.readLine("코치의 이름을 입력해 주세요. (, 로 구분)\n", (input) => {
             continueGame(input)
         })
-    },
-
-    inputNotEat(continueGame) {
-        Console.readLine("", (input) => {
-            continueGame(input)
-        })
-    },
+    }
 
 
 }
