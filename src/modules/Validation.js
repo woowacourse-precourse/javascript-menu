8745e9d35f2fa9e50522aa3e2982633a7873b430
Sungjae.Kwak <sungjae8970@gmail.com>
2022-12-17 15:29:37 +0900

    feat(validateDuplicateCategory): 주 2회 중복 카테고리 유효성검사
    - 주 2회 중복 카테고리 유효성검사실시

--- src/modules/Validation.js
+++ src/modules/Validation.js
@@ -9,6 +9,16 @@ const Validation = {
     if (name.length < 2 || name.length > 4) throw new Error('!');
     return;
   },
+  validateDuplicateCategory(categories) {
+    const board = {};
+    for (const category of categories) {
+      board[category] = (board[category] || 0) + 1;
+    }
+    for (let [category, count] of Object.entries(board)) {
+      if (count > 2) return false;
+    }
+    return true;
+  },
 };
 
 module.exports = Validation;
