6a761fe6b9fed8e62c75405e230777c51a53bebe
Q Kim <qoli0703@gmail.com>
2022-12-17 14:04:00 +0900

    feat(Recommend): add random menu selector
    ApplicationTest.js에서의 mockShuffles를 봤을 때
    섞어서 얻는 값은 숫자 - 즉 메뉴 리스트의 인덱스가 나와야 함

--- src/Recommend.js
+++ src/Recommend.js
@@ -6,6 +6,12 @@ const Recommend = {
     const categoryNumber = MissionUtils.Random.pickNumberInRange(1, MenuConfig.CATEGORIES + 1);
     return MenuConfig[categoryNumber];
   },
+
+  randomMenu(category, menu) {
+    const menuList = menu[category].trim().split(', ');
+    const menuNumber = MissionUtils.Random.shuffle(menuList)[0];
+    return menuList[menuNumber - 1];
+  },
 };
 
 module.exports = Recommend;
