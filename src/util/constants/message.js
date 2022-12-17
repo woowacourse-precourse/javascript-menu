bf2194c4dfdcc62db0267bc29561acb3f341b79c
thewronghand <penfreak77@gmail.com>
2022-12-17 13:27:59 +0900

    feat: 상수 객체 생성

--- src/util/constants/message.js
+++ src/util/constants/message.js
@@ -1,14 +1,15 @@
-const SAMPLE = {
-	일식: '규동, 우동, 미소시루, 스시, 가츠동, 오니기리, 하이라이스, 라멘, 오코노미야끼',
-	한식: '김밥, 김치찌개, 쌈밥, 된장찌개, 비빔밥, 칼국수, 불고기, 떡볶이, 제육볶음',
-	중식: '깐풍기, 볶음면, 동파육, 짜장면, 짬뽕, 마파두부, 탕수육, 토마토 달걀볶음, 고추잡채',
-	아시안:
-		'팟타이, 카오 팟, 나시고렝, 파인애플 볶음밥, 쌀국수, 똠얌꿍, 반미, 월남쌈, 분짜',
-	양식: '라자냐, 그라탱, 뇨끼, 끼슈, 프렌치 토스트, 바게트, 스파게티, 피자, 파니니',
+const { NUMBER } = require('./number');
+
+const MESSAGE = {
+  start: '점심 메뉴 추천을 시작합니다.\n',
+  readCoachNames: '코치의 이름을 입력해 주세요. (, 로 구분)\n',
+  readInedible: '(이)가 못 먹는 메뉴를 입력해 주세요.\n',
+  recommendResult: '메뉴 추천 결과입니다.\n',
+  recommendComplete: '\n추천을 완료했습니다.',
 };
 
-class App {
-  play() {}
-}
-
-module.exports = App;
+const ERROR = {
+  invalidName: `[ERROR] 코치의 이름은 최소 ${NUMBER.minName}글자, 최대 ${NUMBER.maxName}글자로 입력해야 하며, 인원은 최소 ${NUMBER.minMember}명, 최대 ${NUMBER.maxMember}명 입니다.`,
+  invalidInedible: `[ERROR] 먹지 못하는 메뉴는 최소 ${NUMBER.minInedible}개, 최대 ${NUMBER.maxInedible}개의 메뉴여야 합니다.`,
+};
+module.exports = { MESSAGE, ERROR };
