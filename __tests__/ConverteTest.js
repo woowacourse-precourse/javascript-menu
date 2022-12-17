4ae28d0afd9abed50fac1ac305d366f8f55b6b82
안호진 <asdv94@naver.com>
2022-12-17 14:03:19 +0900

    test(utils): convertToStringArray test

--- __tests__/ConverteTest.js
+++ __tests__/ConverteTest.js
@@ -1,14 +1,32 @@
 const { CATEGORY } = require('../src/Constants');
-const { convertNumberToCategory } = require('../src/utils/Converter');
+const {
+  convertNumberToCategory,
+  convertToStringArray,
+} = require('../src/utils/Converter');
 
 describe('변환자 테스트', () => {
   const categories = Object.values(CATEGORY)
     .slice(1)
     .map((category, i) => [i + 1, category]);
+
   test.each(categories)(
-    '숫자에 따른 카테고리 이름으로 변환해준다.',
+    '숫자에 따른 카테고리 이름으로 변환한다.',
     (no, category) => {
       expect(convertNumberToCategory(no)).toEqual(category);
+    }
+  );
+
+  const namesTestArray = [
+    ['토미,제임스,포코', ['토미', '제임스', '포코']],
+    ['우동,스시', ['우동', '스시']],
+  ];
+
+  test.each(namesTestArray)(
+    '입력 받은 문자열을 쉼표(,)로 구분해 배열로 변환한다.',
+    (names, expected) => {
+      expect(convertToStringArray(names)).toEqual(
+        expect.arrayContaining(expected)
+      );
     }
   );
 });
