3668fff49dfd750eef6d8cbdcb1f0c3c93bd417d
Q Kim <qoli0703@gmail.com>
2022-12-17 16:14:44 +0900

    feat(Config): change messages

--- src/Config.js
+++ src/Config.js
@@ -30,12 +30,12 @@ const ValidConfig = {
 
 const Message = {
   START: '점심 메뉴 추천을 시작합니다.',
-  RESULT: '메뉴 추천 결과입니다.',
-  FINISHED: '추천을 완료했습니다.',
+  RESULT: '\n메뉴 추천 결과입니다.',
+  FINISHED: '\n추천을 완료했습니다.',
 
-  ENTER_NAME: '코치의 이름을 입력해 주세요. (, 로 구분)\n',
+  ENTER_NAME: '\n코치의 이름을 입력해 주세요. (, 로 구분)\n',
   enterCannotEat(name) {
-    return `${name}(이)가 못 먹는 메뉴를 입력해 주세요.`;
+    return `\n${name}(이)가 못 먹는 메뉴를 입력해 주세요.\n`;
   },
 
   RESULT_DAY: '[ 구분 | 월요일 | 화요일 | 수요일 | 목요일 | 금요일 ]',
