8767384b2cbac50f5b2a82cfa02e5ec687d86b35
Minhye Kang <alsgp9898@naver.com>
2022-12-17 13:47:03 +0900

    feat: 중복된 코치의 이름이 있을 경우 에러 발생

--- src/Validation.js
+++ src/Validation.js
@@ -1,10 +1,15 @@
 const Validation = {
   checkCoachNames(names) {
-    if (!names.trim()) throw new Error('아무것도 입력되지 않았습니다.');
-    names = names.split(',')
-    if (names.some((name) => name.length < 2 || name.length > 4)) throw new Error('코치 이름은 최소 2글자, 최대 4글자여야 합니다.');
-    if (names.length < 2 || names.length >5) throw new Error('코치는 최소 2명 최대 5명이어야 합니다.')
-  }
-}
+    if (!names.trim()) throw new Error("아무것도 입력되지 않았습니다.");
+    names = names.split(",");
+    names = names.map((name) => name.trim())
+    if (names.some((name) => name.length < 2 || name.length > 4))
+      throw new Error("코치 이름은 최소 2글자, 최대 4글자여야 합니다.");
+    if (names.length < 2 || names.length > 5)
+      throw new Error("코치는 최소 2명 최대 5명이어야 합니다.");
+    if (new Set([...names]).size !== names.length)
+      throw new Error("중복된 코치 이름이 있습니다.");
+  },
+};
 
 module.exports = Validation;
