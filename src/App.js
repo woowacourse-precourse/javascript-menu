fc240f7e7f11eee9d0fd60189840a76f7ae1e074
동동이 <drdragon777@naver.com>
2022-12-17 15:39:40 +0900

    feat - add initial readCoachNotWantMenu method

--- src/App.js
+++ src/App.js
@@ -1,38 +1,54 @@
 const Coach = require("./model/Coach.js");
+const CouchMenu = require("./model/CouchMenu.js");
 const { makeCategory } = require("./utils/CategoryMaker.js");
 const InputView = require("./view/InputView.js");
 const OutputView = require("./view/OutputView.js");
 
-const SAMPLE = {
-  일식: "규동, 우동, 미소시루, 스시, 가츠동, 오니기리, 하이라이스, 라멘, 오코노미야끼",
-  한식: "김밥, 김치찌개, 쌈밥, 된장찌개, 비빔밥, 칼국수, 불고기, 떡볶이, 제육볶음",
-  중식: "깐풍기, 볶음면, 동파육, 짜장면, 짬뽕, 마파두부, 탕수육, 토마토 달걀볶음, 고추잡채",
-  아시안:
-    "팟타이, 카오 팟, 나시고렝, 파인애플 볶음밥, 쌀국수, 똠얌꿍, 반미, 월남쌈, 분짜",
-  양식: "라자냐, 그라탱, 뇨끼, 끼슈, 프렌치 토스트, 바게트, 스파게티, 피자, 파니니",
-};
-
 class App {
+  #coachNames = [];
+  #coachMenus = [];
   #categories;
 
   play() {
-    this.#categories = makeCategory();
+    // this.#categories = makeCategory();
     OutputView.printStart();
-    this.readCoachNames();
+    this.#readCoachNames();
   }
 
-  readCoachNames() {
-    InputView.readCoachNames(this.readCoachNamesCallback);
+  #readCoachNames() {
+    InputView.readCoachNames(this.#readCoachNamesCallback);
   }
 
-  readCoachNamesCallback = (names) => {
+  #readCoachNamesCallback = (names) => {
     try {
-      const coachNames = new Coach(names);
+      this.#coachNames = new Coach(names).getData();
+      this.#coachMenus = new Array(names.length);
+      this.#readAllCouchNotWantMenu();
     } catch (err) {
       OutputView.printError(err.message);
-      this.readCoachNames();
+      this.#readCoachNames();
     }
   };
+
+  #readAllCouchNotWantMenu() {
+    this.#readCoachNotWantMenu(this.#coachNames[0], 0);
+  }
+
+  #readCoachNotWantMenu(coachName, idx) {
+    InputView.readCoachNotWantMenu(coachName, (notWantMenu) => {
+      try {
+        this.#coachMenus[idx] = new CouchMenu(
+          coachName,
+          notWantMenu
+        ).getNotWantMenu();
+        if (idx + 1 < this.#coachNames.length)
+          this.#readCoachNotWantMenu(this.#coachNames[idx + 1], idx + 1);
+      } catch (err) {
+        OutputView.printError(err.message);
+        this.#readCoachNotWantMenu(coachName, idx);
+      }
+    });
+  }
 }
 
 module.exports = App;
