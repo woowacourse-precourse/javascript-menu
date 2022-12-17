2face9089f8ff27b5fbadc90396a2c19a8f93c4e
inyeong-kang <tomatorkd@naver.com>
2022-12-17 17:33:21 +0900

    style: Erase unncessay notes

--- src/App.js
+++ src/App.js
@@ -8,96 +8,87 @@ const OutputView = require("./View/OutputView");
 const { CATEGORY, SAMPLE } = require("./utils/Menu");
 const InputValidation = require("./utils/Validate");
 
+//App -> Controller로 사용
 class App {
 
   constructor() {
-	this.personCnt = 0;
-	this.nameList = [];
-	this.inEdible = {};
-	this.menuList = [];
-	this.categories = [];
+	this.personCnt = 0; //코치님 한 명씩 먹을 수 없는 메뉴를 받기 위해 필요한, 카운트용 변수
+	this.nameList = []; //코치님 성함들
+	this.inEdible = {}; //코치님별로 먹을 수 없는 메뉴
+	this.menuList = []; //코치님들이 월요일-일요일 먹을 메뉴
+	this.categories = []; //카테고리별 총 메뉴들 
   }
 
+  //프로그램 시작
   play() {
 	OutputView.printStartMsg();
 	this.askName();
   }
-  
+  //코치님 이름 입력받기 
   askName() {
 	InputView.readName((names)=>{
 		const nameList = names.split(',');
 		InputValidation.isValidName(nameList); //유효성 검사
-		this.nameList = nameList; //누구인지 저장해두기
-		//this.personCnt = nameList.length; //총 몇명인지 카운트
-		//console.log(nameList, this.personCnt);	
+		this.nameList = nameList; //코치님 이름 목록 저장해두기
 		this.askMenu(nameList[this.personCnt]);	
 	});
   }
+  //코치님 한 명씩 먹을 수 없는 메뉴 입력받기
   askMenu(name) {
 	InputView.readMenu(name, (menus)=> {
 		const menuList = menus.split(',');
 		InputValidation.isValidMenu(menuList); //유효성 검사
-		//console.log(name,menuList);	
-		this.inEdible[name] = menuList; //딕셔너리에 사람마다 못먹는 메뉴 저장
-		//console.log(this.inEdible); 
+		this.inEdible[name] = menuList; //객체에 사람마다 못먹는 메뉴 저장
 		this.personCnt += 1;
-		
 		if(this.personCnt == this.nameList.length) { //모든 사람들의 못먹는 메뉴를 알아봤다!
 			this.categories = this.selectCategory();
-			console.log(this.categories);
 			this.menuList = this.recommendMenu(); 
-			//console.log(this.menuList); 
 			return this.showRecommendation();
 		} 
 		return this.askMenu(this.nameList[this.personCnt]);
 	});
   }
 
-  //요일별 메뉴 카테고리 설정하기
+  //요일별 메뉴 카테고리 선정하기
   selectCategory() {
 	let categories = [];
 	const numberArr = [];
 
-	while(numberArr.length < 5) {
+	while(numberArr.length < 5) { //5 -> 월요일-일요일 총 요일 5개라서
 		const randomNum = this.pickRandomNum();
-		//console.log(randomNum);
 		if(this.checkAboveTwice(randomNum, numberArr)) continue;
 		else numberArr.push(randomNum);
 	}
-
 	categories = numberArr.map((element) => {
 		return CATEGORY[element];
 	})
-	//console.log('카테고리는:', categories);
-	return categories;
+	return categories; //카테고리별 메뉴 리스트 반환하기
   }
-
+  //임의로 메뉴를 선정하기
   pickRandomNum() {
 	return Random.pickNumberInRange(1, 5);
   }
 
+  //한 주에 같은 카테고리는 최대 2회까지만 고를 수 있다.
   checkAboveTwice(number, arr) {
 	let count = arr.filter(element => number === element).length;
 	if(count == 2) return true;
 	return false;
   }
   
+  //메뉴 추천하기
   recommendMenu() {
-	//console.log(this.inEdible);
 	let menuList = [];
 	for(let i=0; i<this.nameList.length;i++){
 		const name = this.nameList[i];
 		let everyDayMenu = [];
 
 		for(let j=0; j<this.categories.length; j++) { //요일별로 순회
-			//let menusForU = []; //오늘 요일에 먹을 수 있는 메뉴들을 저장하자
 			const todayCategory = this.categories[j];//오늘의 카테고리는~~
 			const menuForCategory = SAMPLE[todayCategory].split(', '); //해당 카테고리의 메뉴 모음 리스트
-			//console.log("메뉴카테고리",menuForCategory);
 			const todayMenu = this.selectRandomMenu(everyDayMenu, menuForCategory, name); //당신을 위한 오늘의 메뉴는~~
 			everyDayMenu.push(todayMenu); //오늘 요일의 메뉴는..	
 		}
-		//console.log(everyDayMenu);
 		menuList.push(everyDayMenu); //각 코치의 월요일-금요일 식단을 저장
 	}
 	return menuList;
@@ -105,12 +96,10 @@ class App {
 
   // 주어진 배열에서 요소 1개를 랜덤하게 골라 반환하는 함수
   selectRandomMenu(everyDayMenu, menuForCategory, name) {
-	//console.log(menuForCategory.length);
 	const stringToNum = menuForCategory.map((element, index) => {
-		return index+1;
+		return index + 1;
 	})
 	const randomNum = Random.shuffle(stringToNum)[0];
-	//console.log(randomMenu);
 	const randomMenu = menuForCategory[randomNum-1];
 	if(this.inEdible[name].includes(randomMenu)) return this.selectRandomMenu(everydayMenu, menuForCategory, name); //먹을 수 없는 메뉴이면 다시 고르기!
 	if(everyDayMenu.includes(randomMenu)) return this.selectRandomMenu(everyDayMenu, menuForCategory, name); //메뉴 중복 고려해야함!!!!!!!
@@ -118,13 +107,13 @@ class App {
 	return randomMenu;
   }
 
+  //메뉴 추천 결과 보여주기
   showRecommendation() {
 	this.categories.unshift('카테고리');
 	for(let i=0; i<this.menuList.length; i++){
 		this.menuList[i].unshift(this.nameList[i]);
 	}
 	OutputView.printRecommendation(this.categories, this.menuList);
-	//console.log("hi recommend!");
 	return this.quit();
   }
 
@@ -134,8 +123,7 @@ class App {
   }
 
 }
-//토미,제임스,포코
-//tomi,james,poco
+
 const app = new App();
 app.play();
 
