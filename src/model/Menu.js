b4c1e29194062c20ddbd5e97cf567b3416866876
curomame <curomame@naver.com>
2022-12-17 14:56:48 +0900

    ➕ feat - <Menu> [checkDuplicatedCategory] add
    카테고리 추천을 특정 횟수 이상 했는지 확인하는 메서드 구현

--- src/model/Menu.js
+++ src/model/Menu.js
@@ -1,11 +1,23 @@
 class Menu {
-  #recommendedCategoryList
+  #recommendedCategoryList;
 
-  constructor(){
-    this.#recommendedCategoryList = [];  
+  constructor() {
+    this.#recommendedCategoryList = [];
   }
 
-  
+  checkDuplicatedCategory(category, count) {
+    const checkedCount = 0;
+    this.#recommendedCategoryList.forEach((recommendedCategory) => {
+      if (recommendedCategory === category) {
+        checkedCount += 1;
+      }
+    });
+    if(checkedCount >= count){
+      return false;
+    }
+    return true;
+  }
+
 
 }
 
