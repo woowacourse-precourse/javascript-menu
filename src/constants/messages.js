53ce81823e3798a81b97ab0f7d033002c1559ee6
ReturnReason <reasonz@daum.net>
2022-12-17 13:52:30 +0900

    feat(messages): 인풋 메시지 상수 추가

--- src/constants/messages.js
+++ src/constants/messages.js
@@ -2,6 +2,11 @@ const OUTPUT_MESSAGES = Object.freeze({
   start: '점심 메뉴 추천을 시작합니다.\n',
 });
 
+const INPUT_MESSAGES = Object.freeze({
+  name: '코치의 이름을 입력해 주세요. (, 로 구분)\n',
+});
+
 module.exports = {
   OUTPUT_MESSAGES,
+  INPUT_MESSAGES,
 };
