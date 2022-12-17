339834b66cb3b1783596bdeed4af3cfa4f53173d
unsnruu <unsnruu@gmail.com>
2022-12-17 14:35:50 +0900

    feat : Category와 관련된 사항을 구현

---
+++ src/model/category.js
@@ -0,0 +1,16 @@
+const { Random } = require("@woowacourse/mission-utils");
+
+//todo : 코치가 싫어하는 음식을 제외하고 추천해주는 로직을 여기서 처리할 지 고민해 봐야 한다.
+class Category {
+  #name;
+  #menus;
+
+  constructor(name, menus) {
+    this.#name = name;
+    this.#menus = menus;
+  }
+
+  recommend() {
+    return Random.shuffle(menus)[0];
+  }
+}
