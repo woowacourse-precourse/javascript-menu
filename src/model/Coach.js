8eeda13a0c14c6776399fec629dda2d9483b51a2
shackstack <kms1061@naver.com>
2022-12-17 13:42:13 +0900

    feat(Coach): 예외처리 메소드 분리

--- src/model/Coach.js
+++ src/model/Coach.js
@@ -8,9 +8,17 @@ class Coach {
   validate(names) {
     const coaches = names.split(",");
 
+    this.isValidPersonnel(coaches);
+    this.isValidNameLength(coaches);
+
+    this.#coaches = coaches;
+  }
+  isValidPersonnel(coaches) {
     if (!(coaches.length >= 2 && coaches.length <= 5)) {
       throw new Error("[ERROR] 코치 인원은 최소 2명, 최대 5명이어야 합니다");
     }
+  }
+  isValidNameLength(coaches) {
     coaches.forEach((name) => {
       if (!(name.length >= 2 && name.length <= 4)) {
         throw new Error(
