5f7a18cf56a2b36b73a6fd87bab05ed657d03cd8
curomame <curomame@naver.com>
2022-12-17 14:30:20 +0900

    ➕ feat - <Coach> [createCoachList] add
    Coach의 이름들을 인자로 받아 coachList를 생성하는 매서드 구현

--- src/model/Coach.js
+++ src/model/Coach.js
@@ -1,10 +1,25 @@
 class Coach {
   #coachList;
 
-  constructor() {
+  constructor() {}
+
+  /**
+   * 다리 건너기 게임을 관리하는 클래스
+   * @param {string} coachNames ',로 구분된 코치 이름 문자'
+   * @return {{name,inedibleMenuList,recommendedMenuList}[]}
+   */
+  createCoachList(coachNames) {
+    const coachList = [];
+    const coachNameList = coachNames.split(",");
+    coachNameList.forEach((coachName) => {
+      coachList.push({
+        name: coachName,
+        inedibleMenuList: [],
+        recommendedMenuList: [],
+      });
+    });
+    this.#coachList = coachList;
   }
-
-
 }
 
 module.exports = Coach;
