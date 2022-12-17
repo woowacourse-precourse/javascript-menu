a545c9020b9284526042d7ef903b6c7e5bbf9438
solo5star <solo_5@naver.com>
2022-12-17 14:17:48 +0900

    feat(domains): 메뉴 class 구현

---
+++ src/domains/Menu.js
@@ -0,0 +1,15 @@
+class Menu {
+  /** @type {string} */
+  #name;
+
+  /**
+   * @param {string} name
+   */
+  constructor(name) {
+    this.#name = name;
+  }
+
+  getName() {
+    return this.#name;
+  }
+}
