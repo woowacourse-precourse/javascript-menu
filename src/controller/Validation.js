ea17f4bfa5f77719859507287ae301e72deb8e93
daal2quf <daal2quf@gmail.com>
2022-12-17 14:07:17 +0900

    feat(validation): 입력한 코치의 명수가 2명이상이 아닐때 예외처리 기능 추가

--- src/controller/Validation.js
+++ src/controller/Validation.js
@@ -1,7 +1,11 @@
 const Validation = {
   ofCoachName(userInput) {
-    if (!userInput.split(',').every((name) => name.length >= 2 && name.length <= 4))
+    if (!userInput.split(',').every((name) => name.length >= 2 && name.length <= 4)) {
       throw new Error('[ERROR] 코치의 이름은 2글자 이상 4글자 이하로만 입력해주세요');
+    }
+    if (userInput.split(',').length < 2) {
+      throw new Error('[ERROR] 코치는 최소 2명 이상 입력해야 합니다.');
+    }
   },
 };
 
