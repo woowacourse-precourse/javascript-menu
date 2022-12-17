152dee4c3de8701988dc00dd5759dba952278313
rayark1 <max980203@snu.ac.kr>
2022-12-17 15:06:40 +0900

    feat(src/InputView.js): 코치가 못먹는 음식을 입력 받는 readCoachDisableMenu() 구현

--- src/InputView.js
+++ src/InputView.js
@@ -3,10 +3,14 @@ const { Console } = require("@woowacourse/mission-utils");
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 const InputView = {
-    MESSAGE_INPUT_COACH_NAME:'코치의 이름을 입력해 주세요. (, 로 구분)',
+    MESSAGE_INPUT_COACH_NAME: '코치의 이름을 입력해 주세요. (, 로 구분)',
+    MESSAGE_INPUT_COACH_DISABLE_MENU: '(이)가 못 먹는 메뉴를 입력해 주세요.',
     readCoachName(callback) {
       Console.readLine(`${InputView.MESSAGE_INPUT_COACH_NAME}`, callback);
     },
+    readCoachDisableMenu(name, callback) {
+      Console.readLine(`${name}${InputView.MESSAGE_INPUT_COACH_DISABLE_MENU}`, callback);
+    }
 };
 
 module.exports = InputView;
