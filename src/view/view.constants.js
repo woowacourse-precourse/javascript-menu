48c4284ed9524d72eb79429877c9abd7fe2fb76e
동동이 <drdragon777@naver.com>
2022-12-17 13:50:36 +0900

    chore - add new line at view constants

--- src/view/view.constants.js
+++ src/view/view.constants.js
@@ -1,8 +1,8 @@
 const MESSAGE = Object.freeze({
-  SERVICE_START_MESSAGE: "점심 메뉴 추천을 시작합니다.",
+  SERVICE_START_MESSAGE: "점심 메뉴 추천을 시작합니다.\n\n",
   SERVICE_END_MESSAGE: "추천을 완료했습니다.",
   MENU_RECOMMENDATION_RESULT: "메뉴를 추천 결과입니다.",
-  REQUEST_COACH_NAME: "코치 이름을 입력해 주세요. (, 로 구분)",
+  REQUEST_COACH_NAME: "코치 이름을 입력해 주세요. (, 로 구분)\n",
   REQUEST_NOT_WANT_MENU: "(이)가 못 먹는 메뉴를 입력해 주세요.",
 });
 
