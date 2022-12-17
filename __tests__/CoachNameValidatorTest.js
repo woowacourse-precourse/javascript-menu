700fca5daec69fee2ed2ddfb55f896405b96f0fc
Suyoung <jsy8492@gmail.com>
2022-12-17 16:00:52 +0900

    test: 코치 이름에 대한 유효성을 검사하는 테스트 작성

---
+++ __tests__/CoachNameValidatorTest.js
@@ -0,0 +1,23 @@
+const CoachNameValidator = require('../src/CoachNameValidator');
+
+describe('코치 이름에 대한 입력값의 유효성을 검사하는 객체 테스트', () => {
+  test.each([
+    [''],
+    ['제임스제임'],
+    ['준'],
+    ['토미'],
+    ['토미,'],
+    ['토미,제임스제임'],
+    ['토미,토미'],
+    ['토미,제임스,포코,포비,제이슨,공원'],
+  ])('유효하지 않은 코치 이름 입력값인 경우 예외 처리', input => {
+    expect(() => CoachNameValidator.validate(input)).toThrow('[ERROR]');
+  });
+
+  test.each([['토미,제임스딘'], ['토미,제임스,포코'], ['토미,제임스,포코,포비,공원']])(
+    '유효한 코치 이름 입력값인 경우 정상 동작',
+    input => {
+      expect(() => CoachNameValidator.validate(input)).not.toThrow('[ERROR]');
+    },
+  );
+});
