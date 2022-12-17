1da882f7f9678fc570e028933ba5faa58a8fa67a
ShimSungBo <shimdb96@gmail.com>
2022-12-17 14:27:06 +0900

    feat:(menu) 에러 발생에 대한 예외처리 구현 1. 에러가 발생시 발생한 지점부터 다시 입력 받기

--- src/InputView.js
+++ src/InputView.js
@@ -6,7 +6,8 @@ const InputView = {
     Console.readLine(
       '\n코치의 이름을 입력해 주세요. (, 로 구분)\n',
       (names) => {
-        Validation.isValidInput(names);
+        const error = Validation.isValidInput(names);
+        if (error) return this.readCoachName();
         Console.print(names);
       }
     );
--- src/Validation.js
+++ src/Validation.js
@@ -1,3 +1,5 @@
+const { Console } = require('@woowacourse/mission-utils');
+
 const Validation = {
   isValidInput(names) {
     const nameSize = this.isValidName(names);
@@ -10,17 +12,28 @@ const Validation = {
   },
 
   isValidName(names) {
-    return names.split(',').forEach((item) => {
-      if (item.length < 2 || item.length > 4) {
-        throw new Error('[ERROR] 이름은 최소2글자 ~ 최대 4글자 입니다.');
-      }
-    });
+    try {
+      return names.split(',').forEach((item) => {
+        if (item.length < 2 || item.length > 4) {
+          throw new Error('[ERROR] 이름은 최소2글자 ~ 최대 4글자 입니다.');
+        }
+      });
+    } catch (error) {
+      Console.print(error.message);
+      return true;
+    }
   },
 
   isValidNumbersOfPeople(names) {
     const numbersOfPeople = names.split(',');
-    if (numbersOfPeople.length < 2 || numbersOfPeople.length > 5) {
-      throw new Error('[ERROR] 인원은 최소 2명 최대 5명입니다.');
+
+    try {
+      if (numbersOfPeople.length < 2 || numbersOfPeople.length > 5) {
+        throw new Error('[ERROR] 코치의 인원은 최소 2명 최대 5명입니다.');
+      }
+    } catch (error) {
+      Console.print(error.message);
+      return true;
     }
   },
 };
