c791565b41ecc3ceda1c8fc4f81d4e0b84bfcb31
cruelladevil <dev.timetravel@gmail.com>
2022-12-17 13:44:48 +0900

    feat: validate coach names

--- src/View/InputView.js
+++ src/View/InputView.js
@@ -8,7 +8,20 @@ const InputView = {
     InputView.question(InputView.COACH_NAMES, (input) => {
       const coachNames = input.split(',');
 
+      InputView.validateCoachNames(coachNames);
       callback(coachNames);
+    });
+  },
+
+  validateCoachNames(coachNames) {
+    if (coachNames.length < 2 || coachNames.length > 5) {
+      throw new Error('코치는 최소 2명부터 최대 5명 입력 가능합니다.');
+    }
+
+    coachNames.forEach((name) => {
+      if (name.length < 2 || name.length > 4) {
+        throw new Error('코치 이름은 2글자부터 4글자까지 입력 가능합니다.');
+      }
     });
   },
 
