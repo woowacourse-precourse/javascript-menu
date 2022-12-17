24ff5e2e4331ec41a565de1355b4e54f80fcb9be
안호진 <asdv94@naver.com>
2022-12-17 15:39:29 +0900

    feat(utils): impl checkCategoryTwice

--- src/utils/Checker.js
+++ src/utils/Checker.js
@@ -1,5 +1,18 @@
+const reduceFuncInCategory = (counter, number) => {
+  counter[number] = (counter[number] ?? 0) + 1;
+  return counter;
+};
+
 const Checker = {
-  checkCategoryTwice() {},
+  /**
+   * 새 카테고리 번호가 2번 이상 선택 되었는지 확인한다.
+   * @param {number[]} history 카테고리 이력
+   * @param {number} category 새 카테고리 번호
+   * @returns {boolean}
+   */
+  checkCategoryTwice(history, category) {
+    return history.reduce(reduceFuncInCategory, {})[category] >= 2;
+  },
 
   checkDuplicateFood() {},
 };
