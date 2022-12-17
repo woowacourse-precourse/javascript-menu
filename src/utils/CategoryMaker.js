f7c40026dafa6e5c5c496eb0999e1b5625afa3ca
Leejin-Yang <ejin1996@gmail.com>
2022-12-17 16:23:59 +0900

    fix: 중복 카테고리 수정

--- src/utils/CategoryMaker.js
+++ src/utils/CategoryMaker.js
@@ -1,13 +1,21 @@
 const { CATEGORIES } = require('../constants');
 
 const CategoryMaker = {
+  initCount: {
+    1: 0,
+    2: 0,
+    3: 0,
+    4: 0,
+    5: 0,
+  },
+
   makeCategories(generateRandomNumber) {
-    let prev = 0;
     const categories = new Array(5).fill(0).map(() => {
       let number = generateRandomNumber();
-      while (prev === number) {
+      while (CategoryMaker.initCount[number] > 2) {
         number = generateRandomNumber();
       }
+      CategoryMaker.initCount[number] += 1;
       return CATEGORIES[number - 1];
     });
     return categories;
