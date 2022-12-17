4416d1c863fac808bb17234cca7e101ef1308529
kknyapple <kknyapple@gmail.com>
2022-12-17 15:57:38 +0900

    feat: shuffle을 이용해 메뉴 추천

--- src/MenuRecommendation.js
+++ src/MenuRecommendation.js
@@ -5,12 +5,6 @@ const { checkCoachNumber } = require("./validation");
 const MissionUtils = require("@woowacourse/mission-utils");
 
 const categories = {
-  일식: "규동, 우동, 미소시루, 스시, 가츠동, 오니기리, 하이라이스, 라멘, 오코노미야끼",
-  한식: "김밥, 김치찌개, 쌈밥, 된장찌개, 비빔밥, 칼국수, 불고기, 떡볶이, 제육볶음",
-  중식: "깐풍기, 볶음면, 동파육, 짜장면, 짬뽕, 마파두부, 탕수육, 토마토 달걀볶음, 고추잡채",
-  아시안:
-    "팟타이, 카오 팟, 나시고렝, 파인애플 볶음밥, 쌀국수, 똠얌꿍, 반미, 월남쌈, 분짜",
-  양식: "라자냐, 그라탱, 뇨끼, 끼슈, 프렌치 토스트, 바게트, 스파게티, 피자, 파니니",
   get(number) {
     if (number === 1) return "일식";
     if (number === 2) return "한식";
@@ -19,6 +13,67 @@ const categories = {
     if (number === 5) return "양식";
   },
 };
+
+const menus = {
+  일식: [
+    "규동",
+    "우동",
+    "미소시루",
+    "스시",
+    "가츠동",
+    "오니기리",
+    "하이라이스",
+    "라멘",
+    "오코노미야끼",
+  ],
+  한식: [
+    "김밥",
+    "김치찌개",
+    "쌈밥",
+    "된장찌개",
+    "비빔밥",
+    "칼국수",
+    "불고기",
+    "떡볶이",
+    "제육볶음",
+  ],
+  중식: [
+    "깐풍기",
+    "볶음면",
+    "동파육",
+    "짜장면",
+    "짬뽕",
+    "마파두부",
+    "탕수육",
+    "토마토",
+    "달걀볶음",
+    "고추잡채",
+  ],
+  아시안: [
+    "팟타이",
+    "카오 팟",
+    "나시고렝",
+    "파인애플 볶음밥",
+    "쌀국수",
+    "똠얌꿍",
+    "반미",
+    "월남쌈",
+    "분짜",
+  ],
+  양식: [
+    "라자냐",
+    "그라탱",
+    "뇨끼",
+    "끼슈",
+    "프렌치 토스트",
+    "바게트",
+    "스파게티",
+    "피자",
+    "파니니",
+  ],
+};
+
+const catList = [];
 
 const category = [];
 
@@ -37,25 +92,43 @@ class MenuRecommendation {
     this.receiveCat();
   }
 
-  chooseCat() {
-    while (category.length < 5) {
-      let random = categories.get(MissionUtils.Random.pickNumberInRange(1, 5));
-      if (category.includes(random)) {
-        if (new Set(random).size + 1 === category.length) {
-          category.push(random);
-        }
-      } else if (!category.includes(random)) category.push(random);
+  chooseMenu() {
+    for (let i = 0; i < 5; i++) {
+      let todayCat = category[i];
+
+      let numArr = [];
+      for (let i = 0; i < menus[todayCat].length; i++) {
+        numArr.push(i);
+      }
+
+      const menu = MissionUtils.Random.shuffle(numArr)[0];
+
+      console.log(menus[todayCat][menu]);
     }
 
     this.receiveResult();
   }
 
+  chooseCat() {
+    while (category.length < 5) {
+      let randomNum = MissionUtils.Random.pickNumberInRange(1, 5);
+      let random = categories.get(randomNum);
+
+      if (category.includes(random)) {
+        if (new Set(random).size + 1 === category.length) {
+          category.push(random);
+          // catList.push(randomNum);
+        }
+      } else if (!category.includes(random)) {
+        category.push(random);
+        // scatList.push(randomNum);
+      }
+    }
+    // console.log(catList);
+    this.chooseMenu();
+  }
+
   receiveDislikeMenu(menus) {
-    // console.log(this.coachs.length);
-    // console.log(this.count);
-
-    // console.log(this.coachs[this.count].getName());
-
     if (this.count + 1 === this.coachs.length) {
       console.log(this.coachs);
       this.chooseCat();
