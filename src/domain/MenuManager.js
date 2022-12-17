56bf23edc33d40e391b142e7016c90d35a9a5267
woo-jk <201702042@o.cnu.ac.kr>
2022-12-17 17:09:07 +0900

    refactor: MenuManager coachs -> coaches 변수명 변경

--- src/domain/MenuManager.js
+++ src/domain/MenuManager.js
@@ -2,20 +2,20 @@ const MENU_LIST = require("../MENU_LIST");
 const shuffleArray = require("../util/shuffleArray");
 
 class MenuManager {
-  #coachs;
+  #coaches;
 
-  constructor(coachs) {
-    this.#coachs = coachs;
+  constructor(coaches) {
+    this.#coaches = coaches;
   }
 
   getFirstCoach() {
-    return this.#coachs[0];
+    return this.#coaches[0];
   }
 
   getNextCoach(name) {
-    for (let i = 0; i < this.#coachs.length; i++) {
-      if (i === this.#coachs.length - 1) return false;
-      if (this.#coachs[i].getName() === name) return this.#coachs[i + 1];
+    for (let i = 0; i < this.#coaches.length; i++) {
+      if (i === this.#coaches.length - 1) return false;
+      if (this.#coaches[i].getName() === name) return this.#coaches[i + 1];
     }
   }
 
@@ -28,13 +28,13 @@ class MenuManager {
   }
 
   setMenus(categorys) {
-    this.#coachs.forEach((coach) => {
+    this.#coaches.forEach((coach) => {
       categorys.forEach((category) => this.addMenuToCoach(coach, category));
     });
   }
 
   getMenusByCoaches() {
-    return this.#coachs.map((coach) => ({ name: coach.getName(), menus: coach.getMenus() }));
+    return this.#coaches.map((coach) => ({ name: coach.getName(), menus: coach.getMenus() }));
   }
 }
 
