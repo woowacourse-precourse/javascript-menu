11ccc815c660a641f515976b65f6e45f3febec4e
2pandi <coder.2pandi@gmail.com>
2022-12-17 13:53:38 +0900

    feat(Controller): add name asking logic

--- src/Controller.js
+++ src/Controller.js
@@ -1,9 +1,21 @@
-const { MESSAGE } = require("./constants");
+const { MESSAGE, SYMBOL } = require("./constants");
+const InputView = require("./views/InputView");
 const OutputView = require("./views/OutputView");
 
 class Controller {
   start() {
     OutputView.printMessage(MESSAGE.START);
+    this.askName();
+  }
+
+  askName() {
+    const callback = (input) => {
+      // 유효성검사
+      const namesArr = input.split(SYMBOL.COMMA);
+      console.log(namesArr);
+    };
+
+    InputView.readInput(MESSAGE.ASK_NAME, callback);
   }
 }
 module.exports = Controller;
