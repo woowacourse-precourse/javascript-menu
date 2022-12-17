41976dd22cb412c3946a7468d68ada0b8388cc70
Hanbeul You <109324631+hanbeulYou@users.noreply.github.com>
2022-12-17 14:42:23 +0900

    feat: 못 먹는 메뉴 입력

--- src/App.js
+++ src/App.js
@@ -9,23 +9,95 @@ const SAMPLE = {
 	양식: '라자냐, 그라탱, 뇨끼, 끼슈, 프렌치 토스트, 바게트, 스파게티, 피자, 파니니',
 };
 
-const BannedMenu = {}
+const CATEGORIES = new Map()
+CATEGORIES.set(1, '일식')
+CATEGORIES.set(2, '한식')
+CATEGORIES.set(3, '중식')
+CATEGORIES.set(4, '아시안')
+CATEGORIES.set(5, '양식')
 
-function cantEat(names) {
-  names.forEach((name) => {
-    BannedMenu[name] = ''
-  })
+const BannedMenu = {}
+const Names = []
+const Categories = []
+
+function printCategories() {
+  Console.print(Categories)
 }
 
-function nameIsValid(name) {
-  if(name.length < 2 || name.length > 4) {
-    throw new Error('[ERROR] 코치 이름이 유효하지 않음');
+function chooseCategory() {
+  while(Categories.length < 5) {
+    const category = CATEGORIES.get(Random.pickNumberInRange(1, 5))
+    Categories.push(category)
   }
+  return printCategories()
+}
+
+function menusCntValid(menus) {
+  if(menus.length > 2) {
+    throw new Error('[ERROR] 못먹는 메뉴가 너무 많음')
+  }
+}
+
+function menuIsValid(menu) {
+  const allMenu = Object.values(SAMPLE)
+  let isValid = false
+  allMenu.forEach((menuArr) => {
+    if(menuArr.includes(menu)) isValid = true
+  })
+  if(isValid === false) {
+    throw new Error('[ERROR] 존재하지 않는 메뉴')
+  }
+}
+
+function menuIsDup(menus) {
+  const menuSet = new Set(menus)
+  if(menuSet.size !== menus.length) {
+    throw new Error('[ERROR] 중복된 메뉴 존재')
+  }
+}
+
+function cantEatIsValid(answer) {
+  const menus = answer.split(',')
+  menusCntValid(menus)
+  menus.forEach((menu) => {
+    menuIsValid(menu)
+  })
+  menuIsDup(menus)
+  return menus
+}
+
+function inputCantEat(idx) {
+  const name = Names[idx]
+  Console.readLine(`${name}(이)가 못 먹는 메뉴를 입력해 주세요.\n`, (answer => {
+    try {
+      const cantEatArray = cantEatIsValid(answer)
+      BannedMenu[name] = cantEatArray.slice()
+      return cantEat(idx+1)
+    } catch(error) {
+      Console.print(error.message)
+      return inputCantEat(idx)
+    }
+  }))
+  return
+}
+
+function cantEat(idx) {
+  if(idx === Names.length) {
+    return chooseCategory()
+  }
+  BannedMenu[Names[idx]] = ''
+  inputCantEat(idx)
 }
 
 function namesCntValid(names) {
   if(names.length < 2 || names.length > 5) {
     throw new Error('[ERROR] 코치 숫자가 안맞음')
+  }
+}
+
+function nameIsValid(name) {
+  if(name.length < 2 || name.length > 4) {
+    throw new Error('[ERROR] 코치 이름이 유효하지 않음');
   }
 }
 
@@ -46,13 +118,20 @@ function namesAreValid(answer) {
   return names
 }
 
+function namesToConst(names) {
+  names.forEach((name) => {
+    Names.push(name)
+  })
+  return cantEat(0)
+}
+
 function inputCoachNames() {
   Console.readLine('코치의 이름을 입력해 주세요. (, 로 구분)\n', (answer => {
     try {
       const names = namesAreValid(answer)
-      return bannedMenu(names)
+      return namesToConst(names)
     } catch(error) {
-      Console.print(error.message)
+      Console.print(error)
       return inputCoachNames()
     }
   }))
