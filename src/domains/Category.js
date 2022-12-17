98730a9b59f524f9fb9826d778ee97a2ed7f3605
solo5star <solo_5@naver.com>
2022-12-17 14:24:02 +0900

    feat(domains): 카테고리 기능 구현

---
+++ src/domains/Category.js
@@ -0,0 +1,38 @@
+const Menu = require('./Menu');
+
+class Category {
+  /** @type {number} */
+  #id;
+
+  /** @type {string} */
+  #name;
+
+  /** @type {Menu[]} */
+  #menus;
+
+  /**
+   *
+   * @param {number} id
+   * @param {string} name
+   * @param {Menu[]} menus
+   */
+  constructor(id, name, menus) {
+    this.#id = id;
+    this.#name = name;
+    this.#menus = menus;
+  }
+
+  getId() {
+    return this.#id;
+  }
+
+  getName() {
+    return this.#name;
+  }
+
+  getMenus() {
+    return this.#menus;
+  }
+}
+
+module.exports = Category;
