e43da475ad17581a329da5c430e69f630d5a2662
pakxe <pigkill40@naver.com>
2022-12-17 15:45:49 +0900

    feat(MenuRecommender): 최대 중복 카테고리가 2개까지이도록 메서드 작성

--- src/domain/MenuRecommender.js
+++ src/domain/MenuRecommender.js
@@ -1,3 +1,37 @@
-const MenuRecommender = class {};
+const { Random } = require('@woowacourse/mission-utils');
+const { MENU, WEEK, CATEGORY } = require('../constants/Constants');
 
+const MenuRecommender = class {
+  recommend(names, hateMenus) {
+    const recommendList = {};
+
+    names.reduce((recommendList, name, i) => {
+      recommendList[`${name}`] = this.#personalRecommend(name, hateMenus[i]);
+    }, {});
+  }
+
+  #personalRecommend(name, hateMenus) {}
+
+  initWeekCategory() {
+    const category = [];
+
+    while (category.length < WEEK.size) {
+      const selected = Random.pickNumberInRange(1, CATEGORY.size);
+      if (!this.#isAlreadyHave(category, selected)) category.push(selected);
+    }
+    console.log(category);
+    return category;
+  }
+
+  #isAlreadyHave(category, selected) {
+    const duplicatedCount = category.filter(
+      (already) => already === selected
+    ).length;
+    if (duplicatedCount >= CATEGORY.max) return true;
+    return false;
+  }
+};
+
+const a = new MenuRecommender();
+a.initWeekCategory();
 module.exports = MenuRecommender;
