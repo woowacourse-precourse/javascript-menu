f21ce116b6efbdd8bd4c0742e186e756a664ba02
HeoJiye <heoze1031@gmail.com>
2022-12-17 15:23:34 +0900

    feat: 무작위로 음식 선정 구현

--- src/model/MenuRecommend.js
+++ src/model/MenuRecommend.js
@@ -11,6 +11,10 @@ class MenuRecommend {
         const categorys = this.#menu.keys();
         return categorys.get(MissionUtils.Random.pickNumberInRange(0, 4));
     }
+    recommendFood(category) {
+        const foods = this.#menu[category];
+        return MissionUtils.Randoms.shuffle(foods)[0];
+    }
 }
 
 module.exports = MenuRecommend;
