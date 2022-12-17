103a84eb66129a1123110f875904f689d3602db7
minjaeKim <smallkdb@gmail.com>
2022-12-17 13:25:50 +0900

    feat(InputView): 입력 뷰를 담당하는 객체 구현

---
+++ src/view/InputView.js
@@ -0,0 +1,12 @@
+const { Console } = require('@woowacourse/mission-utils');
+
+const InputView = {
+  readCoach(callback) {
+    Console.readLine('코치의 이름을 입력해 주세요. (, 로 구분)', (input) => callback(input));
+  },
+  readExcludeFood(callback, name) {
+    Console.readLine(`${name}(이)가 못 먹는 메뉴를 입력해 주세요.`, (input) => callback(input));
+  },
+};
+
+module.exports = InputView;
