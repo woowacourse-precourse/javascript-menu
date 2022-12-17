528d7e8889193f3272d6ebcf7f1d9845d64ad871
ming0830 <minsun0830@naver.com>
2022-12-17 17:38:47 +0900

    feat: menu 클래스와 UI로직 연결을 위함

--- src/Controller/Controller.js
+++ src/Controller/Controller.js
@@ -2,58 +2,65 @@ const InputView = require("../View/InputView");
 const OutputView = require("../View/Outputview");
 const { Console } = require("@woowacourse/mission-utils");
 const Menu = require("../Model/Menu");
-const {generate} = require("../util/RandomGenerator");
+const { generate } = require("../util/RandomGenerator");
 
 class Controller {
-  constructor(){
-    this.coachesName = {}
-    this.coachHateMenu = {}
-    this.startCount = 0
-    this.count = this.coachesName.length - 1
-    this.menu = new Menu()
-    this.days = ['월요일','화요일','수요일','목요일','금요일']
-    // this.categoryOfday = [];
+  constructor() {
+    this.coachesName = {};
+    this.coachHateMenu = {};
+    this.startCount = 0;
+    this.count = this.coachesName.length - 1;
+    this.menu = new Menu();
+    this.days = ["월요일", "화요일", "수요일", "목요일", "금요일"];
+    this.recommendMenu = [];
   }
   showgameStartGuide() {
-    OutputView.printGameStartMessage()
-    this.askCoachesName()
+    OutputView.printGameStartMessage();
+    this.askCoachesName();
   }
   askCoachesName() {
     InputView.readCoachesName(this.saveCoachesName.bind(this));
   }
   saveCoachesName(coachesName) {
     this.coachesName = Object.values(coachesName); // [ 'a', 'b', 'c' ]
-    this.askCoachesHateMenu()
+    this.askCoachesHateMenu();
   }
 
   askCoachesHateMenu() {
-    if(this.coachesName[this.startCount]){
-      InputView.readCoachHateMenu(this.coachesName[this.startCount], this.saveCoachHateMenu.bind(this));
-    } else this.ShowResultMessage()
+    if (this.coachesName[this.startCount]) {
+      InputView.readCoachHateMenu(
+        this.coachesName[this.startCount],
+        this.saveCoachHateMenu.bind(this)
+      );
+    } else this.ShowResultMessage();
   }
   saveCoachHateMenu(coachHateMenu) {
-    this.coachHateMenu[this.coachesName[this.startCount]] = coachHateMenu
-    // Console.print(this.coachHateMenu)
-    this.startCount++
-    this.askCoachesHateMenu()
+    this.coachHateMenu[this.coachesName[this.startCount]] = coachHateMenu;
+    this.startCount++;
+    this.askCoachesHateMenu();
   }
   ShowResultMessage() {
-    OutputView.printResultStartMessage()
-    this.showResultCategory()
+    OutputView.printResultStartMessage();
+    this.showResultCategory();
   }
   userCategory() {
-      this.categoryOfday = this.days.reduce((acc,curr) => {
-        return {...acc, [curr]:this.menu.getCategory(generate)}
-      },{});
-
-      return Object.values(this.categoryOfday).join(' | ')
+    this.categoryOfday = this.days.reduce((acc, curr) => {
+      return { ...acc, [curr]: this.menu.getCategory(generate) };
+    }, {});
+      return Object.values(this.categoryOfday).join(" | ");
   }
 
   showResultCategory() {
-    OutputView.printResult(this.userCategory())
+    OutputView.printResult(this.userCategory());
+    this.showResultMenu();
   }
 
+  showResultMenu() {
+    Object.values(this.categoryOfday).forEach((catagory) =>{
+      this.recommendMenu.push(this.menu.getUserMenuCategory(catagory))
+    });
+    Console.print(this.recommendMenu);
+  }
 }
-
 
 module.exports = Controller;
