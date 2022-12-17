74612b494964bdf4b76eb140906ec61f9162dc7f
Minhye Kang <alsgp9898@naver.com>
2022-12-17 13:40:50 +0900

    faet: 입력된 코치가 2~5명이 아닐 때 에러 발생

--- src/Validation.js
+++ src/Validation.js
@@ -2,7 +2,8 @@ const Validation = {
   checkCoachNames(names) {
     if (!names.trim()) throw new Error('아무것도 입력되지 않았습니다.');
     names = names.split(',')
-    if (names.some((name) => name.length < 2 || name.length > 4)) throw new Error('코치 이름은 최소 2글자, 최대 4글자여야 합니다.')
+    if (names.some((name) => name.length < 2 || name.length > 4)) throw new Error('코치 이름은 최소 2글자, 최대 4글자여야 합니다.');
+    if (names.length < 2 || names.length >5) throw new Error('코치는 최소 2명 최대 5명이어야 합니다.')
   }
 }
 
