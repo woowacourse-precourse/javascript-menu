a693e97cc140a5d45a53a7572e50838cd9ee38b8
wonkyum-kim <wonkyum256@gmail.com>
2022-12-17 14:16:52 +0900

    feat(LunchMenu): input impossible foods
    - Read impossible foods for each coaches

--- src/LunchMenu.js
+++ src/LunchMenu.js
@@ -17,9 +17,21 @@ class LunchMenu {
     try {
       const coachNames = names.split(',');
       LunchMenuError.isValidCoachName(coachNames);
+      this.#readImpossibleMenu(coachNames);
     } catch {
       this.#readCoachNames();
     }
+  };
+
+  #readImpossibleMenu = (coachNames) => {
+    coachNames.forEach((name) => {
+      const INPUT_IMPOSSIBLE_MENU = `\n${name}(이)가 못 먹는 메뉴를 입력해 주세요.\n`;
+      MissionUtils.Console.readLine(INPUT_IMPOSSIBLE_MENU, this.#readImpossibleMenuHandler);
+    });
+  };
+
+  #readImpossibleMenuHandler = (menu) => {
+
   };
 }
 
