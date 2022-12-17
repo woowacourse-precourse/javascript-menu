c27a4e884b682387e5fe174b942cd573229ad987
inyeong-kang <tomatorkd@naver.com>
2022-12-17 13:40:06 +0900

    docs: Add fucntion about category

--- src/App.js
+++ src/App.js
@@ -1,15 +1,6 @@
 const InputView = require("./View/InputView");
 const OutputView = require("./View/OutputView");
 
-const SAMPLE = {
-	일식: '규동, 우동, 미소시루, 스시, 가츠동, 오니기리, 하이라이스, 라멘, 오코노미야끼',
-	한식: '김밥, 김치찌개, 쌈밥, 된장찌개, 비빔밥, 칼국수, 불고기, 떡볶이, 제육볶음',
-	중식: '깐풍기, 볶음면, 동파육, 짜장면, 짬뽕, 마파두부, 탕수육, 토마토 달걀볶음, 고추잡채',
-	아시안:
-		'팟타이, 카오 팟, 나시고렝, 파인애플 볶음밥, 쌀국수, 똠얌꿍, 반미, 월남쌈, 분짜',
-	양식: '라자냐, 그라탱, 뇨끼, 끼슈, 프렌치 토스트, 바게트, 스파게티, 피자, 파니니',
-};
-
 class App {
 
   constructor() {
@@ -38,13 +29,19 @@ class App {
 		
 		this.personCnt += 1;
 		
-		if(this.personCnt == this.nameList.length) {
-			return this.showRecommendation();
+		if(this.personCnt == this.nameList.length) { //모든 사람들의 못먹는 메뉴를 알아봤다!
+			return this.showRecommendation(); 
 		} 
 		return this.askMenu(this.nameList[this.personCnt]);
 	});
   }
+
+  recommend() {
+
+  }
+
   showRecommendation() {
+	
 	console.log("hi recommend!");
   }
 
