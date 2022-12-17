29840584b2cf979a12393f613aedf6707944a138
HeoJiye <heoze1031@gmail.com>
2022-12-17 13:42:18 +0900

    feat: 서비스 시작 문구를 출력 단순 구현

---
+++ src/view/OutputView.js
@@ -0,0 +1,8 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
+const OutputView = {
+    printStart() {
+        MissionUtils.Console.print('점심 메뉴 추천을 시작합니다.');
+    }
+};
+module.exports = OutputView;
