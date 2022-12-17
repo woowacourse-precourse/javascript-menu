e3075c9cff90bb630c602a5dc4112a434b74fcc5
violetwhenisawu <anashape710@gmail.com>
2022-12-17 15:59:25 +0900

    feat: 각 난수 추출의 결과로, 카테고리 배열을 반환하는 매서드 구현

--- src/model/CategoryMaker.js
+++ src/model/CategoryMaker.js
@@ -1,3 +1,30 @@
-const CategoryMaker = {};
+const CategoryMaker = {
+  makeCategory(generateRandomNumber) {
+    let count = 0;
+    const categoryArr = [];
+    while (count < 5) {
+      let randomNum = Number(generateRandomNumber());
+      switch (randomNum) {
+        case 1:
+          categoryArr.push('일식');
+          break;
+        case 2:
+          categoryArr.push('한식');
+          break;
+        case 3:
+          categoryArr.push('중식');
+          break;
+        case 4:
+          categoryArr.push('아시안');
+          break;
+        case 5:
+          categoryArr.push('양식');
+          break;
+      }
+      count++;
+    }
+    return categoryArr;
+  },
+};
 
 module.exports = CategoryMaker;
