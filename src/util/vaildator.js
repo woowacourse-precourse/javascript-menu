4ae0c2832a4ce3ca1fd9ca6dbc46a995e45303f1
2yunseong <dbsdltjd3701@naver.com>
2022-12-17 14:03:53 +0900

    feat(Vaildator): 코치의 수가 2명에서 5명 사이인지 판단하는 기능

--- src/util/vaildator.js
+++ src/util/vaildator.js
@@ -2,6 +2,10 @@ const Vaildator = {
   isVaildNameLength(name) {
     return name.length >= 2 && name.length <= 4;
   },
+
+  isVaildCoachNumber(coachs) {
+    return coachs.length >= 2 && coachs.length <= 5;
+  },
 };
 
 module.exports = Vaildator;
