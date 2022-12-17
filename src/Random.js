648159964906369e1d1bd0979b91489fadaf64fd
chsua <csj1919@naver.com>
2022-12-17 16:00:32 +0900

    Fix: random으로 메뉴 만들어내는 함수 수정

--- src/Random.js
+++ src/Random.js
@@ -1,5 +1,4 @@
 const { Console, Random } = require("@woowacourse/mission-utils");
-const { MATCH, CATEGORY } = require("./Constant");
 
 const Make = {
     makeCategory() {
@@ -14,10 +13,14 @@ const Make = {
     },
 
     makeMenu(category) {
-        let foodList = CATEGORY.get(category)
-        foodList = foodList.split(", ")
-        foodList = MissionUtils.Random.shuffle(foodList)
-        return foodList[0]
+        const menus = new Map([["일식", '규동, 우동, 미소시루, 스시, 가츠동, 오니기리, 하이라이스, 라멘, 오코노미야끼'],
+        ["한식", '김밥, 김치찌개, 쌈밥, 된장찌개, 비빔밥, 칼국수, 불고기, 떡볶이, 제육볶음'],
+        ["중식", '깐풍기, 볶음면, 동파육, 짜장면, 짬뽕, 마파두부, 탕수육, 토마토 달걀볶음, 고추잡채'],
+        ["아시안", '팟타이, 카오 팟, 나시고렝, 파인애플 볶음밥, 쌀국수, 똠얌꿍, 반미, 월남쌈, 분짜'],
+        ["양식", '라자냐, 그라탱, 뇨끼, 끼슈, 프렌치 토스트, 바게트, 스파게티, 피자, 파니니']])
+        const randomIndex = Random.shuffle([1, 2, 3, 4, 5, 6, 7, 8, 9])
+        const menu = menus.get(category).split(", ")
+        return menu[0]
     }
 }
 
