d0594a2d3b3ce065a02b08b5704f0642fe869eac
SEMNIL5202 <semnil5202@gmail.com>
2022-12-17 15:08:16 +0900

    fix: 누락된 개행처리 및 출력문

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -2,11 +2,11 @@ const { Console } = require('@woowacourse/mission-utils');
 
 const InputView = {
   Coach(callback) {
-    Console.readLine('코치의 이름을 입력해 주세요.', callback);
+    Console.readLine('코치의 이름을 입력해 주세요. (, 로 구분)\n', callback);
   },
 
   notEat(coachName, callback) {
-    Console.readLine(`${coachName}(이)가 못 먹는 메뉴를 입력해 주세요.`, callback);
+    Console.readLine(`${coachName}(이)가 못 먹는 메뉴를 입력해 주세요.\n`, callback);
   },
 };
 
--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -2,7 +2,7 @@ const { Console } = require('@woowacourse/mission-utils');
 
 const OutputView = {
   startMent() {
-    Console.print('점심 메뉴 추천을 시작합니다');
+    Console.print('점심 메뉴 추천을 시작합니다.\n');
   },
 };
 
