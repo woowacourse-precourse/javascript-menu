4060d46d1cd02e59b16f461f206fc5e738d497ce
TaeeunKim <xodms0309@ewhain.net>
2022-12-17 17:22:24 +0900

    test: 카테고리를 만드는 함수 테스트 작성

---
+++ __tests__/CategoryTest.js
@@ -0,0 +1,18 @@
+const CategoryMaker = require('../src/CategoryMaker');
+
+describe('랜덤 카테고리를 만드는 함수 테스트', () => {
+  test('카테고리는 중복을 2개까지만 허용한다.', () => {
+    const categories = CategoryMaker.createCategory();
+    const occurrence = new Array(6).fill(0);
+    for (const category of categories) {
+      occurrence[category] += 1;
+    }
+    const size = occurrence.filter((el) => el > 2).length;
+    expect(size).toBe(0);
+  });
+
+  test('카테고리 생성 함수는 배열을 리턴한다.', () => {
+    const categories = CategoryMaker.createCategory();
+    expect(categories).toBeInstanceOf(Array);
+  });
+});
