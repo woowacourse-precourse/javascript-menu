792cc901b9466c3bbd54ed379ef0de78dd933832
evencoding <evencoding@gmail.com>
2022-12-17 13:27:45 +0900

    feat: 메뉴 추천을 받을 코치의 이름을 입력하는 기능 구현

--- src/controller/MenuController.js
+++ src/controller/MenuController.js
@@ -1,9 +1,18 @@
+const InputView = require('../views/InputView');
 const OutputView = require('../views/OutputView');
 
 class MenuController {
   startRecommend() {
     OutputView.printStartRecommend();
+
+    this.#inputCoachesName();
   }
+
+  #inputCoachesName() {
+    InputView.askCoachesName(this.#validateCoachesName.bind(this));
+  }
+
+  #validateCoachesName(names) {}
 }
 
 module.exports = MenuController;
--- src/views/InputView.js
+++ src/views/InputView.js
@@ -1,5 +1,13 @@
 const { Console } = require('@woowacourse/mission-utils');
 
-const InputView = {};
+const MESSAGE = {
+  ASK_COACHES_NAME: '\n코치의 이름을 입력해 주세요. (, 로 구분)\n',
+};
+
+const InputView = {
+  askCoachesName(callback) {
+    Console.readLine(MESSAGE.ASK_COACHES_NAME, callback);
+  },
+};
 
 module.exports = InputView;
