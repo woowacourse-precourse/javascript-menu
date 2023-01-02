ff918350a40191af701d18a8dfa76a9f02864b8c
daal2quf <daal2quf@gmail.com>
2023-01-02 17:53:06 +0900

    refactor(coach): 불필요한 필드값 제거 및 getter반환값 변경
    changed:
    getNames(): 코치 이름으로 이루어진 키값을 배열로 반환

--- src/domain/Coach.js
+++ src/domain/Coach.js
@@ -1,24 +1,19 @@
 class Coach {
   constructor() {
-    this.coachs;
-    this.coachHateFood = new Map();
+    this.coachs = new Map();
   }
 
   setName(coach) {
-    this.coachs = coach.split(',');
+    const coachNameArray = coach.split(',');
+    coachNameArray.forEach(name => this.coachs.set(name));
   }
 
   setHateFood(coachName, hateFood) {
     this.coachHateFood.set(coachName, hateFood);
-    console.log(this.coachHateFood);
   }
 
-  getName() {
-    return this.coachs;
-  }
-
-  getHateFood() {
-    return this.coachHateFood();
+  getNames() {
+    return [...this.coachs.keys()];
   }
 }
 
