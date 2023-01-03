f1462241665c0861513e983cee7889098443e9e3
daal2quf <daal2quf@gmail.com>
2023-01-03 20:56:44 +0900

    test(recommendMenu)잘못된 테스트 삭제 및 pickRandomMenuStyle() 테스트
    add:
    test1: 랜덤으로 생성된 메뉴 스타일이 MenuMap의 키값에 포함되어있는지 확인

--- __tests__/RecommandMenuTest.js
+++ __tests__/RecommandMenuTest.js
@@ -1,31 +1,20 @@
 const { Console } = require('@woowacourse/mission-utils');
 const { MenuMap } = require('../src/domain/MenuMap');
-const RecommandMenu = require('../src/domain/RecommandMenu');
+const RecommendMenu = require('../src/domain/RecommandMenu');
 
 afterEach(() => {
   Console.close();
 });
 
 beforeEach(() => {
-  recommandMenu = new RecommandMenu();
+  recommendMenu = new RecommendMenu();
 });
 
-describe('RecommandMenu 클래스의 exceptHateFoodOfMenu 메서드 테스트', () => {
-  test('싫어하는 음식이 제거되는지 확인', () => {
-    jest.setTimeout(30000);
-    const menu = ['치킨', '피자', '야채'];
-    const hateFood = ['야채'];
-    const result = recommandMenu.exceptHateFoodOfMenu(menu, hateFood);
-    expect(result).toEqual(['치킨', '피자']);
-  });
-});
-
-describe('RecommandMenu 클래스의 convertArray 메서드 테스트', () => {
-  test('메뉴맵 형식의 문자열을 메뉴추천 프로그램에 맞는 형태의 배열로변환', () => {
-    jest.setTimeout(30000);
-    const menu = '참치, 김치';
-    const expected = ['참치', '김치'];
-    const result = recommandMenu.convertArray(menu);
-    expect(result).toEqual(expected);
+describe(' RecommendMenu 클래스의 pickRandomMenuStyle() 테스트', () => {
+  test('MenuMap에 들어있는 메뉴 스타일에 포함된 메뉴스타일인지 확인', () => {
+    const randomMenuStyle = recommendMenu.pickRandomMenuStyle();
+    const validMenuStyle = [...recommendMenu.menu.keys()].join(', ');
+    console.log(validMenuStyle);
+    expect(validMenuStyle).toEqual(expect.stringContaining(randomMenuStyle));
   });
 });
