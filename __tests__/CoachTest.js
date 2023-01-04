d4d73970bd260b81276a0acc2019d61abbeed73e
daal2quf <daal2quf@gmail.com>
2023-01-04 13:12:01 +0900

    fix(coachTest): 코치님들의 이름 입력값 초기 설정 수정
    change:
    beforeEach() 에서 coach.setName의 argument로 띄어쓰기 수정

--- __tests__/CoachTest.js
+++ __tests__/CoachTest.js
@@ -2,7 +2,7 @@ const Coach = require('../src/domain/Coach');
 
 beforeEach(() => {
   coach = new Coach();
-  coach.setName('구구, 제임스');
+  coach.setName('구구,제임스');
   coach.setHateFood('구구', '김밥');
   coach.setHateFood('제임스', '떡볶이');
 });
@@ -10,6 +10,7 @@ beforeEach(() => {
 describe('Coach 클래스의 getNames() 메서드 테스트', () => {
   test('초기 설정된 코치님들의 이름이 배열로 반환되는지 확인', () => {
     const coachsNameArray = coach.getNames();
+    console.log(coachsNameArray);
     expect(coachsNameArray).toEqual(['구구', '제임스']);
   });
   test.failing('초기 설정된 코치님들의 이름을 잘못된 값으로 설정했을때 실패 테스트', () => {
