05ca776586df1527759b05d7b617830a52cbc64e
daal2quf <daal2quf@gmail.com>
2023-01-04 13:01:49 +0900

    test(recommendMenu): createRandomCateroy() 메서드 테스트
    add:
    test: 생성된 랜덤 카테고리 배열의 길이 확인

--- __tests__/RecommendMenuTest.js
+++ __tests__/RecommendMenuTest.js
@@ -1,6 +1,6 @@
 const { Console } = require('@woowacourse/mission-utils');
 const { MenuMap } = require('../src/domain/MenuMap');
-const RecommendMenu = require('../src/domain/RecommandMenu');
+const RecommendMenu = require('../src/domain/RecommendMenu');
 
 afterEach(() => {
   Console.close();
@@ -10,11 +10,17 @@ beforeEach(() => {
   recommendMenu = new RecommendMenu();
 });
 
-describe(' RecommendMenu 클래스의 pickRandomMenuStyle() 테스트', () => {
+describe('RecommendMenu 클래스의 pickRandomMenuStyle() 테스트', () => {
   test('MenuMap에 들어있는 메뉴 스타일에 포함된 메뉴스타일인지 확인', () => {
     const randomMenuStyle = recommendMenu.pickRandomMenuStyle();
     const validMenuStyle = [...recommendMenu.menu.keys()].join(', ');
-    console.log(validMenuStyle);
     expect(validMenuStyle).toEqual(expect.stringContaining(randomMenuStyle));
+  });
+});
+
+describe('RecommendMenu 클래스의 createRandomCateroy() 테스트', () => {
+  test('메서드로 생성된 배열의 길이가 맞는지 확인', () => {
+    const category = recommendMenu.createRandomCateroy();
+    expect(category).toHaveLength(5);
   });
 });
