abb9964a2cb7b1558a4c04f1458645565fd1b500
daal2quf <daal2quf@gmail.com>
2023-01-04 13:05:26 +0900

    feat(outputView): 메뉴추천 결과 출력 및 카테고리 출력 기능 추가
    add:
    printRecommendResult(): 메뉴 추천 결과 문구 출력 기능
    printCategory(): argumnet로 random category 배열을 받아 출력

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -1,16 +1,24 @@
 const { Console } = require('@woowacourse/mission-utils');
 
 const OutputView = {
-  printRecommandMenuStart() {
+  printRecommendMenuStart() {
     Console.print('점심 메뉴 추천을 시작합니다.');
   },
 
-  printRecommandMenuAllDone() {
+  printRecommendMenuAllDone() {
     Console.print('추천을 완료했습니다.');
+  },
+
+  printRecommendResult() {
+    Console.print('메뉴 추천 결과입니다.');
   },
 
   printErrorMessage(errorMessage) {
     Console.print(errorMessage);
+  },
+
+  printCategory(category) {
+    Console.print(`[ 카테고리 | ${category.join(' | ')} ]`);
   },
 };
 
