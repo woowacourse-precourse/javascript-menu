49205cf3775d6618484d29e1fd6adb93a3e4f432
daal2quf <daal2quf@gmail.com>
2023-01-04 12:59:09 +0900

    feat(recommendMenu): 랜덤 메뉴 스타일 및 랜덤 카테고리 생성 기능 요구사항에 맞게 변경
    add:
    pickRandomMenuStyle(): menuMap의 키값에 랜덤으로 생성된 1-5범위의 숫자를 인덱스로 랜덤 메뉴 스타일 생성
    createRandomCateroy(): pickRandomMenuStyle()의 반환값을 사용해 3개 이상 중복되지 않은 메뉴스타일 카테고리 생성

--- src/domain/RecommendMenu.js
+++ src/domain/RecommendMenu.js
@@ -1,30 +1,41 @@
 const { Random } = require('@woowacourse/mission-utils');
 const { MenuMap } = require('./MenuMap');
 
-class RecommandMenu {
+class RecommendMenu {
   constructor() {
     this.menu = MenuMap;
   }
 
-  pickCategory() {
-    const index = Random.pickNumberInRange(1, 5) - 1;
-    return [...this.menu.keys()][index];
+  pickRandomMenuStyle() {
+    return [...this.menu.keys()][Random.pickNumberInRange(1, 5) - 1];
   }
 
-  createRandomMenu(category) {
-    return this.shuffleMenu(this.convertArray(this.menu.get(category)));
+  createRandomCateroy() {
+    const category = [];
+    while (category.length < 5) {
+      const RandomMenuStyle = this.pickRandomMenuStyle();
+      const categoryMap = category.reduce((acc, cur) => {
+        acc.set(cur, (acc.get(cur) || 0) + 1);
+        return acc;
+      }, new Map());
+      if (categoryMap.get(RandomMenuStyle) === 2) {
+        continue;
+      }
+      category.push(RandomMenuStyle);
+    }
+    return category;
   }
 
-  convertArray(ovject) {
+  static convertArray(ovject) {
     return ovject.split(', ');
   }
 
-  shuffleMenu(menu) {
+  static shuffleMenu(menu) {
     return Random.shuffle(menu);
   }
 
-  exceptHateFoodOfMenu(menu, hateFood) {
-    return menu.filter((food) => !hateFood.includes(food));
+  static exceptHateFoodOfMenu(menu, hateFood) {
+    return menu.filter(food => !hateFood.includes(food));
   }
 
   getFoodOfCategory(category) {
@@ -32,4 +43,4 @@ class RecommandMenu {
   }
 }
 
-module.exports = RecommandMenu;
+module.exports = RecommendMenu;
