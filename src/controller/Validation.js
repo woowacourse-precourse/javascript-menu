c9d5428ddad19b76cc080024974db38e5220177b
daal2quf <daal2quf@gmail.com>
2023-01-06 16:50:57 +0900

    feat(validation): 유저가 싫어하는 음식을 입력시에 공백을 입력했을때 메뉴추천 진행 기능 추가
    add:
    ofMenu(): userInput이 '' 즉, 공백일때 return하여 종료

--- src/controller/Validation.js
+++ src/controller/Validation.js
@@ -2,30 +2,32 @@ const { MenuMap } = require('../domain/MenuMap');
 
 const allFood = [...MenuMap.values()].join(', ').split(', ');
 
-const isCorrectLength = userInput => {
-  return userInput.split(',').every(name => name.length >= 2 && name.length <= 4);
-};
+const IsCorrect = {
+  coachName(userInput) {
+    return userInput.split(',').every(name => name.length >= 2 && name.length <= 4);
+  },
+  howManyCoach(userInput) {
+    return userInput.split(',').length < 2 || userInput.split(',').length > 5;
+  },
 
-const isCorrectHowManyCoach = userInput => {
-  return userInput.split(',').length < 2 || userInput.split(',').length > 5;
-};
-
-const isCorrectInputOfFood = userInput => {
-  return userInput.split(',').every(food => allFood.includes(food));
+  inputOfFood(userInput) {
+    return userInput.split(',').every(food => allFood.includes(food));
+  },
 };
 
 const Validation = {
   ofCoachName(userInput) {
-    if (!isCorrectLength(userInput)) {
+    if (!IsCorrect.coachName(userInput)) {
       throw new Error('[ERROR] 코치의 이름은 2글자 이상 4글자 이하로만 입력해주세요');
     }
-    if (isCorrectHowManyCoach(userInput)) {
+    if (IsCorrect.howManyCoach(userInput)) {
       throw new Error('[ERROR] 코치는 최소 2명 이상 입력해야 합니다.');
     }
   },
 
   ofMenu(userInput) {
-    if (!isCorrectInputOfFood(userInput)) throw new Error('[ERROR] 메뉴에 없는 메뉴입니다.');
+    if (userInput === '') return;
+    if (!IsCorrect.inputOfFood(userInput)) throw new Error('[ERROR] 메뉴에 없는 메뉴입니다.');
   },
 };
 
