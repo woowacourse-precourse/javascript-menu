8944ac4c21529cd42728b9c2c2d7fb0c154695fe
daal2quf <daal2quf@gmail.com>
2023-01-06 17:39:02 +0900

    refactor(recommendMenu): 불필요한 멤버변수 삭제 및 비지니스 로직을 최상단으로 이동
    delete:
    this.menu: MenuMap 객체자체를 불러와 사용하기 때문

--- src/domain/RecommendMenu.js
+++ src/domain/RecommendMenu.js
@@ -2,51 +2,23 @@ const { Random } = require('@woowacourse/mission-utils');
 const { MenuMap } = require('./MenuMap');
 
 class RecommendMenu {
-  constructor() {
-    this.menu = MenuMap;
-  }
-
-  pickRandomMenuStyle() {
-    return [...this.menu.keys()][Random.pickNumberInRange(1, 5) - 1];
-  }
-
-  duplicateCountMap(category) {
-    return category.reduce((acc, cur) => {
-      acc.set(cur, (acc.get(cur) || 0) + 1);
-      return acc;
-    }, new Map());
-  }
-
   createRandomCateroy() {
-    const category = [];
-    while (category.length < 5) {
+    const categoryArray = [];
+    while (categoryArray.length < 5) {
       const RandomMenuStyle = this.pickRandomMenuStyle();
-      if (this.duplicateCountMap(category).get(RandomMenuStyle) === 2) {
+      if (this.duplicateCountMap(categoryArray).get(RandomMenuStyle) === 2) {
         continue;
       }
-      category.push(RandomMenuStyle);
+      categoryArray.push(RandomMenuStyle);
     }
-    return category;
-  }
-
-  shuffleArray(length) {
-    return Random.shuffle(Array.from({ length: length }, (_, index) => index + 1));
-  }
-
-  readFoods(menuStyle) {
-    return this.menu.get(menuStyle).split(', ');
-  }
-
-  shuffledMenu(menuStyle) {
-    const shuffleArray = this.shuffleArray(9);
-    return this.readFoods(menuStyle).map((food, index) => this.readFoods(menuStyle)[shuffleArray[index] - 1]);
+    return categoryArray;
   }
 
   create(coachHateFood, category) {
     const result = [];
     category.forEach((menuStyle, idx) => {
       while (true) {
-        const shuffledMenu = this.shuffledMenu(menuStyle);
+        const shuffledMenu = this.shuffleMenu(menuStyle);
         if (coachHateFood.includes(shuffledMenu[0]) || shuffledMenu[0] === result[result.length - 1]) {
           continue;
         }
@@ -55,6 +27,30 @@ class RecommendMenu {
       }
     });
     return result;
+  }
+
+  pickRandomMenuStyle() {
+    return [...MenuMap.keys()][Random.pickNumberInRange(1, 5) - 1];
+  }
+
+  readMenu(menuStyle) {
+    return MenuMap.get(menuStyle).split(', ');
+  }
+
+  duplicateCountMap(array) {
+    return array.reduce((acc, cur) => {
+      acc.set(cur, (acc.get(cur) || 0) + 1);
+      return acc;
+    }, new Map());
+  }
+
+  shuffleArray(length) {
+    return Random.shuffle(Array.from({ length: length }, (_, index) => index + 1));
+  }
+
+  shuffleMenu(menuStyle) {
+    const shuffleArray = this.shuffleArray(9);
+    return this.readMenu(menuStyle).map((food, index) => this.readMenu(menuStyle)[shuffleArray[index] - 1]);
   }
 }
 
