c1ec5738a559c070f827b31d1af88395d96ed77b
daal2quf <daal2quf@gmail.com>
2023-01-10 19:32:14 +0900

    feat(coach): controller에서 멤버변수로 코치한분을 불러오던 로직을 coach클래스로 분리
    add:
    getCurrentCoach(): handlingCount라는 멤버변수를 사용해 순차적으로 코치님의 이름을 반환
    possibleGetCurrentCoach(): 코치님들이 저장된 키값 사이즈를 비교해 반환 가능한지 판단
    resetHandlingCount, decreaseHandlingCount(): 코치 한분씩 불러오기위한 handlingCount 관리 메서드

--- src/domain/Coach.js
+++ src/domain/Coach.js
@@ -1,6 +1,7 @@
 class Coach {
   constructor() {
     this.coachs = new Map();
+    this.handlingCount = 0;
   }
 
   setName(coach) {
@@ -17,7 +18,25 @@ class Coach {
   }
 
   getHateFood(coach) {
-    return this.coachs.get(coach).split(',');
+    return this.coachs.get(coach);
+  }
+
+  getCurrentCoach() {
+    const currentCoach = this.getNames()[this.handlingCount];
+    this.handlingCount += 1;
+    return currentCoach;
+  }
+
+  possibleGetCurrentCoach() {
+    return this.handlingCount <= this.getNames().length;
+  }
+
+  resetHandlingCount() {
+    this.handlingCount = 0;
+  }
+
+  decreaseHandlingCount() {
+    this.handlingCount -= 1;
   }
 }
 
